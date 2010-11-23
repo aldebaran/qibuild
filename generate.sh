5d805d69df653428ec6f5e019c1db5372ba52e9d
gestes <gestes@aldebaran-robotics.com>
2010-11-23 19:58:59 +0100

    doc: added generate.sh

--- generate.sh
+++ generate.sh
@@ -9,12 +9,16 @@
 CURDIR=$(dirname "$(readlink -f $0 2>/dev/null)")/
 
 
-python ${CURDIR}/doc/asciidoc/generate_doc_from_cmake.py \
+python ${CURDIR}/doc/tools/generate_doc_from_cmake.py \
   "${CURDIR}/cmake/qibuild" \
   "${CURDIR}/cmake/samples" \
   "${CURDIR}/build-doc-pre"
 
+cp "${CURDIR}/doc/asciidoc/index.txt" "${CURDIR}/build-doc-pre"
+
 find ${CURDIR}/build-doc-pre/ -type f -name '*.txt' | while read f ; do
-  asciidoc -a toc -a theme=bare -a pygments "$f"
+  #asciidoc -a toc -a theme=bare -a pygments "$f"
+  #asciidoc -a toc -a linkcss -a theme=bare -a pygments "$f"
+  asciidoc -a toc -a linkcss -a theme= -a stylesheet=bare.css -a pygments "$f"
 done
 
