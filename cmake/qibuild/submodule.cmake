8f2668e40005f30c155996d6863bae0c56239f56
gestes <gestes@aldebaran-robotics.com>
2010-11-23 16:29:12 +0100

    cmake:submodule: add doc

--- cmake/qibuild/submodule.cmake
+++ cmake/qibuild/submodule.cmake
@@ -5,12 +5,11 @@
 ## Copyright (C) 2010 Aldebaran Robotics
 ##
 
-
-# QiBuild SubModule
+#! QiBuild SubModule
 # =================
 # Cedric GESTES <gestes@aldebaran-robotics.com>
 
-# == SubModule ==
+#! == SubModule ==
 # This Cmake module behave more or less like a library, but does not produce
 # any output, you can use submodule to organise your CMakeLists.txt more efficiently
 # but you need to call qi_create_bin or qi_create_lib to make use of your submodule.
@@ -22,44 +21,23 @@
 # for PUBLIC_HEADER *.hpp and *.hxx are searched.
 
 
-
-
-
-# === function qi_submodule_create ===
-# .Description
+#!
 # A submodule is a convenient place to store  sources paths, dependencies
 # and public headers. Submodule can be added a library or a module.
 # SubModule are directly visible in Visual Studio, you can change the
 # submodule name displayed in Visual Studio using VSGROUP, or disable it
 # using NO_VSGROUP
 #
-# .Prototype
-#   qi_submodule_create(name [NO_VSGROUP]
-#                            [VSGROUP vsgroup]
-#                            [SRC src .. ]
-#                            [PUBLIC_HEADER pub .. ]
-#                            [DEPENDENCIES dep .. ])
+# \arg:name               the name of the submodule
+# \flag:NO_SOURCE_GROUP   do not create a source_group
+# \param:SOURCE_GROUP     by default a source_group with name is created,
+#                         if sourcegroup is specified then the source_group
+#                         name will be name\\sourcegroup
+# \group:SRC              the list of source to include in the submodule
+# \group:PUBLIC_HEADER    the list of public headers
+# \group:DEPENDENCIES     the list of dependencies
 #
-# .Parameters
-# * name                    : the name of the submodule
-# * NO_SOURCE_GROUP         : do not create a source_group
-# * SOURCE_GROUP sourcegroup: by default a source_group with name is created,
-#   if sourcegroup is specified then the source_group name will be name\\sourcegroup
-# * SRC src ..              : the list of source to include in the submodule
-# * PUBLIC_HEADER pub ..    : the list of public headers
-# * DEPENDENCIES dep ..     : the list of dependencies
-#
-# .Example
-# [source,cmake]
-# ----
-# # create a submodule with your lib, with one publicheader and a Qt dependencies
-# qi_submodule_create(mylib SRC mylib.cpp myprivateheader.hpp
-#                           PUBLIC_HEADER mypublicheader.hpp
-#                           DEPENDENCIES Qt)
-# # this create a binary that have a dependencies on Qt, and the sources set in the
-# # submodule
-# qi_create_bin(mybin SUBMODULE mylib)
-# ----
+# \example:qi_submodule.cmake
 function(qi_submodule_create _name)
   qi_argn_init(${ARGN})
   qi_argn_flags(NO_VSGROUP)
@@ -75,42 +53,23 @@ function(qi_submodule_create _name)
 endfunction()
 
 
-# === function qi_submodule_add ===
-# .Description
-# This function is similar to qi_submodule_create but append content to
+#! This function is similar to qi_submodule_create but append content to
 # the submodule. This function can be condionnal, if you specify IF
 # then the content will be append to the submodule only if the flags
 # is defined.
 #
-# .Prototype
-#   qi_submodule_add(name [NO_VSGROUP]
-#                         [VSGROUP vsgroup]
-#                         [SRC src .. ]
-#                         [PUBLIC_HEADER pub .. ]
-#                         [DEPENDENCIES dep .. ]
-#                         [IF flags ..])
+# \arg:name               the name of the submodule
+# \flag:NO_SOURCE_GROUP   do not create a source_group
+# \param:SOURCE_GROUP     by default a source_group with name is created,
+#                         if sourcegroup is specified then the source_group
+#                         name will be name\\sourcegroup
+# \param:IF               condition that should be verified before adding content
+#                         for example (WITH_QT)
+# \group:SRC              the list of source to include in the submodule
+# \group:PUBLIC_HEADER    the list of public headers
+# \group:DEPENDENCIES     the list of dependencies
 #
-# .Parameters
-# * [IF flags ..]           : condition that should be verified to add content
-#   to the submodule
-# * see qi_submodule_create for other parameters
-# .Example
-# [source,cmake]
-# ----
-# # create a submodule with your lib, with one publicheader and a Qt dependencies
-# qi_submodule_create(mylib SRC mylib.cpp myprivateheader.hpp
-#                           PUBLIC_HEADER mypublicheader.hpp
-#                           DEPENDENCIES Qt)
-# # append files related to boost to the submodule, this will occur only if
-# # WITH_BOOST is defined.
-# qi_submodule_add(mylib SRC mylibboostfeature.cpp myprivateboostheader.hpp
-#                           PUBLIC_HEADER mypublicboostheader.hpp
-#                           DEPENDENCIES boost
-#                           IF WITH_BOOST)
-# # this create a binary that have a dependencies on Qt and boost (if WITH_BOOST is set)
-# # source and public_header are taken from the submodule
-# qi_create_bin(mybin SUBMODULE mylib)
-# ----
+# \example:qi_submodule.cmake
 function(qi_submodule_add _name)
   qi_argn_init(${ARGN})
   qi_argn_flags(NO_VSGROUP)
