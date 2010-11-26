66a766479d545c41bae42657238b82922b9c4001
gestes <gestes@aldebaran-robotics.com>
2010-11-26 11:45:37 +0100

    cmake: add a global qibuild/qibuild header

---
+++ cmake/qibuild/qibuild.cmake
@@ -0,0 +1,32 @@
+##
+## Author(s):
+##  - Cedric GESTES <gestes@aldebaran-robotics.com>
+##
+## Copyright (C) 2010 Aldebaran Robotics
+##
+
+include("qibuild/log")
+include("qibuild/set")
+include("qibuild/expect")
+include("qibuild/arguments")
+include("qibuild/layout")
+
+if (NOT QI_SDK_DIR)
+  qi_set_global(QI_SDK_DIR "${CMAKE_BINARY_DIR}/sdk/")
+  qi_info("QI_SDK_DIR: ${QI_SDK_DIR}")
+endif (NOT QI_SDK_DIR)
+
+#force buildtype to be Upper case
+if (DEFINED CMAKE_BUILD_TYPE)
+  string(TOUPPER "${CMAKE_BUILD_TYPE}" "_BUILD_TYPE")
+  qi_set_global(CMAKE_BUILD_TYPE "${_BUILD_TYPE}")
+endif()
+
+#ensure CMAKE_BUILD_TYPE is either Debug or Release
+if (CMAKE_BUILD_TYPE STREQUAL "")
+  qi_set_global(CMAKE_BUILD_TYPE "RELEASE")
+endif (CMAKE_BUILD_TYPE STREQUAL "")
+
+
+include("qibuild/target")
+include("qibuild/submodule")
