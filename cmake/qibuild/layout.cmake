8298cb7b44ca90ef739d77e0fed2ba50bba7c829
gestes <gestes@aldebaran-robotics.com>
2010-11-26 11:45:13 +0100

    cmake:qibuild: add layout back

---
+++ cmake/qibuild/layout.cmake
@@ -0,0 +1,34 @@
+##
+## layout.cmake
+## Login : <ctaf@ctaf-maptop>
+## Started on  Sun Oct 18 21:45:51 2009 Cedric GESTES
+## $Id$
+##
+## Author(s):
+##  - Cedric GESTES <gestes@aldebaran-robotics.com>
+##
+## Copyright (C) 2009, 2010 Aldebaran Robotics
+##
+
+#warning findmodule.*.sdk.in and sdk.cmake use implicit relative path
+#between _SDK_LIB and _SDK_CMAKE_MODULES, name could be change, but
+#the relative path between each var should remain the same.
+
+qi_set_global(QI_SDK_BIN            "bin"              )
+qi_set_global(QI_SDK_LIB            "lib"              )
+qi_set_global(QI_SDK_FRAMEWORK      "Frameworks"       )
+qi_set_global(QI_SDK_INCLUDE        "include"          )
+qi_set_global(QI_SDK_SHARE          "share"            )
+qi_set_global(QI_SDK_CONF           "etc"              )
+qi_set_global(QI_SDK_DOC            "share/doc"        )
+qi_set_global(QI_SDK_CMAKE          "lib/cmake"        )
+qi_set_global(QI_SDK_CMAKE_MODULES  "lib/cmake/modules")
+
+mark_as_advanced(QI_SDK_BIN
+                 QI_SDK_LIB
+                 QI_SDK_INCLUDE
+                 QI_SDK_SHARE
+                 QI_SDK_CONF
+                 QI_SDK_DOC
+                 QI_SDK_CMAKE
+                 QI_SDK_CMAKE_MODULES)
