5015c11b470dfdd09affa7b3953f5aae1505824e
gestes <gestes@aldebaran-robotics.com>
2010-12-02 19:58:13 +0100

    add qtwebkit

---
+++ cmake/qibuild/modules/QT_QTWEBKITConfig.cmake
@@ -0,0 +1,12 @@
+## Copyright (C) 2009, 2010 Aldebaran Robotics
+
+get_filename_component(_ROOT_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
+include("${_ROOT_DIR}/qtutils.cmake")
+
+
+set(_suffix "QTWEBKIT")
+set(_libame "QtWebKit")
+
+qt_flib(${_suffix} ${_libame})
+export_lib(QT_${_suffix})
+set(_ROOT_DIR)
