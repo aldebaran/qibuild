a4dfeeb1785b0a7fb47b7f26135f6b1e2920d759
gestes <gestes@aldebaran-robotics.com>
2010-12-07 17:26:01 +0100

    set: add qi_set_advanced_cache and qi_set_cache

--- cmake/qibuild/set.cmake
+++ cmake/qibuild/set.cmake
@@ -8,3 +8,12 @@
 macro(qi_set_global name)
   set("${name}" ${ARGN} CACHE INTERNAL "" FORCE)
 endmacro()
+
+macro(qi_set_cache name)
+  set("${name}" ${ARGN} CACHE STRING "" FORCE)
+endmacro()
+
+macro(qi_set_advanced_cache name)
+  set("${name}" ${ARGN} CACHE STRING "" FORCE)
+  mark_as_advanced("${name}")
+endmacro()
