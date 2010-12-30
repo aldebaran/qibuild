0bd49608f6c3d57c19b8c5dbb87b3d17514cb9f9
Cedric GESTES <gestes@aldebaran-robotics.com>
2010-12-30 18:07:07 +0100

    python: install-qibuild.sh: forward PYTHONPATH, better output

--- install-qibuild.sh
+++ install-qibuild.sh
@@ -6,23 +6,25 @@
 ## Copyright (C) 2010 Aldebaran Robotics
 ##
 
-#this script will install naogentoo, toc, on, dorelease
+#Create a simple launcher for QiBuild. It will install in /usr/local/bin.
 
 DESTDIR="/usr/local/bin"
 
 create_launcher() {
   full_path=$1
   name=$(basename $full_path)
-  if which readlink ; then
+  if which readlink >/dev/null 2>/dev/null ; then
       p=$(dirname "$(readlink -f $0 2>/dev/null)")
   else
       p=$(pwd)
   fi
-  echo "QiBuild directory: $p"
+  #echo "QiBuild directory: $p"
 
-  echo '#!/bin/sh'                                           > ${DESTDIR}/${name}
-  echo "PYTHONPATH=\"$p\" python \"${p}/${full_path}\" \$@" >> ${DESTDIR}/${name}
+  echo '#!/bin/sh'                                                         > ${DESTDIR}/${name}
+  echo "PYTHONPATH=\"$p:${PYTHONPATH}\" python \"${p}/${full_path}\" \$@" >> ${DESTDIR}/${name}
   chmod 755 ${DESTDIR}/${name}
+
+  echo "installed: ${DESTDIR}/${name}"
 }
 
 mkdir -p ${DESTDIR}
