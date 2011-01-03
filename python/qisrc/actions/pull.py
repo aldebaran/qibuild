305b2eb4f630b12828f93f5de94bc6247e2560c7
gestes <gestes@aldebaran-robotics.com>
2011-01-03 09:56:29 +0100

    qisrc pull: support --rebase

--- python/qisrc/actions/pull.py
+++ python/qisrc/actions/pull.py
@@ -16,6 +16,7 @@ def configure_parser(parser):
     """Configure parser for this action """
     qitools.qiworktree.work_tree_parser(parser)
     parser.add_argument("--continue", action="store_true", dest="continue_on_error", help="continue on error")
+    parser.add_argument("--rebase", action="store_true", dest="rebase", help="rebase")
 
 def do(args):
     """Main entry point"""
@@ -26,7 +27,10 @@ def do(args):
             #TODO: replace by try/except?
             if git.is_valid():
                 print "pull:", git_project
-                git.pull()
+                if args.rebase:
+                    git.pull('--rebase')
+                else:
+                    git.pull()
         except Exception, e:
             if (args.continue_on_error):
                 continue
