dae5904e82a52b7cd7f80ee12abd298ed93c2f0d
Cedric GESTES <gestes@aldebaran-robotics.com>
2011-01-03 02:26:27 +0100

    python: added python_profile.sh

---
+++ python/python_profile.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+##
+## Author(s):
+##  - Cedric GESTES <gestes@aldebaran-robotics.com>
+##
+## Copyright (C) 2011 Aldebaran Robotics
+##
+
+PYTHONPATH=. python -m cProfile -stime $@
+
+
+#how to get better profiling:
+#python_profile.sh -o mysuperoutput.stats ../bin/qibuild
+#python -mpstats mysuperoutput.stats
+#sort time
+#stats 10
