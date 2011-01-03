648c24780e4b98e2cb836632e47b472bdd83929c
Cedric GESTES <gestes@aldebaran-robotics.com>
2011-01-03 09:42:00 +0100

    qibuild: cleanup

--- qibuild/__init__.py
+++ qibuild/__init__.py
@@ -1,42 +1,22 @@
-"""Welcome to qibuild project.
+""" Welcome to qibuild project.
 
 Here's what you'll find here:
 
-
-shell:
+shell/
     deals with the command line interface.
 
-    (for instance, after qibuild --verbose cmake --relase foo, call
-    qibuild.actions.cmake with options verbose=True, release=True)
-
-bin/
-    scripts
-
 actions/
-    plugins for qibuild
+    qibuild actions
 
-
-command/
+command
     tools for launching executables
 
-make/
-    tools for building software
-
-cmake/
-    tools for using CMake, CTest and friends
-
-git/
-    tools for using git
+build
+    tools for building software, calling make, cmake, ctest ..
 
 toc/
     Toc Obviously Compiles ! : the Toc object contains a list
     of projects with their configurations
-
-toc/project:
-    a Project is a git repository using CMake as a build system,
-    which depends on other projects
-
-
 """
 
 from . import command
