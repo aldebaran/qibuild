f09a85276852aee5d77e40b2a76187e0a0f57c73
gestes <gestes@aldebaran-robotics.com>
2011-01-03 10:51:06 +0100

    qisrc status: do not show untracked files by default

--- python/qisrc/actions/status.py
+++ python/qisrc/actions/status.py
@@ -19,6 +19,7 @@ LOGGER = logging.getLogger("qisrc.status")
 def configure_parser(parser):
     """Configure parser for this action """
     qitools.qiworktree.work_tree_parser(parser)
+    parser.add_argument("--untracked-files", "-u", dest="untracked_files", action="store_true", help="display untracked files")
     # qitools.cmdparse.action_parser(parser)
     # parser.add_argument("toolchain", action="store", help="the toolchain name")
     # parser.add_argument("feed", nargs='?', action="store", help="an url to a toolchain feed")
@@ -29,21 +30,28 @@ def do(args):
     dirty = list()
     for git_project in qiwt.git_projects.values():
         git = qisrc.git.open(git_project)
-        if git.is_valid() and not git.is_clean():
+        if git.is_valid() and not git.is_clean(untracked=args.untracked_files):
             dirty.append(git_project)
 
     LOGGER.info("Dirty projects: %d/%d", len(dirty), len(qiwt.git_projects))
     for git_project in dirty:
         git = qisrc.git.open(git_project)
-        if git.is_valid() and not git.is_clean():
+        if git.is_valid() and not git.is_clean(untracked=args.untracked_files):
             shortpath = os.path.relpath(git_project, qiwt.work_tree)
             #print "[ %s ]" % shortpath
             print ""
             LOGGER.info("%s : %s tracking %s", shortpath, git.get_current_branch(), git.get_tracking_branch())
-            lines = git.cmd.call_output("status", "-s")
+            if args.untracked_files:
+                lines = git.cmd.call_output("status", "-s")
+            else:
+                lines = git.cmd.call_output("status", "-suno")
+
             nlines = [ x[:3] + shortpath + "/" + x[3:] for x in lines if len(x.strip()) > 0 ]
             print "\n".join(nlines)
-            #git.status("-s", "-uno")
+
+    if not args.untracked_files:
+        print
+        print("Tips: use -u to show untracked files")
 
 if __name__ == "__main__" :
     import sys
