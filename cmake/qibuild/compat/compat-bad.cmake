e0931a75b46ba72deddf98df8c69918cd4ba9082
gestes <gestes@aldebaran-robotics.com>
2011-01-06 11:29:59 +0100

    cmake: compat: remove useless function

--- cmake/qibuild/compat/compat-bad.cmake
+++ cmake/qibuild/compat/compat-bad.cmake
@@ -1,26 +1,8 @@
 ##
-## compat-bad.cmake
-## Login : <ctaf42@cgestes-de2>
-## Started on  Wed Jan  5 12:16:56 2011 Cedric GESTES
-## $Id$
-##
 ## Author(s):
-##  - Cedric GESTES <gestes@aldebaran-robotics.com>
+##  - Cedric GESTES <cgestes@aldebaran-robotics.com>
 ##
-## Copyright (C) 2011 Cedric GESTES
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program; if not, write to the Free Software
-## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+## Copyright (C) 2009, 2010, 2011 Cedric GESTES
 ##
 
 function(_try_install_dir _dir)
@@ -179,16 +161,4 @@ function(generate_revision_header _name _revision)
   configure_file("${T001CHAIN_DIR}/cmake/templates/revision.in"
                  "${CMAKE_CURRENT_BINARY_DIR}/include/${_name}_revision.h" @ONLY)
   include_directories("${CMAKE_CURRENT_BINARY_DIR}/include")
-endfunction()
-
-function(set_cpack_version_from_git MODULENAME)
-  string(REGEX MATCH "v?([0-9]*\\.[0-9]*)\\.([0-9]*(-rc[0-9]+)?)[\\.\\-]?(.*)" _out ${${MODULENAME}_REVISION})
-  # CMAKE_MATCH_0 contains the whole match
-  # CMAKE_MATCH_3 optionaly contains the [rct] part
-  if (CMAKE_MATCH_4 STREQUAL "")
-    set(CMAKE_MATCH_4 "0")
-  endif()
-  set(CPACK_PACKAGE_VERSION_MAJOR ${CMAKE_MATCH_1} PARENT_SCOPE)
-  set(CPACK_PACKAGE_VERSION_MINOR ${CMAKE_MATCH_2} PARENT_SCOPE)
-  set(CPACK_PACKAGE_VERSION_PATCH ${CMAKE_MATCH_4} PARENT_SCOPE)
 endfunction()
