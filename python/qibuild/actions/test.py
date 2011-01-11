c425cff172f478223dfd7548ecad4b5431692ce2
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-01-11 11:39:10 +0100

    Fix qibuild test
     - No need to resolve deps here, you just want to run tests on ONE project.
    
     - Use "project_name" and "project" instead of "project" and "p"
    (grrrr.)

--- python/qibuild/actions/test.py
+++ python/qibuild/actions/test.py
@@ -17,21 +17,16 @@ def configure_parser(parser):
     """Configure parser for this action"""
     qibuild.parsers.toc_parser(parser)
     qibuild.parsers.build_parser(parser)
-    qibuild.parsers.project_parser(parser)
+    parser.add_argument("project_name", metavar="PROJECT")
 
 def do(args):
     """Main entry point"""
     logger   = logging.getLogger(__name__)
     toc      = qibuild.toc.open(args.work_tree, args, use_env=True)
 
-    wanted_projects = qibuild.toc.get_projects_from_args(toc, args)
-    (src_projects, bin_projects, not_found_projects) = toc.split_sources_and_binaries(wanted_projects)
-
-    for project in src_projects:
-        p = toc.projects[project]
-        logger.info("Testing %s in %s", project, toc.build_folder_name)
-        logger.debug("%s", p)
-        qibuild.ctest(p.directory, p.build_directory)
+    project = toc.projects[args.project_name]
+    logger.info("Testing %s in %s", project.name, toc.build_folder_name)
+    qibuild.ctest(project.directory, project.build_directory)
 
 if __name__ == "__main__":
     import sys
