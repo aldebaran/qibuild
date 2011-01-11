7b30f02ce38dbd553573e71af070b476929d4d9f
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-01-11 11:00:42 +0100

    qibuild bdist: generate bdist in QI_WORK_TREE/bdist
    makes more sense here than in /tmp.

--- python/qibuild/actions/bdist.py
+++ python/qibuild/actions/bdist.py
@@ -22,12 +22,13 @@ def do(args):
     toc      = qibuild.toc.open(args.work_tree, args, use_env=True)
     wanted_projects = qibuild.toc.get_projects_from_args(toc, args)
     (src_projects, bin_projects, not_found_projects) = toc.split_sources_and_binaries(wanted_projects)
+    inst_dir = os.path.join(toc.work_tree, "bdist")
     for project_name in wanted_projects:
         project = toc.projects[project_name]
-        logger.info("Generating bin sdk for %s in /tmp", project_name)
+        logger.info("Generating bin sdk for %s in %s", project_name, inst_dir)
         qibuild.project.configure(project)
         qibuild.project.make(project, "release")
-        destdir = os.path.join("/tmp", project_name)
+        destdir = os.path.join(inst_dir, project_name)
         qibuild.project.install(project, destdir)
         archive = qitools.archive.zip_unix(destdir)
         logger.info("Archive generated in %s", archive)
