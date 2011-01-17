6f4b221162b8114a8d2bafcb551565590cd64c59
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-01-17 11:35:57 +0100

    cmake: change layout if using visual studio

--- cmake/qibuild/internal/layout.cmake
+++ cmake/qibuild/internal/layout.cmake
@@ -9,14 +9,21 @@
 #between _SDK_LIB and _SDK_CMAKE_MODULES, name could be change, but
 #the relative path between each var should remain the same.
 
-qi_set_global(QI_SDK_BIN            "bin"              )
-qi_set_global(QI_SDK_LIB            "lib"              )
-qi_set_global(QI_SDK_FRAMEWORK      "Frameworks"       )
-qi_set_global(QI_SDK_INCLUDE        "include"          )
-qi_set_global(QI_SDK_SHARE          "share"            )
-qi_set_global(QI_SDK_CONF           "etc"              )
-qi_set_global(QI_SDK_DOC            "share/doc"        )
-qi_set_global(QI_SDK_CMAKE          "share/cmake"      )
+if(MSVC)
+  message(STATUS "We love visual studio")
+  qi_set_global(QI_SDK_BIN "")
+  qi_set_global(QI_SDK_LIB "")
+else()
+  qi_set_global(QI_SDK_BIN "bin")
+  qi_set_global(QI_SDK_LIB "lib")
+endif()
+
+qi_set_global(QI_SDK_FRAMEWORK      "Frameworks" )
+qi_set_global(QI_SDK_INCLUDE        "include"    )
+qi_set_global(QI_SDK_SHARE          "share"      )
+qi_set_global(QI_SDK_CONF           "etc"        )
+qi_set_global(QI_SDK_DOC            "share/doc"  )
+qi_set_global(QI_SDK_CMAKE          "share/cmake")
 qi_set_global(QI_SDK_CMAKE_MODULES  "cmake")
 
 mark_as_advanced(QI_SDK_BIN
