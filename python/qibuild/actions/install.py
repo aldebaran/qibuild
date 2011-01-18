5aafe125d270ae5795da4151af104af618d388c8
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-01-18 14:52:47 +0100

    qibuild install: start to fix a few things
    TODO: have foo.pkg and foo-dev.pkg ?

--- python/qibuild/actions/install.py
+++ python/qibuild/actions/install.py
@@ -1,14 +1,17 @@
 """Install a project """
 
+import os
 import logging
 import qibuild
 
-def install_project(args, project):
-    r"""Build a project.
-
-    /!\ assumes that set_build_configs has been called
+def run_time_install(package_src, destdir):
     """
+
+    """
+    # FIXME: implement :)
     pass
+
+
 
 def configure_parser(parser):
     """Configure parser for this action"""
@@ -26,9 +29,15 @@ def do(args):
     logger = logging.getLogger(__name__)
     toc      = qibuild.toc.open(args.work_tree, args, use_env=True)
 
-    (project_names, _, _) = qibuild.toc.resolve_deps(toc, args, runtime=True)
+    (project_names, package_names, _) = qibuild.toc.resolve_deps(toc, args, runtime=True)
 
+    print project_names, package_names
     logger.info("Installing %s to %s", ", ".join([n for n in project_names]), args.destdir)
     for project_name in project_names:
         project = toc.get_project(project_name)
         toc.install_project(project,  args.destdir)
+
+    for package_name in package_names:
+        package_src = toc.toolchain.get(package_name)
+        run_time_install(package_src, args.destdir)
+
