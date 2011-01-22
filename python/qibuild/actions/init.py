d7e6370a9fccd5a41b13191f0ad1282b1cd24255
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-01-22 17:07:42 +0100

    qibuild init : now with --interactive action
    Trying to have a smart wizard :)
    
    Helps a lot with qibuild installation/configuration
    for newbies

--- python/qibuild/actions/init.py
+++ python/qibuild/actions/init.py
@@ -1,18 +1,185 @@
 """Init a new toc workspace """
 
 import os
+import subprocess
 import qibuild
 import qitools.cmdparse
+
+
+def ask_choice(choices, input_text):
+    """Ask the user to choose from a list of choices
+
+    """
+    print "::", input_text
+    for i, choice in enumerate(choices):
+        print "  ", (i+1), choice
+    keep_asking = True
+    res = None
+    while keep_asking:
+        answer = raw_input("> ")
+        if not answer:
+            return choices[0]
+        try:
+            index = int(answer)
+        except ValueError:
+            print "Please enter number"
+            continue
+        if index not in range(1, len(choices)+1):
+            print "%i is out of range" % index
+            continue
+        res = choices[index-1]
+        keep_asking = False
+
+    return res
+
+def ask_yes_no(question):
+    """Ask the user to answer by yes or no"""
+    print "::", question, "(y/n)?"
+    anwer = raw_input("> ")
+    return anwer == "y"
+
+def ask_cmake_generator():
+    """Ask the user to choose a cmake generator """
+    cmake_process = subprocess.Popen(["cmake"], stdout=subprocess.PIPE)
+    (out, _err) = cmake_process.communicate()
+    lines = out.splitlines()
+    interesting_lines = list()
+    interesting = False
+    for line in lines:
+        if line == "Generators":
+            interesting = True
+            continue
+        if interesting and line:
+           interesting_lines.append(line)
+
+    generators = list()
+    for line in interesting_lines:
+        if " = " in line:
+            generator = line.split("=")[0]
+            generator = generator.strip()
+            if generator:
+                generators.append(generator)
+
+    generator = ask_choice(generators, "Choose a CMake generator:")
+    return generator
+
+
+def ask_toolchain():
+    """Ask the user to choose a toolchain name"""
+    print(":: Choose a toolchain name")
+    toolchain_name = raw_input("> ")
+    return toolchain_name
+
+def ask_build_configs():
+    """Ask the user to choose build configurations"""
+    keep_going = True
+    build_configs = dict()
+    while keep_going:
+        print ":: Choose a build configuration name: "
+        build_config_name = raw_input("> ")
+        if not build_config_name:
+            keep_going = False
+            continue
+        build_configs[build_config_name] = list()
+        keep_asking_flags=True
+        while keep_asking_flags:
+            print ":: Flags for %s build config (name=value)" % build_config_name
+            flags = raw_input("> ")
+            if not flags:
+                keep_asking_flags = False
+                continue
+            if not "=" in flags:
+                print "invalid flags"
+                continue
+            splitted = flags.split("=")
+            if len(splitted) != 2:
+                print "invalid flags"
+                continue
+            build_configs[build_config_name].append(flags)
+            if ask_yes_no("Done with %s build config" % build_config_name):
+                keep_asking_flags = False
+            continue
+
+        keep_going = ask_yes_no("Add a new build config")
+
+    return build_configs
+
+def ask_path():
+    """Ask the user to choose paths to be added to
+    os.environ[PATH]
+
+    """
+    paths = list()
+    keep_going = True
+    while keep_going:
+        print ":: Select a new path to be added to os.environ"
+        path = raw_input("> ")
+        if not path:
+            keep_going = False
+            continue
+        paths.append(path)
+        keep_going = ask_yes_no("A a new path to os.environ")
+    # configuration file expects paths separated pathsep
+    return os.path.pathsep.join(paths)
+
+def ask_default_build_config(build_configs):
+    """Ask the user to choose a default build config"""
+    return ask_choice(build_configs.keys(),
+        "Select a default build config")
+
+def ask_bat_file():
+    """Ask the user to choose the path to a .bat file
+
+    """
+    pass
 
 def configure_parser(parser):
     """Configure parser for this action """
     qibuild.parsers.toc_parser(parser)
+    parser.add_argument("--interactive", action="store_true",
+        help="start a wizard to help you configuring qibuild")
 
 def do(args):
     """Main entry point"""
     work_tree = qitools.qiworktree.worktree_from_args(args)
+    qiworktree = qitools.qiworktree_open(work_tree)
     qibuild.toc.create(work_tree, args)
 
-if __name__ == "__main__" :
-    import sys
-    qitools.cmdparse.sub_command_main(sys.modules[__name__])
+    if not args.interactive:
+        return
+
+    cmake_generator = ask_cmake_generator()
+
+    default_build_config = None
+    build_configs = dict()
+    if ask_yes_no("Define custom build configurations"):
+        build_configs  = ask_build_configs()
+    if build_configs:
+        default_build_config = ask_default_build_config(build_configs)
+
+    toolchain_name = None
+    if ask_yes_no("Use a toolchain"):
+        toolchain_name  = ask_toolchain()
+
+    env_path = ""
+    bat_file = ""
+    if ask_yes_no("Use custom environment"):
+        env_path       = ask_path()
+        if "Visual Studio" in cmake_generator:
+            bat_file = ask_bat_file()
+
+    qiworktree.update_config(
+        "general", "build", "cmake_generator", cmake_generator)
+    qiworktree.update_config(
+        "general", "build", "toolchain", toolchain_name)
+    qiworktree.update_config(
+        "general", "env", "path", env_path)
+    if build_configs:
+        for name, flags in build_configs.iteritems():
+            qiworktree.update_config(
+            "build", name, "cmake.flags", flags)
+        qiworktree.update_config(
+            "general", "build", "build_config", default_build_config)
+    if bat_file:
+        qiworktree.update_config(
+            "general", "env", "bat_file", bat_file)
