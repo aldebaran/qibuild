9be06514b1de5028bc0252955af2bab745e61df3
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-01-27 14:31:13 +0100

    qibuild/tests: make it work on windows

--- python/qibuild/test/world/world/world.cpp
+++ python/qibuild/test/world/world/world.cpp
@@ -1,18 +1,7 @@
-#include "world.h"
+#include "world/world.h"
 
 
 int get_answer_to_the_question_of_life_universe_and_everything() {
-  return _this_is_a_non_exported_function();
+  return 42;
 }
 
-int _this_is_a_non_exported_function() {
-    return 42;
-}
-
-bool is_cross_compiled() {
-#ifdef CROSS_COMPILING
-  return true;
-#else
-  return false;
-#endif
-}
