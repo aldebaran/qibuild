11f07ec90e04fa9842de9e2bd61e5f3636c1c80e
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-02-03 15:19:10 +0100

    qitoolchain update: can be used without specifying toolchain name

--- python/qitoolchain/actions/update.py
+++ python/qitoolchain/actions/update.py
@@ -7,16 +7,16 @@
 ##
 
 """ Update a toolchain
-    This will try to update every package already found in the toolchain
-"""
+    This will try to update every package already found in the toolchain """
 
 import qitools
 import qitoolchain
 
 def configure_parser(parser):
     """Configure parser for this action """
-    qitools.cmdparse.default_parser(parser)
-    parser.add_argument("toolchain_name", metavar="TOOLCHAIN NAME", action="store", help="the toolchain name")
+    qitools.qiworktree.work_tree_parser(parser)
+    parser.add_argument("toolchain_name", nargs='?', metavar="TOOLCHAIN NAME",
+        action="store", help="the toolchain name")
     parser.add_argument("--all", action="store_true", help="get all the projects known by the feed")
     parser.add_argument("toolchain_feed", nargs='?', metavar="FEED URL", action="store",
         help="an url to a toolchain feed. If not given, the previous feed will be used. "
@@ -25,7 +25,18 @@ def configure_parser(parser):
 
 def do(args):
     """Main method """
-    toolchain_name = args.toolchain_name
+    toolchain_name = None
+    if args.toolchain_name:
+        toolchain_name = args.toolchain_name
+    else:
+        qiwt = qitools.qiworktree_open(args.work_tree, use_env=True)
+        tc_from_conf = qiwt.configstore.get("general", "build", "toolchain")
+        if not tc_from_conf:
+            mess  = "Could not find default toolchain name in configuration.\n"
+            mess += "Try specifying a toolchain name on the command line"
+            raise Exception(mess)
+        toolchain_name = tc_from_conf
+
     toolchain_feed = args.toolchain_feed
     toolchain = qitoolchain.Toolchain(toolchain_name)
     toolchain.update(toolchain_feed, all=args.all)
