9ebd77bd46760957ae50a570663f5cd2ea4d742f
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-02-10 11:52:04 +0100

    Install-qibuild: 5th version
    This one also works from a git bash using a windows python
    (window's python.exe knows nothing about /usr/local ...,
    so we have to write the install-qibuild.sh in bash):wq

--- install-qibuild.sh
+++ install-qibuild.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-python tools/install-qibuild.py --posix
+python tools/install-qibuild.py
+
+mkdir -p /usr/local/bin
+
+function install_wrapper {
+  name=$1
+  script_path=$(which ${name}.py)
+  ln -sf $script_path /usr/local/bin/$1
+}
+
+install_wrapper qitoolchain
+install_wrapper qibuild
+install_wrapper qisrc
+
--- tools/install-qibuild.py
+++ tools/install-qibuild.py
@@ -17,7 +17,7 @@ def chmod_755(path):
         stat.S_IRGRP | stat.S_IXGRP |
         stat.S_IROTH | stat.S_IXOTH)
 
-def install(script_name, on_win=False):
+def install(script_name):
     python_path = os.path.join(CUR_DIR, "..", "python")
     python_path = os.path.abspath(python_path)
     script_path = os.path.join(python_path, "bin", script_name)
@@ -27,7 +27,7 @@ def install(script_name, on_win=False):
     script_contents = script_contents.replace("# sys.path", """import sys
 sys.path.insert(0, r"%s")""" % python_path)
 
-    if on_win:
+    if sys.platform.startswith("win"):
         dest_dir = os.path.join(os.path.dirname(sys.executable), "Scripts")
     else:
         dest_dir = "/usr/local/bin"
@@ -39,20 +39,15 @@ sys.path.insert(0, r"%s")""" % python_path)
     print "Script installed in ", out_path
 
     chmod_755(out_path)
-    if not on_win:
-        without_ext = out_path[:-3]
-        if os.path.exists(without_ext):
-            os.remove(without_ext)
-        os.link(out_path, without_ext)
 
 if __name__ == "__main__":
     if "--windows" in sys.argv:
         on_win = True
     else:
         on_win = False
-    install("qitoolchain", on_win=on_win)
-    install("qibuild", on_win=on_win)
-    install("qisrc", on_win=on_win)
+    install("qitoolchain")
+    install("qibuild")
+    install("qisrc")
 
 
 
