77ee073524888dbf913ed8655f06ecb967a548f4
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-02-11 16:26:38 +0100

    cmake: boost : we are in  boost 1.44 now !

--- cmake/qibuild/modules/boostutils.cmake
+++ cmake/qibuild/modules/boostutils.cmake
@@ -3,15 +3,15 @@
 function(boost_flib _suffix _libname)
   flib(BOOST_${_suffix} OPTIMIZED NAMES
                           "boost_${_libname}-mt"
-                          "boost_${_libname}-xgcc40-mt-1_38"
-                          "libboost_${_libname}-vc80-mt-1_38"
-                          "libboost_${_libname}-vc90-mt-1_38"
+                          "boost_${_libname}-xgcc40-mt-1_44"
+                          "libboost_${_libname}-vc80-mt-1_44"
+                          "libboost_${_libname}-vc90-mt-1_44"
                           )
   flib(BOOST_${_suffix} DEBUG NAMES
                           "boost_${_libname}-mt-d"
-                          "boost_${_libname}-xgcc40-mt-1_38"
-                          "libboost_${_libname}-vc80-mt-gd-1_38"
-                          "libboost_${_libname}-vc90-mt-gd-1_38"
+                          "boost_${_libname}-xgcc40-mt-1_44"
+                          "libboost_${_libname}-vc80-mt-gd-1_44"
+                          "libboost_${_libname}-vc90-mt-gd-1_44"
                           "boost_${_libname}-mt"
                           )
 endfunction()
@@ -19,15 +19,15 @@ endfunction()
 function(boost_flib_static _suffix _libname)
   flib(BOOST_${_suffix} OPTIMIZED PATH_SUFFIXES static NAMES
                           "boost_${_libname}-mt"
-                          "boost_${_libname}-xgcc40-mt-1_38"
-                          "libboost_${_libname}-vc80-mt-1_38"
-                          "libboost_${_libname}-vc90-mt-1_38"
+                          "boost_${_libname}-xgcc40-mt-1_44"
+                          "libboost_${_libname}-vc80-mt-1_44"
+                          "libboost_${_libname}-vc90-mt-1_44"
                           )
   flib(BOOST_${_suffix} DEBUG PATH_SUFFIXES static NAMES
                           "boost_${_libname}-mt-d"
-                          "boost_${_libname}-xgcc40-mt-1_38"
-                          "libboost_${_libname}-vc80-mt-gd-1_38"
-                          "libboost_${_libname}-vc90-mt-gd-1_38"
+                          "boost_${_libname}-xgcc40-mt-1_44"
+                          "libboost_${_libname}-vc80-mt-gd-1_44"
+                          "libboost_${_libname}-vc90-mt-gd-1_44"
                           "boost_${_libname}-mt"
                           )
 endfunction()
