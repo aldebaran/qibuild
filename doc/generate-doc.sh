6a445b707b6558ec7adf3b8db5aeb94f3c07d7f8
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-02-15 19:34:21 +0100

    asciidoc is *really* stupid about css ...
    Make Obi api :)

--- doc/generate-doc.sh
+++ doc/generate-doc.sh
@@ -33,7 +33,7 @@ PYTHONPATH=python python doc/tools/generate_manpage_from_qibuild.py doc/qitoolch
 find build-doc/ -type f -name '*.txt' | while read f ; do
   #asciidoc is stupid about css...
   #we desactivated default theme, set the stylesheet to bare.css, and disable embedded css
-  asciidoc.py -a toc -a toclevels=1  -a linkcss -a 'theme=' -a stylesheet="bare.css" -a pygments "$f"
+  asciidoc.py -a toc -a toclevels=1  -a linkcss -a 'theme=bare' -a stylesheet="bare.css" -a pygments "$f"
 done
 
 doc/tools/generate_examples_archive python/qibuild/test/ "build-doc"
