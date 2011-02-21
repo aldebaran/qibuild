d704ad883034c044e07fd76a4139e4108b3cd358
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-02-21 10:42:39 +0100

    qibuild: remove unused code

--- python/qibuild/__init__.py
+++ python/qibuild/__init__.py
@@ -125,28 +125,7 @@ def cmake(source_dir, build_dir, cmake_args):
     cmake_args += [source_dir]
     qitools.command.check_call(["cmake"] + cmake_args, cwd=build_dir)
 
-def ctest(source_dir, build_dir):
-    """
-    Just run test using ctest.
-    source_dir should contain CMakeLists.txt
-    build_dir must have been configured (cmake) and built.
 
-    """
-    cmd = ["ctest", "-VV", "-DExperimental", source_dir ]
-    # Check whether source_dir and build_dir look valid.
-    # TODO: Move these checks in a common place?
-    if not os.path.exists(source_dir):
-        raise Exception("source dir: %s does not exist, aborting" % \
-                             source_dir)
-    if not os.path.exists(os.path.join(source_dir, "CMakeLists.txt")):
-        raise Exception("source dir: %s does not contain CMakeLists,"\
-                             " aborting" % source_dir)
-    if not os.path.exists(build_dir):
-        raise Exception("build dir: %s does not exist, aborting" % \
-                             build_dir)
-    if not os.path.exists(os.path.join(build_dir, "CMakeCache.txt")):
-        raise Exception("build dir: %s does not contain "\
-                             "CMakeCache.txt, aborting" % build_dir)
 def _add_missing_project_call(cmake_list_file, project_name):
     """Patch a CMakeLists.txt to add missing call to project() """
     # Check that the root CMakeLists contains a project() call
