4976ced79e064b8476b04026bd03059b39a6e2f8
ckilner <ckilner@aldebaran-robotics.com>
2011-02-23 18:55:33 +0100

    fix boostutils

--- cmake/qibuild/modules/boostutils.cmake
+++ cmake/qibuild/modules/boostutils.cmake
@@ -1,33 +1,48 @@
 ## Copyright (C) 2011 Aldebaran Robotics
 
 function(boost_flib _suffix _libname)
+  SET (BOOST_VERSION 1_44)
   flib(BOOST_${_suffix} OPTIMIZED NAMES
-                          "boost_${_libname}-mt"
-                          "boost_${_libname}-xgcc40-mt-1_44"
-                          "libboost_${_libname}-vc80-mt-1_44"
-                          "libboost_${_libname}-vc90-mt-1_44"
-                          )
+    # windows dynamic
+    "boost_${_libname}-vc80-mt-${BOOST_VERSION}"
+    "boost_${_libname}-vc90-mt-${BOOST_VERSION}"
+    "boost_${_libname}-vc100-mt-${BOOST_VERSION}"
+    # windows static
+    "libboost_${_libname}-vc80-mt-${BOOST_VERSION}"
+    "libboost_${_libname}-vc90-mt-${BOOST_VERSION}"
+    "libboost_${_libname}-vc100-mt-${BOOST_VERSION}"
+    
+    "boost_${_libname}-xgcc40-mt-${BOOST_VERSION}"
+    "boost_${_libname}-mt"
+  )
   flib(BOOST_${_suffix} DEBUG NAMES
-                          "boost_${_libname}-mt-d"
-                          "boost_${_libname}-xgcc40-mt-1_44"
-                          "libboost_${_libname}-vc80-mt-gd-1_44"
-                          "libboost_${_libname}-vc90-mt-gd-1_44"
-                          "boost_${_libname}-mt"
-                          )
-endfunction()
+    # windows dynamic
+    "boost_${_libname}-vc80-mt-gd-${BOOST_VERSION}"
+    "boost_${_libname}-vc90-mt-gd-${BOOST_VERSION}"
+    "boost_${_libname}-vc100-mt-gd-${BOOST_VERSION}"
+    # windows static
+    "libboost_${_libname}-vc80-mt-gd-${BOOST_VERSION}"
+    "libboost_${_libname}-vc90-mt-gd-${BOOST_VERSION}"
+    "libboost_${_libname}-vc100-mt-gd-${BOOST_VERSION}"
+    
+    "boost_${_libname}-xgcc40-mt-${BOOST_VERSION}"
+    "boost_${_libname}-mt"
+  )
+endfunction(boost_flib _suffix _libname)
 
 function(boost_flib_static _suffix _libname)
+  SET (BOOST_VERSION 1_44)
   flib(BOOST_${_suffix} OPTIMIZED PATH_SUFFIXES static NAMES
                           "boost_${_libname}-mt"
-                          "boost_${_libname}-xgcc40-mt-1_44"
-                          "libboost_${_libname}-vc80-mt-1_44"
-                          "libboost_${_libname}-vc90-mt-1_44"
+                          "boost_${_libname}-xgcc40-mt-${BOOST_VERSION}"
+                          "libboost_${_libname}-vc80-mt-${BOOST_VERSION}"
+                          "libboost_${_libname}-vc90-mt-${BOOST_VERSION}"
                           )
   flib(BOOST_${_suffix} DEBUG PATH_SUFFIXES static NAMES
                           "boost_${_libname}-mt-d"
-                          "boost_${_libname}-xgcc40-mt-1_44"
-                          "libboost_${_libname}-vc80-mt-gd-1_44"
-                          "libboost_${_libname}-vc90-mt-gd-1_44"
+                          "boost_${_libname}-xgcc40-mt-1_38-${BOOST_VERSION}"
+                          "libboost_${_libname}-vc80-mt-gd-1_38-${BOOST_VERSION}"
+                          "libboost_${_libname}-vc90-mt-gd-1_38-${BOOST_VERSION}"
                           "boost_${_libname}-mt"
                           )
 endfunction()
