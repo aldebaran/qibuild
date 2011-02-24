884fd46d58068f555760cd8df4bc9ef42d0506ab
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-02-24 12:48:25 +0100

    qibuild package: generate package inside build directory
    (safer this way)

--- python/qibuild/actions/package.py
+++ python/qibuild/actions/package.py
@@ -13,35 +13,37 @@ def configure_parser(parser):
     """Configure parser for this action"""
     qibuild.parsers.toc_parser(parser)
     qibuild.parsers.build_parser(parser)
-    parser.add_argument("project_name")
+    parser.add_argument("project", nargs="?")
     parser.set_defaults(
         cmake_flags=["CMAKE_INSTALL_PREFIX='/'"])
 
-def _do(args, project_name, destdir, build_type):
+def _do(args, build_type):
     """Called by do().
     Once with build_type=release on UNIX.
     Twice with build_type=debug and build_type=release on windows
 
+    Returns the directory to make an archive from
     """
     args.build_type = build_type
     toc      = qibuild.toc_open(args.work_tree, args, use_env=True)
+    if not args.project:
+        project_name = qibuild.toc.project_from_cwd()
+    else:
+        project_name = args.project
     project = toc.get_project(project_name)
+    inst_dir = os.path.join(project.build_directory, "package")
+    destdir = os.path.join(inst_dir, project_name)
     toc.package_project(project, destdir)
+    return destdir
 
 def do(args):
     """Main entry point"""
-    logger   = logging.getLogger(__name__)
-    project_name = args.project_name
-    work_tree = qitools.qiworktree.worktree_from_args(args)
-    inst_dir = os.path.join(work_tree, "package")
-    destdir = os.path.join(inst_dir, project_name)
-    logger.info("Generating bin sdk for %s in %s", project_name, inst_dir)
-
     if sys.platform.startswith("win"):
-        _do(args, project_name, destdir, "debug")
-    _do(args, project_name, destdir, "release")
+        _do(args, "debug")
+    destdir = _do(args, "release")
 
     archive = qitools.archive.zip(destdir)
-    logger.info("Archive generated in %s", archive)
+    logger   = logging.getLogger(__name__)
+    logger.info("Package generated in %s", archive)
 
 
