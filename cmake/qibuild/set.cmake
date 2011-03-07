630a568105cdb59ed8d796f27a26960e2d69e1bf
gestes <gestes@aldebaran-robotics.com>
2011-03-07 19:00:10 +0100

    cmake: set: document function

--- cmake/qibuild/set.cmake
+++ cmake/qibuild/set.cmake
@@ -1,13 +1,16 @@
 ## Copyright (C) 2011 Aldebaran Robotics
 
+# write the variable into cache, variable wont be visible
 macro(qi_set_global name)
   set("${name}" ${ARGN} CACHE INTERNAL "" FORCE)
 endmacro()
 
+# write the variable into cache
 macro(qi_set_cache name)
   set("${name}" ${ARGN} CACHE STRING "" FORCE)
 endmacro()
 
+# write the variable into cache, mark as an advanced variable
 macro(qi_set_advanced_cache name)
   set("${name}" ${ARGN} CACHE STRING "" FORCE)
   mark_as_advanced("${name}")
