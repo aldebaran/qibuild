661e740204295c55b3695b002ef2b2252459e223
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-03-10 11:17:50 +0100

    cmake: add internal/list.cmake for list manipulation
    Those are always tricky

---
+++ cmake/qibuild/internal/list.cmake
@@ -0,0 +1,25 @@
+# Add a value to a list, keeping the
+# list sorted, and without duplicate
+function(_qi_list_append_uniq _list _value)
+
+  list(APPEND ${_list} ${_value})
+  list(SORT ${_list})
+  list(REMOVE_DUPLICATES ${_list})
+  set(${_list} ${${_list}} PARENT_SCOPE)
+endfunction()
+
+# Add a value to a list of paths,
+# keeping the list sorted, whithout
+# duplicates and only canonical paths
+function(_qi_list_append_path _list _path)
+  get_filename_component(_abs_path ${_path} REALPATH)
+  file(TO_CMAKE_PATH ${_abs_path} _abs_path)
+  _qi_list_append_uniq(${_list} ${_abs_path})
+  set(${_list} ${${_list}} PARENT_SCOPE)
+endfunction()
+
+
+function(_qi_list_append_cache _name _value)
+  _qi_list_append_uniq(${_name} ${_value})
+  set(${_name} ${${_name}} CACHE INTERNAL "" FORCE)
+endfunction()
