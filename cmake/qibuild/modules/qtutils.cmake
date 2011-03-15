2cedaa9985fef20c8a22132499531ce04ea7467a
gestes <gestes@aldebaran-robotics.com>
2011-03-15 19:20:07 +0100

    qt: path suffixes "qt4" for libs too

--- cmake/qibuild/modules/qtutils.cmake
+++ cmake/qibuild/modules/qtutils.cmake
@@ -8,8 +8,8 @@
 # and include/QtCore
 #
 function(qt_flib _suffix _libame)
-   flib(QT_${_suffix} OPTIMIZED NAMES "${_libame}" "${_libame}4")
-   flib(QT_${_suffix} DEBUG     NAMES "${_libame}" "${_libame}d4")
+   flib(QT_${_suffix} OPTIMIZED NAMES "${_libame}" "${_libame}4"  PATH_SUFFIXES qt4)
+   flib(QT_${_suffix} DEBUG     NAMES "${_libame}" "${_libame}d4" PATH_SUFFIXES qt4)
 
    #we want to be able to #include <QtLib>
    fpath(QT_${_suffix} ${_libame} PATH_SUFFIXES qt4 )
