4a826793b8b6b8abdafe5e274157249c50f30889
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-03-15 15:49:25 +0100

    bootstrap.cmake version 4
    enable cross compiling with qibuild based ctc

--- cmake/qibuild/templates/bootstrap.cmake
+++ cmake/qibuild/templates/bootstrap.cmake
@@ -8,22 +8,31 @@
 # This file is part of the T001CHAIN project  #
 ###############################################
 
-set(BOOTSTRAP_VERSION 3)
+set(BOOTSTRAP_VERSION 4)
+
+# Function to use qibuild from a old toc project
+function(use_qibuild)
+  if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/qibuild.cmake)
+    include(${CMAKE_CURRENT_SOURCE_DIR}/qibuild.cmake)
+
+    set(NO_WARN_DEPRECATED TRUE PARENT_SCOPE)
+    include(qibuild/compat/compat)
+  endif()
+endfunction()
 
 if (CMAKE_TOOLCHAIN_FILE)
   # a toolchain which defines TOOLCHAIN_DIR -> toc
   if(TOOLCHAIN_DIR)
-    set(T001CHAIN_DIR ${TOOLCHAIN_DIR} CACHE PATH "" FORCE)
-    include("${T001CHAIN_DIR}/cmake/general.cmake")
-    return()
+    # toc --cross -> qibuild (ctc uses qibuild now)
+    if(OE_CROSS_DIR)
+      use_qibuild()
+      return()
+    else()
+      set(T001CHAIN_DIR ${TOOLCHAIN_DIR} CACHE PATH "" FORCE)
+      include("${T001CHAIN_DIR}/cmake/general.cmake")
+      return()
+    endif()
   endif()
 endif()
 
-# else, simply includes qibuild.cmake if it is there:
-if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/qibuild.cmake)
-  include(${CMAKE_CURRENT_SOURCE_DIR}/qibuild.cmake)
-
-  # plus a few stuff:
-  set(NO_WARN_DEPRECATED TRUE)
-  include(qibuild/compat/compat)
-endif()
+use_qibuild()
