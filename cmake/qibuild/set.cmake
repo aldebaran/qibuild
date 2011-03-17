12b19095579efe5895f81ecd7b73708f8cb48fe6
gestes <gestes@aldebaran-robotics.com>
2011-03-17 11:03:47 +0100

    set: added macro qi_append_global and qi_prepend_global

--- cmake/qibuild/set.cmake
+++ cmake/qibuild/set.cmake
@@ -15,3 +15,13 @@ macro(qi_set_advanced_cache name)
   set("${name}" ${ARGN} CACHE STRING "" FORCE)
   mark_as_advanced("${name}")
 endmacro()
+
+macro(qi_append_global _name _value)
+  list(APPEND "${_name}" ${_value} ${ARGN})
+  set("${_name}" "${${_name}}" CACHE INTERNAL "" FORCE)
+endmacro()
+
+macro(qi_prepend_global _name _value)
+  list(INSERT "${_name}" 0 ${_value} ${ARGN})
+  set("${_name}" "${${_name}}" CACHE INTERNAL "" FORCE)
+endmacro()
