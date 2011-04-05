b60b39b1c5bccb0a1479e3d4572813b3cccf17b6
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-04-05 12:25:04 +0200

    Fix finding of glibconfig.h when cross-compiling

--- cmake/qibuild/modules/glib-config.cmake
+++ cmake/qibuild/modules/glib-config.cmake
@@ -1,13 +1,21 @@
 ## Copyright (C) 2011 Aldebaran Robotics
 
+message(STATUS "HERE")
+
 clean(GLIB)
 
 fpath(GLIB glib.h PATH_SUFFIXES glib-2.0)
 
-#glibconfig.h is located in the folder (at least on archlinux and gentoo)
-if (UNIX AND NOT APPLE)
-  set(GLIB_INCLUDE_DIR /usr/lib/glib-2.0/include ${GLIB_INCLUDE_DIR} CACHE PATH "" FORCE)
+# Note:
+# when cross-compiling, you should add
+# <sysroot>/usr/lib/glib-2.0/include in CMAKE_INCLUDE_PATH
+# before getting here.
+# (for instance, you can do this in the cmake toolchain file)
+if(UNIX AND NOT APPLE)
+  list(APPEND CMAKE_INCLUDE_PATH "/usr/lib/glib-2.0/include")
+  fpath(GLIB glibconfig.h)
 endif()
+
 
 flib (GLIB glib-2.0     glib-2.0.0   )
 flib (GLIB gobject-2.0  gobject-2.0.0)
