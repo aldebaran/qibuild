5d7345a3fc4d6a6d79d6825ff55995ac68798874
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-04-12 19:32:05 +0200

    Fix finding boost on mingw

--- cmake/qibuild/modules/boostutils.cmake
+++ cmake/qibuild/modules/boostutils.cmake
@@ -3,14 +3,16 @@
 function(boost_flib _suffix _libname)
   set (BOOST_VERSION 1_44)
   flib(BOOST_${_suffix} OPTIMIZED NAMES
-    # windows dynamic
+    # windows vs dynamic
     "boost_${_libname}-vc80-mt-${BOOST_VERSION}"
     "boost_${_libname}-vc90-mt-${BOOST_VERSION}"
     "boost_${_libname}-vc100-mt-${BOOST_VERSION}"
-    # windows static
+    # windows vs static
     "libboost_${_libname}-vc80-mt-${BOOST_VERSION}"
     "libboost_${_libname}-vc90-mt-${BOOST_VERSION}"
     "libboost_${_libname}-vc100-mt-${BOOST_VERSION}"
+    # mingw
+    "libboost_${_libname}-mgw44-mt-${BOOST_VERSION}"
     # osx
     "boost_${_libname}-xgcc40-mt-${BOOST_VERSION}"
 
@@ -27,6 +29,8 @@ function(boost_flib _suffix _libname)
     "libboost_${_libname}-vc80-mt-gd-${BOOST_VERSION}"
     "libboost_${_libname}-vc90-mt-gd-${BOOST_VERSION}"
     "libboost_${_libname}-vc100-mt-gd-${BOOST_VERSION}"
+    # mingw
+    "libboost_${_libname}-mgw44-mt-d-${BOOST_VERSION}"
 
     # osx
     "boost_${_libname}-xgcc40-mt-${BOOST_VERSION}"
