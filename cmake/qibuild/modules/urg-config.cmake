d083b9d23eafb33289d9882de31262518ad58a78
gestes <gestes@aldebaran-robotics.com>
2011-04-18 13:30:45 +0200

    cmake: module: urg: update to support v0.8.11

--- cmake/qibuild/modules/urg-config.cmake
+++ cmake/qibuild/modules/urg-config.cmake
@@ -1,8 +1,8 @@
 ## Copyright (C) 2011 Aldebaran Robotics
 
 clean(URG)
-fpath(URG "urg_ctrl.h" PATH_SUFFIXES "urg")
-flib(URG "c_connection")
-flib(URG "c_urg")
-flib(URG "c_system")
+fpath(URG "urg_ctrl.h" PATH_SUFFIXES "urg" "c_urg")
+flib(URG "c_connection" NAMES c_urg_connection)
+flib(URG "c_urg" )
+flib(URG "c_system" NAME c_urg_system)
 export_lib(URG)
