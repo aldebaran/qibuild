74f28b52c79b8357776a103559b3ba328a1863b3
Cedric GESTES <gestes@aldebaran-robotics.com>
2011-04-21 16:55:22 +0200

    add boost-locale

---
+++ cmake/qibuild/modules/boost_locale-config.cmake
@@ -0,0 +1,17 @@
+## Copyright (C) 2011 Aldebaran Robotics
+
+#get the root folder of this sdk
+get_filename_component(_ROOT_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
+include("${_ROOT_DIR}/boostutils.cmake")
+
+set(_libname "locale")
+set(_suffix "LOCALE")
+
+clean(BOOST_${_suffix})
+fpath(BOOST_${_suffix} boost/locale.hpp)
+
+boost_flib(${_suffix} ${_libname})
+#boost filesystem use boost_system
+boost_flib(${_suffix} "system")
+
+export_lib(BOOST_${_suffix})
