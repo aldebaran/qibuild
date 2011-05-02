a2e1c3dab18046cd04b262d4befe8e64802c6ad2
jmassot <jmassot@aldebaran-robotics.com>
2011-05-02 15:00:34 +0000

    qibuild: add CMakeList for dbus-glib binding

---
+++ cmake/qibuild/modules/dbus-glib-config.cmake
@@ -0,0 +1,15 @@
+## Copyright (C) 2011 Aldebaran Robotics
+
+clean(DBUS-GLIB)
+
+# Note:
+# when cross-compiling, you should add
+# <sysroot>/usr/lib/glib-2.0/include in CMAKE_INCLUDE_PATH
+# before getting here.
+# (for instance, you can do this in the cmake toolchain file)
+if(UNIX AND NOT APPLE)
+  list(APPEND CMAKE_INCLUDE_PATH "/usr/include/dbus-1.0/dbus")
+  fpath(DBUS-GLIB dbus-glib.h)
+endif()
+flib (DBUS-GLIB dbus-glib-1)
+export_lib(DBUS-GLIB)
