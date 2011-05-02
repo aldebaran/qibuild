f5493b47f09ecd396c28a06e69dc40610b9d1c17
gestes <gestes@aldebaran-robotics.com>
2011-05-02 17:44:54 +0200

    cmake: modules: python/glib/dbus: fpath after if (unix)

--- cmake/qibuild/modules/glib-config.cmake
+++ cmake/qibuild/modules/glib-config.cmake
@@ -4,7 +4,6 @@ message(STATUS "HERE")
 
 clean(GLIB)
 
-fpath(GLIB glib.h PATH_SUFFIXES glib-2.0)
 
 # Note:
 # when cross-compiling, you should add
@@ -13,10 +12,10 @@ fpath(GLIB glib.h PATH_SUFFIXES glib-2.0)
 # (for instance, you can do this in the cmake toolchain file)
 if(UNIX AND NOT APPLE)
   list(APPEND CMAKE_INCLUDE_PATH "/usr/lib/glib-2.0/include")
-  fpath(GLIB glibconfig.h)
 endif()
 
-
+fpath(GLIB glib.h PATH_SUFFIXES glib-2.0)
+fpath(GLIB glibconfig.h)
 flib (GLIB glib-2.0     glib-2.0.0   )
 flib (GLIB gobject-2.0  gobject-2.0.0)
 flib (GLIB gio-2.0      gio-2.0.0    )
