b7c9935779798d63aa838bd526aee8408b1c5830
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-05-10 14:51:48 +0200

    boost cleaning
    The point is: you NEVER want to look for boost_${liname} on debug on windows

--- cmake/qibuild/modules/boostutils.cmake
+++ cmake/qibuild/modules/boostutils.cmake
@@ -1,43 +1,79 @@
 ## Copyright (C) 2011 Aldebaran Robotics
 
+
+
+
 function(boost_flib _suffix _libname)
   set (BOOST_VERSION 1_44)
-  flib(BOOST_${_suffix} OPTIMIZED NAMES
-    # windows vs dynamic
+  set(_linux_names
+    "boost_${_libname}-mt"
+    # for boost-locale < 1.37
+    "boost_${_libname}"
+  )
+
+  set(_osx_names
+    "boost_${_libname}-xgcc40-mt-${BOOST_VERSION}"
+    # for boost-locale < 1.37
+    "boost_${_libname}"
+  )
+
+  set(_vc_names_release
+    # dynamic
     "boost_${_libname}-vc80-mt-${BOOST_VERSION}"
     "boost_${_libname}-vc90-mt-${BOOST_VERSION}"
     "boost_${_libname}-vc100-mt-${BOOST_VERSION}"
-    # windows vs static
+    # for boost-locale < 1.37
+    "boost_${_libname}"
+    # static
     "libboost_${_libname}-vc80-mt-${BOOST_VERSION}"
     "libboost_${_libname}-vc90-mt-${BOOST_VERSION}"
     "libboost_${_libname}-vc100-mt-${BOOST_VERSION}"
-    # mingw
-    "libboost_${_libname}-mgw44-mt-${BOOST_VERSION}"
-    # osx
-    "boost_${_libname}-xgcc40-mt-${BOOST_VERSION}"
-
-    # linux
-    "boost_${_libname}-mt"
-    "boost_${_libname}"
   )
-  flib(BOOST_${_suffix} DEBUG NAMES
-    # windows dynamic
+
+  set(_vc_names_debug
+    # dynamic
     "boost_${_libname}-vc80-mt-gd-${BOOST_VERSION}"
     "boost_${_libname}-vc90-mt-gd-${BOOST_VERSION}"
     "boost_${_libname}-vc100-mt-gd-${BOOST_VERSION}"
-    # windows static
+    # for boost-locale < 1.37
+    "boost_${_libname}_d"
+    # static
     "libboost_${_libname}-vc80-mt-gd-${BOOST_VERSION}"
     "libboost_${_libname}-vc90-mt-gd-${BOOST_VERSION}"
     "libboost_${_libname}-vc100-mt-gd-${BOOST_VERSION}"
-    # mingw
-    "libboost_${_libname}-mgw44-mt-d-${BOOST_VERSION}"
-
-    # osx
-    "boost_${_libname}-xgcc40-mt-${BOOST_VERSION}"
-
-    # linux
-    "boost_${_libname}_d"
-    "boost_${_libname}-mt"
-    "boost_${_libname}"
   )
+
+  set (_mingw_names_debug
+    "libboost_${_libname}-mgw44-mt-d-${BOOST_VERSION}"
+  )
+
+  set (_mingw_names_release
+    "libboost_${_libname}-mgw44-mt-${BOOST_VERSION}"
+  )
+
+  if(WIN32)
+    if(MSCV)
+      # vs
+      flib(BOOST_${_suffix} DEBUG NAMES
+        ${_vc_names_debug}
+      )
+      flib(BOOST_${_suffix} OPTIMIZED NAMES
+        ${_vc_names_release}
+      )
+    else()
+      # mingw
+      flib(BOOST_${_suffix} DEBUG NAMES
+        ${_mingw_names_debug}
+      )
+      flib(BOOST_${_suffix} OPTIMIZED NAMES
+        ${_mingw_names_release}
+      )
+    endif()
+  else() # No win32, no need for debug/release
+    if(APPLE)
+      flib(BOOST_${_suffix} NAMES ${_osx_names})
+    else()
+      flib(BOOST_${_suffix} NAMES ${_osx_names})
+    endif()
+  endif()
 endfunction()
