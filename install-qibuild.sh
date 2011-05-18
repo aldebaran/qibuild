64d22b2552770b9b69925ed7c84f2ec88a260ffb
ALDEBARAN\cgestes <ALDEBARAN\cgestes@bn-mac-po.aldebaran.lan>
2011-05-18 11:33:23 +0200

    qibuild: support local install in ~/bin

--- install-qibuild.sh
+++ install-qibuild.sh
@@ -11,22 +11,33 @@ DESTDIR="/usr/local/bin"
 
 
 create_launcher() {
-  full_path=$1
-  name=$(basename $full_path)
+  full_path="$1"
+  name=$(basename "$full_path")
   if readlink -f . >/dev/null 2>/dev/null ; then
-      p=$(dirname "$(readlink -f $0 2>/dev/null)")
+      p=$(dirname "$(readlink -f '$0' 2>/dev/null)")
   else
       p=$(pwd)
   fi
   #echo "QiBuild directory: $p"
 
-  echo '#!/bin/sh'      >  ${DESTDIR}/${name}
+  echo '#!/bin/sh'      >  "${DESTDIR}/${name}"
   echo "PYTHONPATH=\"$p/python\" python \"${p}/${full_path}\" \"\$@\"" >> ${DESTDIR}/${name}
-  chmod 755 ${DESTDIR}/${name}
+  chmod 755 "${DESTDIR}/${name}"
   echo "installed: ${DESTDIR}/${name}"
 }
 
-mkdir -p ${DESTDIR}
+
+if ! mkdir -p "${DESTDIR}" 2>/dev/null ; then
+  echo WARNING: ${DESTDIR} is not writable
+  echo =====================
+  echo installing into ~/bin
+  echo You should add ~/bin to your PATH
+  echo =====================
+  echo
+  DESTDIR=~/bin
+  mkdir -p "${DESTDIR}"
+fi
+
 create_launcher python/bin/qibuild
 create_launcher python/bin/qitoolchain
 create_launcher python/bin/qisrc
