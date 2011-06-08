b42d262059440309df30ba60dca8621fc56bb8bd
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-06-08 16:28:21 +0200

    qibuild create: always create project at the root of the qiworktree.
    Makes more sense, especially when using --qi-work-tree option.
    
    Should we also add --project-path option and warn if
    project_path is not inside the worktree ?

--- python/qibuild/actions/create.py
+++ python/qibuild/actions/create.py
@@ -33,28 +33,31 @@ def copy_helper(project_name, directory):
 def configure_parser(parser):
     """Configure parser for this action """
     qibuild.qiworktree.work_tree_parser(parser)
-    parser.add_argument("project_path", help="either the name of the project, or  "
-        "a new path for the project")
+    parser.add_argument("project_name",
+        help="The name of the project. "
+             "The project will be created in QI_WORK_TREE/<name> ")
 
 
 def do(args):
     """"Create a new project """
-    project_path = qibuild.sh.to_native_path(args.project_path)
-    project_name = os.path.basename(project_path)
+    # Try to open a qiworktree.
+    # If not, ask the user if he wants to create one:
+    try:
+        qiwt = qibuild.qiworktree_open(args.work_tree, use_env=True)
+    except qibuild.qiworktree.WorkTreeException:
+        if qibuild.interact.ask_yes_no("Warning, no worktree found. Create one"):
+            qibuild.run_action("qibuild.actions.init", ["--interactive"])
+            qiwt = qibuild.qiworktree_open(args.work_tree, use_env=True)
+        else:
+            return
+
+    project_name = args.project_name
+    project_path = os.path.join(qiwt.work_tree, project_name)
     if os.path.exists(project_path):
         raise Exception("%s already exists" % project_path)
     os.mkdir(project_path)
     copy_helper(project_name, project_path)
 
     LOGGER.info("New project initialized in %s", project_path)
-
-    # Try to open a qiworktree.
-    # If not, ask the user if he wants to create one:
-    try:
-        qibuild.qiworktree_open(args.work_tree, use_env=True)
-    except qibuild.qiworktree.WorkTreeException:
-        if qibuild.interact.ask_yes_no("Warning, no worktree found. Create one"):
-            qibuild.run_action("qibuild.actions.init", ["--interactive"])
-
 
 
