b4ddae6714761d81a6c1ee89295bae916f8c3eea
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-06-15 11:06:20 +0200

    cmake: fix qi_add_optional_package : the WITH_ option was always off
    after option(), the cmake variable IS defined ...

--- cmake/qibuild/option.cmake
+++ cmake/qibuild/option.cmake
@@ -1,6 +1,7 @@
 ## Copyright (C) 2011 Aldebaran Robotics
 
 
+
 include(CMakeDependentOption)
 
 #! Add optional dependency to a package
@@ -20,17 +21,20 @@ function(qi_add_optional_package name)
   set(_desc "${ARGN}")
   string(TOUPPER "${name}" _U_name)
 
-  option("WITH_${_U_name}" "${_desc}" OFF)
-
   # if already set by user, do nothing:
   if(DEFINED "WITH_${_U_name}")
+    message(STATUS "WITH_${_U_name}: ${WITH_${_U_name}}")
     return()
   endif()
+
+  option("WITH_${_U_name}" "${_desc}" OFF)
 
   # else, set the value of the option using the
   # result of find_package
 
   find_package("${_U_name}" QUIET)
+  message(STATUS "${_U_name}_PACKAGE_FOUND: ${${_U_name}_PACKAGE_FOUND}")
+
 
   if(${_U_name}_PACKAGE_FOUND)
     set("WITH_${_U_name}" ON CACHE BOOL "${_desc}" FORCE)
