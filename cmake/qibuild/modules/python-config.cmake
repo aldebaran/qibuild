f79c8368c24e7a5bd9b4ec4c9eb916f14c0dea93
opennao <opennao@aldebaran-robotics.com>
2011-07-05 12:57:43 +0200

    cmake: python-config.cmake: fallback on upstream code
    This allows us to use non-precompiled python packages on windows,
    for instance

--- cmake/qibuild/modules/python-config.cmake
+++ cmake/qibuild/modules/python-config.cmake
@@ -1,27 +1,24 @@
 ## Copyright (C) 2011 Aldebaran Robotics
 
 clean(PYTHON)
-if (WIN32 AND NOT MSVC)
-  # On mingw it's very easy: the python library and headers are
-  # installed next to the executable.
-  find_program(_python NAMES python)
-  if(NOT _python)
-    qi_error("Could not find python!")
-  endif()
-
-  get_filename_component(_python_path ${_python} PATH)
-  fpath(PYTHON Python.h "${_python_path}/include")
-  flib(PYTHON  python26 python27 "${_python_path}/libs")
-else()
-  # Other platforms: libraries could come from a pre-compiled package,
-  # so never use the python executable
-  fpath(PYTHON Python.h PATH_SUFFIXES "python2.7" "python2.6")
-  flib(PYTHON OPTIMIZED NAMES python27 python2.7
-                              python26 python2.6
-                              Python)
-  flib(PYTHON DEBUG     NAMES python27_d python2.7
+# first, use fpath flib to get python from one of our pre-compiled packages
+fpath(PYTHON Python.h PATH_SUFFIXES "python2.7" "python2.6")
+flib(PYTHON OPTIMIZED NAMES python27 python2.7
+                            python26 python2.6
+                            Python)
+flib(PYTHON DEBUG     NAMES python27_d python2.7
                               python26_d python2.6
                               Python)
+
+if(NOT PYTHON_LIBRARIES OR NOT PYTHON_INCLUDE_DIR)
+  # If it does not work, use upstream cmake code
+  # Note: upstream code does NOT work with visual studio on debug
+  clean(PYTHON)
+  unset(PYTHON_INCLUDE_DIR)
+  unset(PYTHON_INCLUDE_DIR CACHE)
+  find_package(PythonLibs REQUIRED)
+  qi_set_global(PYTHON_LIBRARIES ${PYTHON_LIBRARY})
+  qi_set_global(PYTHON_INCLUDE_DIRS ${PYTHON_INCLUDE_DIR})
 endif()
 export_lib(PYTHON)
 
