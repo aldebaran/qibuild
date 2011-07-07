fc953bf8c8d9baf251f89483de61d51bd5416d4c
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-07-07 13:06:37 +0200

    test.cmake: fix running ctest with msvc IDE

--- cmake/qibuild/tests.cmake
+++ cmake/qibuild/tests.cmake
@@ -22,11 +22,7 @@ function(qi_add_test test_name target_name)
     set(ARG_TIMEOUT 20)
   endif()
 
-  if(MSVC_IDE)
-    set(_bin_path ${QI_SDK_DIR}/${target_name})
-  else()
-    set(_bin_path ${QI_SDK_DIR}/${QI_SDK_BIN}/${target_name})
-  endif()
+  set(_bin_path ${QI_SDK_DIR}/${QI_SDK_BIN}/${target_name})
 
   if(MSVC AND "${CMAKE_BUILD_TYPE}" STREQUAL "DEBUG")
     set(_bin_path ${_bin_path}_d)
