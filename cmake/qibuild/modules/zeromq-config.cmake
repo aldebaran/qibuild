e7c652f0bb00440b1762ade73721e3fe7cf8b1b6
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-07-10 16:46:55 +0200

    zeromq: last fix for vs2008

--- cmake/qibuild/modules/zeromq-config.cmake
+++ cmake/qibuild/modules/zeromq-config.cmake
@@ -4,8 +4,9 @@ clean(ZEROMQ)
 fpath(ZEROMQ zmq.h)
 
 if(WIN32)
-  flib(ZEROMQ OPTIMIZED NAMES zmq)
-  flib(ZEROMQ DEBUG     NAMES zmq_d)
+  # For some reason, it's zmq on vs2010 and libzmq on vs2008
+  flib(ZEROMQ OPTIMIZED NAMES libzmq   zmq)
+  flib(ZEROMQ DEBUG     NAMES libzmq_d zmq_d)
 else()
   flib(ZEROMQ NAMES zmq)
 endif()
