45851411c4413a374b4303376d189c8a9df3868f
Cedric GESTES <gestes@aldebaran-robotics.com>
2011-09-26 11:31:38 +0200

    qisrc: added -b to show all tracking branch.

--- python/qisrc/actions/status.py
+++ python/qisrc/actions/status.py
@@ -15,6 +15,7 @@ def configure_parser(parser):
     """Configure parser for this action """
     qibuild.qiworktree.work_tree_parser(parser)
     parser.add_argument("--untracked-files", "-u", dest="untracked_files", action="store_true", help="display untracked files")
+    parser.add_argument("--show-branch", "-b", dest="show_branch", action="store_true", help="display branch and tracking branch for each repository")
 
 def do(args):
     """ Main method """
@@ -26,12 +27,14 @@ def do(args):
             dirty.append(git_project)
 
     LOGGER.info("Dirty projects: %d/%d", len(dirty), len(qiwt.git_projects))
+    #we want to show all project
+    if args.show_branch:
+        dirty = qiwt.git_projects.values()
+
     for git_project in dirty:
         git = qisrc.git.open(git_project)
         if git.is_valid() and not git.is_clean(untracked=args.untracked_files):
             shortpath = os.path.relpath(git_project, qiwt.work_tree)
-            #print "[ %s ]" % shortpath
-            print ""
             LOGGER.info("%s : %s tracking %s", shortpath, git.get_current_branch(), git.get_tracking_branch())
             if args.untracked_files:
                 lines = git.cmd.call_output("status", "-s")
@@ -40,6 +43,10 @@ def do(args):
 
             nlines = [ x[:3] + shortpath + "/" + x[3:] for x in lines if len(x.strip()) > 0 ]
             print "\n".join(nlines)
+        elif git.is_valid():
+            shortpath = os.path.relpath(git_project, qiwt.work_tree)
+            LOGGER.info("%s : %s tracking %s", shortpath, git.get_current_branch(), git.get_tracking_branch())
+
 
     if not args.untracked_files:
         print
