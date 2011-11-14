07f45288c5fb39a71f374eb4c6b42c8a42ec1847
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-11-14 11:50:17 +0100

    Revert "cmake/pthread-config.cmake : use upstream FindThreads.cmake"
    This reverts commit 71878fbc22c222443dd222a832b365aa9d112d03.
    
    Does not work if you have
    
    project(foo CXX):
    
    Make Error at /usr/share/cmake-2.8/Modules/CheckIncludeFiles.cmake:48
    (TRY_COMPILE):
      Unknown extension ".c" for file
    
      /home/dmerewsky/work/qi/nao-flasher/build-sys-linux-x86_64/CMakeFiles/CMakeTmp/CheckIncludeFiles.c
    
      -- Looking for include files CMAKE_HAVE_PTHREAD_H - not found.
      try_compile() works only for enabled languages.  Currently these
      are:
    
      CXX
    
      See project() command to enable other languages.
    
    And besides, it does not solve the gtest/pthread bug we were having ...

--- cmake/qibuild/modules/pthread-config.cmake
+++ cmake/qibuild/modules/pthread-config.cmake
@@ -2,13 +2,5 @@
 
 clean(PTHREAD)
 fpath(PTHREAD "pthread.h")
-
-# Use upstream FindThreads.cmake to correctly set -lpthread or
-# -pthread
-find_package(Threads QUIET)
-if(CMAKE_THREAD_LIBS_INIT)
-  qi_set_global(PTHREAD_LIBRARIES ${CMAKE_THREAD_LIBS_INIT})
-else()
-  flib(PTHREAD pthread pthreadVCE2)
-endif()
+flib(PTHREAD pthread pthreadVCE2)
 export_lib(PTHREAD)
