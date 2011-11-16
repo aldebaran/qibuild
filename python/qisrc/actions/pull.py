9c3b4703d69a9f34c582b8673554548384f518d6
Cedric GESTES <gestes@aldebaran-robotics.com>
2011-11-16 11:53:33 +0100

    qisrc pull: continue by default, display error at the end

--- python/qisrc/actions/pull.py
+++ python/qisrc/actions/pull.py
@@ -14,27 +14,46 @@ def configure_parser(parser):
     """Configure parser for this action """
     qibuild.worktree.work_tree_parser(parser)
     parser.add_argument("--continue", action="store_true", dest="continue_on_error", help="continue on error")
+    parser.add_argument("--stop-on-error", action="store_false", dest="continue_on_error", help="continue on error")
     parser.add_argument("--rebase", action="store_true", dest="rebase", help="rebase")
+    parser.set_defaults(continue_on_error=True)
 
 def do(args):
     """Main entry point"""
+    fail = list()
     qiwt = qibuild.worktree_open(args.work_tree)
     for git_project in qiwt.git_projects.values():
-        try:
-            git = qisrc.git.open(git_project)
-            #TODO: replace by try/except?
-            if git.is_valid():
-                if git.get_current_remote_url() == None:
-                    LOGGER.debug("Not pulling. No remote for %s", git_project)
-                    continue
-                LOGGER.info("Pull %s", git_project)
-                if args.rebase:
-                    git.pull('--rebase')
-                else:
-                    git.pull()
-        except Exception, e:
-            if (args.continue_on_error):
-                continue
-            else:
-                raise
+        git = qisrc.git.open(git_project)
+        LOGGER.info("Pull %s", git_project)
+        if args.rebase:
+            out = git.cmd.call_output("pull", "--rebase", rawout=True)
+        else:
+            out = git.cmd.call_output("pull", rawout=True)
+        out = git.cmd.call_output("pull", rawout=True)
+        if out[0] == 0:
+            print out[1][0],
+            print out[1][1],
+        else:
+            LOGGER.error("failed (See bellow for details)")
+            #print "Failed"
+            fail.append((git_project, out))
+            if not args.continue_on_error:
+                # LOGGER.error("Pull failed: %s", f[0])
+                # print "error:"
+                # print f[1][1][0],
+                # print f[1][1][1],
+                raise Exception("\n%s%s" % (out[1][0], out[1][1]))
 
+    if len(fail) > 0:
+        print ""
+        LOGGER.info("=====================")
+        LOGGER.info("Projects that failed:")
+        print "\n".join(x[0] for x in fail)
+        LOGGER.info("=====================")
+        print ""
+        LOGGER.info("details:")
+
+    for f in fail:
+        LOGGER.error(f[0])
+        print f[1][1][0],
+        print f[1][1][1],
