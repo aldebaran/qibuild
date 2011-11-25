0fb02ee1dc1e4654cfd63daa654c32767e803323
hcuche <hcuche@aldebaran-robotics.com>
2011-11-25 10:28:04 +0100

    Add QXT support for ubuntu

--- cmake/qibuild/modules/qxt_core-config.cmake
+++ cmake/qibuild/modules/qxt_core-config.cmake
@@ -3,7 +3,8 @@
 ##
 
 clean(QXT_CORE)
-fpath(QXT_CORE qxtcore.h PATH_SUFFIXES QxtCore)
+fpath(QXT_CORE qxtcore.h PATH_SUFFIXES QxtCore qxt/QxtCore)
+fpath(QXT_CORE QxtCore PATH_SUFFIXES qxt)
 flib(QXT_CORE QxtCore)
 export_lib(QXT_CORE)
 qi_set_global(QXT_CORE_DEPENDS "QT_QTCORE")
--- cmake/qibuild/modules/qxt_network-config.cmake
+++ cmake/qibuild/modules/qxt_network-config.cmake
@@ -3,7 +3,8 @@
 ##
 
 clean(QXT_NETWORK)
-fpath(QXT_NETWORK qxtnetwork.h PATH_SUFFIXES QxtNetwork)
+fpath(QXT_NETWORK qxtnetwork.h PATH_SUFFIXES qxt/QxtNetwork QxtNetwork)
+fpath(QXT_NETWORK QxtNetwork PATH_SUFFIXES qxt)
 flib(QXT_NETWORK QxtNetwork)
 export_lib(QXT_NETWORK)
 qi_set_global(QXT_NETWORK_DEPENDS "QXT_CORE" "QT_QTNETWORK")
