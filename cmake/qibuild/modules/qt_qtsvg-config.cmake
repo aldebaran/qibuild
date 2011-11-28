dec081a3366d6ab7b39740f2142df6c74054bfb7
Cedric GESTES <gestes@aldebaran-robotics.com>
2011-11-28 10:38:02 +0100

    added qtsvg config

---
+++ cmake/qibuild/modules/qt_qtsvg-config.cmake
@@ -0,0 +1,11 @@
+## Copyright (C) 2011 Aldebaran Robotics
+
+get_filename_component(_ROOT_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
+include("${_ROOT_DIR}/qtutils.cmake")
+
+set(_suffix "QTSVG")
+set(_libame "QtSvg")
+
+qt_flib(${_suffix} ${_libame})
+export_lib(QT_${_suffix})
+set(_ROOT_DIR)
