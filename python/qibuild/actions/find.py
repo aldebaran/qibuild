f704c2e05263fe32ecf88fd15ea64fc5a02d8d2d
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-12-08 14:55:43 +0100

    qibuild find: massive rewrite

--- python/qibuild/actions/find.py
+++ python/qibuild/actions/find.py
@@ -5,6 +5,7 @@
 """
 
 import os
+import sys
 import qibuild
 
 def configure_parser(parser):
@@ -16,87 +17,47 @@ def configure_parser(parser):
     parser.add_argument("--libs",
         help="Ouputs required linnker flags")
     parser.add_argument("package")
+    parser.add_argument("project_name", nargs="?", metavar="project",
+        help="The name of a project")
     parser.set_defaults(quiet=True)
 
 
 def do(args):
     """Main entry point """
+    toc = qibuild.toc.toc_open(args.work_tree, args)
     package = args.package
-    # The algorithm is quite simple:
-    # * create a temporary project, containing find_package(project)
-    # * configures it with CMake
-    # * parse the cache
-    previous_toc = qibuild.toc.toc_open(args.work_tree, args)
-    project_names = [p.name for p in previous_toc.projects]
-    cache = dict()
-    with qibuild.sh.TempDir() as tmp:
-        dummy = os.path.join(tmp, "dummy")
-        qibuild_cmake_src  = os.path.join(qibuild.CMAKE_QIBUILD_DIR, "templates", "qibuild.cmake")
-        qibuild_cmake_dest = os.path.join(dummy, "qibuild.cmake")
-        qibuild.sh.install(qibuild_cmake_src, qibuild_cmake_dest, quiet=True)
-        cmake = os.path.join(dummy, "CMakeLists.txt")
-        main = os.path.join(dummy, "main.cpp")
-        with open(main, "w") as fp:
-            fp.write("")
-        template = """ # Dummy project
-cmake_minimum_required(VERSION 2.8)
-project(dummy)
-include(qibuild.cmake)
-qi_create_bin(dummy main.cpp)
-qi_use_lib(dummy {package})
-"""
-        with open(cmake, "w") as fp:
-            fp.write(template.format(package=package))
-        manifest = os.path.join(dummy, "qibuild.manifest")
-        template = """[project dummy]
-depends = {depends}
-"""
-        with open(manifest, "w") as fp:
-            fp.write(template.format(depends=" ".join(project_names)))
+    project_name = args.project_name
+    if not project_name:
+        project_name = qibuild.toc.project_from_cwd()
 
-        work_tree = args.work_tree
-        if not work_tree:
-            work_tree = qibuild.worktree.guess_work_tree()
-        toc = qibuild.toc.Toc(work_tree,
-               config=args.config,
-               build_type=args.build_type,
-               cmake_generator=args.cmake_generator,
-               path_hints=[dummy])
-        project_names = toc.buildable_projects.keys()
-        with open(manifest, "w") as fp:
-            fp.write(template.format(depends=" ".join(project_names)))
 
-        qibuild.command.CONFIG["quiet"] = True
-        dummy_proj = toc.get_project("dummy")
-        toc.configure_project(dummy_proj, quiet=True)
-        build_dir = dummy_proj.build_directory
-        cmake_cache = os.path.join(build_dir, "CMakeCache.txt")
-        cache = qibuild.read_cmake_cache(cmake_cache)
-
+    project = toc.get_project(project_name)
+    cmake_cache = os.path.join(project.build_directory, "CMakeCache.txt")
+    if not os.path.exists(cmake_cache):
+        print "Could not find CMakeCache for project %s" % project_name
+        print "Try using `qibuild configure` first"
+        sys.exit(2)
+    cache = qibuild.read_cmake_cache(cmake_cache)
     u_package = package.upper()
-    keys = cache.keys()
-    keys.sort()
-    keys = [k for k in keys if k.startswith(u_package)]
 
     u_package_dir = cache.get(u_package + "_DIR")
-    cmake_find_root_path = cache.get("CMAKE_FIND_ROOT_PATH")
-    if not u_package_dir:
-        # Should not happen
-        print "Could not check if %s was found or not"
-        print "Please file a bug report"
-        return
-
-    if u_package_dir.endswith("-NOTFOUND"):
+    if not u_package_dir or u_package_dir.endswith("-NOTFOUND"):
         print "No such package: ", package
+        cmake_find_root_path = cache.get("CMAKE_FIND_ROOT_PATH")
         if cmake_find_root_path:
             print "Looked in this directories: "
             paths = cmake_find_root_path.split(";")
             paths.sort()
             for path in paths:
-                if dummy in path:
-                    continue
                 print "  ", path
-        return
+        keys = cache.keys()
+        keys.sort()
+        keys = [k for k in keys if k.startswith(u_package)]
+        if keys:
+            print "Here are the list of relative CMake variables:"
+            for key in keys:
+                print key, cache[key]
+        sys.exit(2)
 
     print "Package", package
     libs = cache.get(u_package + "_LIBRARIES")
