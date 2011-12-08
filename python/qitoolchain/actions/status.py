3ac6ac504ea805f5e2c39ae7e525b41a157659a6
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-12-08 17:41:41 +0100

    qitoolchain status: add an optional toolchain name

--- python/qitoolchain/actions/status.py
+++ python/qitoolchain/actions/status.py
@@ -1,8 +1,7 @@
 
 ## Copyright (C) 2011 Aldebaran Robotics
 
-"""Display the toolchains status:
-their names, and what projects they provide
+"""Display the toolchains status their names, and what projects they provide
 
 """
 
@@ -13,11 +12,22 @@ import qitoolchain
 def configure_parser(parser):
     """Configure parser for this action """
     qibuild.cmdparse.default_parser(parser)
+    parser.add_argument("name", nargs="?",
+        help="Name of the toolchain to print. Default: all")
 
 
 def do(args):
     """ Main method """
     tc_names = qitoolchain.get_tc_names()
+    tc_name = args.name
+    if tc_name:
+        if not tc_name in qitoolchain.get_tc_names():
+            print "No such toolchain: ", tc_name
+            return
+        toolchain = qitoolchain.Toolchain(tc_name)
+        print toolchain
+        return
+
     for tc_name in tc_names:
         toolchain = qitoolchain.Toolchain(tc_name)
         print toolchain
