ba9ef8c74a71cba5b0b10b0137a955958784de55
Cedric GESTES <gestes@aldebaran-robotics.com>
2012-01-23 19:07:25 +0100

    cmake: cleanup zlib

--- cmake/qibuild/modules/zlib-config.cmake
+++ cmake/qibuild/modules/zlib-config.cmake
@@ -5,11 +5,7 @@
 #this is zlib for windows
 clean(ZLIB)
 fpath(ZLIB zlib.h PATH_SUFFIXES zlib)
-if (UNIX AND NOT APPLE)
-  set(ZLIB_LIBRARIES "-lz"  CACHE STRING "" FORCE)
-else()
-  flib(ZLIB OPTIMIZED zlib z)
-  flib(ZLIB DEBUG NAMES zlib_d zlib z)
-endif()
+flib(ZLIB OPTIMIZED zlib z)
+flib(ZLIB DEBUG NAMES zlib_d zlib z)
 export_lib(ZLIB)
 
