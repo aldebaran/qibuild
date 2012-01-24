eb4234596d974e7bb61b4d5758857c7a8df3ab79
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2012-01-24 18:53:05 +0100

    qidoc build: fix previous commit
    (commands would no longer be interrputed when using ctrl+c)

--- python/qidoc/command.py
+++ python/qidoc/command.py
@@ -21,14 +21,18 @@ def call(cmd, cwd=None, env=None, quiet=False):
         subprocess.check_call(cmd, cwd=cwd, env=env)
         return
 
+    out = ""
     process = subprocess.Popen(cmd, cwd=cwd, env=env,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT)
+    retcode = process.wait()
+        out += process.stdout.readline()
     retcode = process.wait()
     if retcode != 0:
         mess  = "Command failed\n"
         mess += "cmd: %s\n" % cmd
         mess += "cwd: %s\n" % cwd
         mess += "retcode: %s\n" % retcode
+        mess += "output: %s\n" % out
         raise Exception(mess)
 
