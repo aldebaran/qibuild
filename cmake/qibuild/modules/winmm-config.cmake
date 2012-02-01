f1f92c6cfe27b33f9c081bffb670af1d8934d3e2
platform <platform@aldebaran-robotics.com>
2012-02-01 17:49:55 +0100

    cmake: add winmm module
    Change-Id: Idea223c8956eb9a4054457a942d131d42ec152f8
    Reviewed-on: http://gerrit.aldebaran.lan:8080/58
    Reviewed-by: Cedric GESTES <gestes@aldebaran-robotics.com>
    Tested-by: Cedric GESTES <gestes@aldebaran-robotics.com>

---
+++ cmake/qibuild/modules/winmm-config.cmake
@@ -0,0 +1,10 @@
+## Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+clean(WINMM)
+# Note the space is important, we need to set it to something!
+set(WINMM_INCLUDE_DIRS " " CACHE STRING "" FORCE)
+set(WINMM_LIBRARIES "winmm" CACHE STRING "" FORCE)
+export_lib(WINMM)
+
