d4206620f7f2dfa988caba1b3e209d1cd6fbcbc2
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2012-02-06 13:58:22 +0100

    doc/cmakedomain: add index generation and references roles
    Change-Id: I00303eb3da36254765d728611e97bc73fe419890

--- doc/tools/cmakedomain.py
+++ doc/tools/cmakedomain.py
@@ -10,20 +10,55 @@ import docutils
 
 
 import sphinx
+from sphinx.roles import XRefRole
 from sphinx import addnodes
 from sphinx.locale import l_
-from sphinx.domains import ObjType
+from sphinx.domains import ObjType, Index
 from sphinx.util.docfields import TypedField, Field
+from sphinx.util.nodes import make_refnode
+
+class CMakeFunctionIndex(Index):
+    name = 'functions-index'
+    localname = l_('CMake functions Index')
+    shortname = l_('cmake functions')
+    _data = 'function'
+
+    def generate(self, docnames=None):
+        content = {}
+        for ev, modname in sorted(self.domain.data[self._data].iteritems()):
+            entries = content.setdefault(ev[0].lower(), [])
+            entries.append([ev, 2, modname, ev, '', '', ''])
+        # sort by first letter
+        result   = sorted(content.iteritems())
+        collapse = False
+        return result, collapse
+
 
 class CMakeFunction(sphinx.directives.ObjectDescription):
+    objtype_pretty = u"CMake function"
     doc_field_types = [
         TypedField('arg', label=l_('Arguments'),
                    names=['arg']),
     ]
+    def add_target_and_index(self, name, sig, signode):
+        signode['ids'].append(name)
+        self.state.document.note_explicit_target(signode)
+        inv = self.env.domaindata['cmake'][self.objtype]
+        if name in inv:
+            msg = 'duplicate CMake {0} description of {1}, other instance ' \
+                  'in {2}'.format(self.objtype, name,
+                                  self.env.doc2path(inv[name]))
+            self.env.warn(self.env.docname, msg, self.lineno)
+        inv[name] = self.env.docname
+        indextext = u"{0} ({1})".format(name, self.objtype_pretty)
+        self.indexnode['entries'].append(('single', indextext, name, name))
+
     def handle_signature(self, sig, signode):
         """
         Transform a CMake function signature
-        into RST nodes
+        into RST nodes.
+
+        Return a name to add to the index
         """
         nb_param = len(sig.split(" "))
         code = sig.replace("] ", "]\n    ")
@@ -32,6 +67,8 @@ class CMakeFunction(sphinx.directives.ObjectDescription):
         literal = docutils.nodes.literal_block(code, code)
         literal['language'] = 'cmake'
         signode += literal
+        name = sig.split("(")[0]
+        return name
 
 class CMakeDomain(sphinx.domains.Domain):
     name = 'cmake'
@@ -43,7 +80,30 @@ class CMakeDomain(sphinx.domains.Domain):
     directives = {
             'function': CMakeFunction,
     }
+    initial_data = {
+        'function': {}
+    }
+    roles = {
+        'function' : XRefRole()
+    }
+    indices = [
+        CMakeFunctionIndex,
+    ]
 
+    def clear_doc(self, docname):
+        for objtype in (u'function',):
+            for (objname, objdocname) in self.data[objtype].items():
+                if objdocname == docname:
+                    del self.data[objtype][objname]
+
+    def resolve_xref(self, env, fromdocname, builder,
+                     typ, target, node, contnode):
+        try:
+            todocname = self.data[typ][target]
+        except KeyError:
+            return None
+        return make_refnode(builder, fromdocname, todocname, target,
+	        contnode, target)
 
 
 def setup(app):
