247ec30f4a61be0726d669aba3e7d7eb8b71654f
Samuel Martin <smartin@aldebaran-robotics.com>
2012-02-09 20:03:07 +0100

    cmake: add usb-1 module
    Change-Id: I0f14593f9b332d780084a448d123f331bd8926c9
    Reviewed-on: http://gerrit.aldebaran.lan:8080/74
    Reviewed-by: Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Tested-by: Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>

--- cmake/qibuild/modules/usb_1-config.cmake
+++ cmake/qibuild/modules/usb_1-config.cmake
@@ -2,8 +2,8 @@
 ## Use of this source code is governed by a BSD-style license that can be
 ## found in the COPYING file.
 
-# libusb-0.1 module
-clean(USB)
-fpath(USB usb.h)
-flib(USB usb)
-export_lib(USB)
+# libusb-1.0 module
+clean(USB_1)
+fpath(USB_1 libusb.h PATH_SUFFIXES libusb-1.0)
+flib(USB_1 usb-1.0)
+export_lib(USB_1)
