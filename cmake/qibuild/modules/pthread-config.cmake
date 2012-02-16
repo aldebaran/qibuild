760da3d011fb19f4c8795edd36af28c7d160010d
Cedric GESTES <gestes@aldebaran-robotics.com>
2012-02-16 11:52:27 +0100

    Revert "Revert "cmake/pthread-config.cmake : use upstream FindThreads.cmake""
    This reverts commit 07f45288c5fb39a71f374eb4c6b42c8a42ec1847.

--- cmake/qibuild/modules/pthread-config.cmake
+++ cmake/qibuild/modules/pthread-config.cmake
@@ -4,5 +4,13 @@
 
 clean(PTHREAD)
 fpath(PTHREAD "pthread.h")
-flib(PTHREAD pthread pthreadVCE2)
+
+# Use upstream FindThreads.cmake to correctly set -lpthread or
+# -pthread
+find_package(Threads QUIET)
+if(CMAKE_THREAD_LIBS_INIT)
+  qi_set_global(PTHREAD_LIBRARIES ${CMAKE_THREAD_LIBS_INIT})
+else()
+  flib(PTHREAD pthread pthreadVCE2)
+endif()
 export_lib(PTHREAD)
