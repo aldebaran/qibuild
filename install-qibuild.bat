51270cb93d4950a2b9f9f0af64479d6e8bd16e2e
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2012-04-24 19:17:04 +0200

    misc: fix a few missing license headers
    Change-Id: Idfca33c9fb59026f28a6954eb0af8e57b6477bc3

--- install-qibuild.bat
+++ install-qibuild.bat
@@ -1,3 +1,6 @@
+rem Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
+rem Use of this source code is governed by a BSD-style license that can be
+rem found in the COPYING file.
 @echo off
 python tools/install-qibuild.py --windows
 pause
--- install-qibuild.sh
+++ install-qibuild.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-##
-## Copyright (C) 2010, 2011 Aldebaran Robotics
+## Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
 ##
 
 # Create a simple launcher for QiBuild scipts.
