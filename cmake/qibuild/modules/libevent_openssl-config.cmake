572abaf3be196b89955064031fb7f6a0cc683644
Pierre Roullon <proullon@aldebaran-robotics.com>
2012-05-10 14:52:42 +0200

    add module for libevent_openssl
    Change-Id: I840ed912255282ed2847be1f59a4ebab14151963
    Reviewed-on: http://gerrit.aldebaran.lan:8080/1067
    Tested-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

---
+++ cmake/qibuild/modules/libevent_openssl-config.cmake
@@ -0,0 +1,15 @@
+## Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+clean(LIBEVENT_OPENSSL)
+
+fpath(LIBEVENT_OPENSSL event2/bufferevent_ssl.h)
+if (WIN32)
+  flib(LIBEVENT_OPENSSL DEBUG     NAMES event_ssl)
+  flib(LIBEVENT_OPENSSL OPTIMIZED NAMES event_ssl)
+else()
+  flib(LIBEVENT_OPENSSL NAMES event_openssl)
+endif()
+qi_set_global(LIBEVENT_OPENSSL_DEPENDS OPENSSL)
+export_lib(LIBEVENT_OPENSSL)
