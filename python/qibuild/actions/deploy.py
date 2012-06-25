f7d2e61928c922db77973c3c703c9782c48e10ee
Cedric GESTES <gestes@aldebaran-robotics.com>
2012-06-25 15:49:39 +0200

    qibuild deploy generate all scripts for remote debugging
    Also:
    
      * split debug symbols by default (will save lots of time
        when using rsync)
      * instead of installing projects to a strange location,
        install them in a 'deploy' directory inside the build dir.
    
    Change-Id: Ieecb2da79b63ae373981b8d7210381441b89cd09
    Reviewed-on: http://gerrit.aldebaran.lan:8080/2044
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Tested-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

--- python/qibuild/actions/deploy.py
+++ python/qibuild/actions/deploy.py
@@ -5,10 +5,13 @@
 """Deploy code to a remote target """
 
 import os
+import logging
 
 import qibuild
+import qibuild.sh
 import qibuild.deploy
 
+LOGGER = logging.getLogger()
 
 def configure_parser(parser):
     """Configure parser for this action"""
@@ -29,29 +32,32 @@ def do(args):
     if rsync:
         use_rsync = True
     else:
+        LOGGER.warning("Please install rsync to get faster synchronisation")
         scp = qibuild.command.find_program("scp", env=toc.build_env)
         if not scp:
             raise Exception("Could not find rsync or scp")
 
-    config = toc.active_config
-    if not config:
-        config = "system"
-    destdir = os.path.join("~/.local/share/qi", "deploy", config, remote_directory)
-    destdir = qibuild.sh.to_native_path(destdir)
-
     # Resolve deps:
     (project_names, package_names, _) = toc.resolve_deps(runtime=True)
 
-    # Install packages to destdir:
+    # Deploy packages: install all of them in the same temp dir, they
+    # deploy this temp dir to the target
     if not args.single:
-        for package_name in package_names:
-            toc.toolchain.install_package(package_name, destdir, runtime=True)
+        with qibuild.sh.TempDir() as tmp:
+            for package_name in package_names:
+                toc.toolchain.install_package(package_name, tmp, runtime=True)
+            qibuild.deploy.deploy(tmp, args.url, use_rsync=use_rsync, port=args.port)
 
-    # Install projects to destdir:
+    # Deploy projects: install them inside a 'deploy' dir inside the build dir,
+    # then deploy this dir to the target
     for project_name in project_names:
         project = toc.get_project(project_name)
+        destdir = os.path.join(project.build_directory, "deploy")
+        project = toc.get_project(project_name)
+        #create folder for project without install rules
+        qibuild.sh.mkdir(destdir, recursive=True)
         toc.install_project(project, destdir, prefix="/",
-                            runtime=True, num_jobs=args.num_jobs)
-
-
-    qibuild.deploy.deploy(destdir, args.url, use_rsync=use_rsync, port=args.port)
+                            runtime=True, num_jobs=args.num_jobs,
+                            split_debug=True)
+        qibuild.deploy.deploy(destdir, args.url, use_rsync=use_rsync, port=args.port)
+        qibuild.deploy.generate_debug_scripts(toc, project_name, args.url)
