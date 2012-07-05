ffe1daaa052b12baff9823e4c7e3d471306d0ca3
Cedric GESTES <gestes@aldebaran-robotics.com>
2012-07-05 20:38:11 +0200

    cmake: update boost_threadpool
    Change-Id: I76f59f63fe70073ac01ab6827542587c121d2286
    Reviewed-on: http://gerrit.aldebaran.lan:8080/2673
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Tested-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

--- cmake/qibuild/modules/boost_threadpool-config.cmake
+++ cmake/qibuild/modules/boost_threadpool-config.cmake
@@ -2,8 +2,6 @@
 ## Use of this source code is governed by a BSD-style license that can be
 ## found in the COPYING file.
 
-#get the root folder of this sdk
-get_filename_component(_ROOT_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
-include("${_ROOT_DIR}/boostutils.cmake")
-
-boost_flib("threadpool")
+clean(BOOST_THREADPOOL)
+fpath(BOOST_THREADPOOL boost/threadpool.hpp)
+export_header(BOOST_THREADPOOL)
