6c7e5da936e36194bbcf2d51278986e87356b7a2
Samuel Martin <smartin@aldebaran-robotics.com>
2012-07-13 17:11:36 +0200

    python: qirsc: add pattern matching capability
    Change-Id: Ifef1e086e61faaf44711035e01669e3210175f1e
    Reviewed-on: http://gerrit.aldebaran.lan:8080/2949
    Reviewed-by: cgestes <gestes@aldebaran-robotics.com>
    Tested-by: cgestes <gestes@aldebaran-robotics.com>

--- python/qisrc/actions/list.py
+++ python/qisrc/actions/list.py
@@ -2,10 +2,12 @@
 ## Use of this source code is governed by a BSD-style license that can be
 ## found in the COPYING file.
 
-""" List the name and paths of every project
+""" List the names and paths of every project, or those matching a pattern
+
 """
 
 import os
+import re
 import sys
 import qibuild.log
 
@@ -16,13 +18,19 @@ import qibuild
 
 
 def configure_parser(parser):
-    """Configure parser for this action """
+    """ Configure parser for this action """
     qibuild.parsers.worktree_parser(parser)
+    parser.add_argument("pattern", metavar="PATTERN", nargs="?",
+                        help="pattern to be matched")
 
 def do(args):
     """ Main method """
-    qiwt = qisrc.open_worktree(args.worktree)
+    qiwt  = qisrc.open_worktree(args.worktree)
+    regex = args.pattern
+    if args.pattern is not None:
+        regex = re.compile(regex)
     print "Projects in :", qiwt.root
     print
     for project in qiwt.projects:
-       print project.src
+        if regex is None or regex.search(project.src) is not None:
+            print project.src
