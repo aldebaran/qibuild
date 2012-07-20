ada1a5d14a77241a8663d8e70e9e98991a2cbc25
Samuel Martin <smartin@aldebaran-robotics.com>
2012-07-20 20:08:18 +0200

    python: qitoolchain: import-package: refactoring post qibuild.cmake.modules and qitoolchain.binary_package.convert_to_qibuild
    Change-Id: If79b849cdbad892b4c8c6e827a051e5678f585ad

--- python/qitoolchain/actions/import_package.py
+++ python/qitoolchain/actions/import_package.py
@@ -3,6 +3,7 @@
 ## found in the COPYING file.
 
 """Import a binary package into a toolchain.
+
 """
 
 import os
@@ -10,37 +11,27 @@ import subprocess
 
 import qibuild
 import qitoolchain
-import qitoolchain.binary_package
+import qitoolchain.binary_package as binpkg
 
-_CMAKE_MODULE_PKG_LIST = """
-Package {0} already provides the following CMake module(s):
-{1}
-You can find it/them at the above location(s) from the package installation
-directory (run 'qitoolchain info' to get it).
-"""
-
-_CMAKE_MODULE_QIBUILD_LIST = """
-qiBuild already provides the following CMake module(s) for the package {0}:
-{1}
-"""
-
-_MESSAGE_START = "Importing '{1}' in the toolchain '{0}' ..."
-
-_MESSAGE_END = "Package '{1}' has successfully been added to the toolchain '{0}'."
 
 def configure_parser(parser):
     """Configure parser for this action """
     qibuild.parsers.toc_parser(parser)
     parser.add_argument("package_name", metavar='NAME',
-        help="The name of the package", nargs='?')
-    parser.add_argument("package_path", metavar='PATH',
-        help="The path to the package")
+                        help="The name of the package", nargs='?')
+    parser.add_argument("package_path", metavar='BINPKGPATH',
+                        help="The path to the package")
+    parser.add_argument("-d", "--directory", dest="dest_dir",
+                        metavar='DESTDIR', help="""\
+destination directory of the qiBuild package after convertsion
+(default: aside the original package)""")
     return
+
 
 def do(args):
     """ Import a binary package into a toolchain
 
-    - Check that there is a CMake module into the binary package
+    - Convert the binary package into a qiBuild package
     - Add the qiBuild package to the cache
     - Add the qiBuild package from cache to toolchain
 
@@ -49,38 +40,71 @@ def do(args):
     tc = qitoolchain.get_toolchain(tc_name)
 
     package_name = args.package_name
-    package_path = args.package_path
-
-    with qibuild.sh.TempDir() as tmp:
-        package = qitoolchain.binary_package.open_package(package_path)
-
-    package_metadata = package.get_metadata()
-
-    if package_name is None:
-        package_name = package_metadata['name']
-
-    package_names = [ package_metadata['name'] ]
-    if not package_name in package_names:
-        package_names.insert(0, package_name)
-
+    package_path = os.path.abspath(args.package_path)
+    dest_dir     = args.dest_dir
+    if dest_dir is None:
+        dest_dir = os.path.dirname(package_path)
+    if os.path.isdir(package_path):
+        if package_name is None:
+            message = """
+Error: when turning an install directory into a qiBuild package,
+a package name must be passed to the command line.
+"""
+            raise Exception(message)
+        package = package_path
+    else:
+        package = open_package(package_path)
+        package_metadata = package.get_metadata()
+        if package_name is None:
+            package_name = package_metadata['name']
+            other_names.append(package_metadata['name'])
+    other_names.append(package_name)
+    other_names = list(set(other_names))
     # extract it to the default packages path of the toolchain
     tc_packages_path = qitoolchain.toolchain.get_default_packages_path(tc.name)
-    dest = os.path.join(tc_packages_path, package_name)
-    qibuild.sh.rm(dest)
-    qibuild.ui.info(_MESSAGE_START.format(tc.name, package_name))
+    package_dest = os.path.join(tc_packages_path, package_name)
+    qibuild.sh.rm(package_dest)
+    message = """
+Importing '{1}' in the toolchain {0} ...
+""".format(tc.name, package_path)
+    qibuild.ui.info(message)
+    # conversion into qiBuild
     with qibuild.sh.TempDir() as tmp:
-        conversion  = qitoolchain.binary_package.convert_to_qibuild(tmp, package, package_names)
-        qibuild_pkg = conversion[0]
-        modules_from_pkg     = conversion[1]
-        modules_from_qibuild = conversion[2]
-        if len(modules_from_pkg) > 0:
-            list_ = "".join(["  {0}\n".format(x) for x in modules_from_pkg])
-            print _CMAKE_MODULE_PKG_LIST.format(package_name, list_)
-        if len(modules_from_qibuild) > 0:
-            list_ = "".join(["  {0}\n".format(x) for x in modules_from_qibuild])
-            print _CMAKE_MODULE_PKG_LIST.format(package_name, list_)
-        extracted   = qibuild.archive.extract(qibuild_pkg, tmp, quiet=True)
-        qibuild.sh.install(extracted, dest, quiet=True)
-    qibuild_package = qitoolchain.Package(package_name, dest)
+        conversion = binpkg.convert_to_qibuild(tmp, package, package_name,
+                                               other_names=other_names,
+                                               interactive=True)
+        qibuild_package_path = conversion[0]
+        modules_package      = conversion[1]
+        modules_qibuild      = conversion[2]
+        src = os.path.abspath(qibuild_package_path)
+        dst = os.path.join(dest_dir, os.path.basename(qibuild_package_path))
+        dst = os.path.abspath(dst)
+        qibuild.sh.mkdir(dest_dir, recursive=True)
+        qibuild.sh.rm(dst)
+        qibuild.sh.mv(src, dst)
+        qibuild_package_path = dst
+    # installation of the qiBuild package
+    with qibuild.sh.TempDir() as tmp:
+        extracted = qibuild.archive.extract(qibuild_package_path, tmp, quiet=True)
+        qibuild.sh.install(extracted, package_dest, quiet=True)
+    qibuild_package = qitoolchain.Package(package_name, package_dest)
     tc.add_package(qibuild_package)
-    qibuild.ui.info(_MESSAGE_END.format(tc.name, package_name))
+    # end :)
+    package_content = qibuild.sh.ls_r(package_dest)
+    modules_list = qibuild.cmake.modules.find_cmake_module_in(package_content)
+    modules_list = [os.path.join(package_dest, cmake_) for cmake_ in modules_list]
+    modules_list.extend(modules_qibuild)
+    modules_list = ["  {0}".format(module_) for module_ in modules_list]
+    modules_list = "\n".join(modules_list)
+    message = """\
+Import succedded.
+
+qiBuild package:
+  {2}
+
+Package '{1}' has been added to the toolchain '{0}'.
+
+To use this package in your project, you may want to check out:
+{3}
+""".format(tc.name, package_name, qibuild_package_path, modules_list)
+    qibuild.ui.info(message)
