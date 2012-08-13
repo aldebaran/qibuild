e06ab8be943fad42ea3fc09dc6062ab637c6ac69
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2012-08-13 17:25:07 +0200

    cmake: use ode-config to set -DdSINGLE or -DdDOUBLE
    Change-Id: Ib2b85e281375563592f00704c48b2c610799336c
    Reviewed-on: http://gerrit.aldebaran.lan:8080/3777
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Tested-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

--- cmake/qibuild/modules/ode-config.cmake
+++ cmake/qibuild/modules/ode-config.cmake
@@ -4,5 +4,23 @@
 
 clean(ODE)
 fpath(ODE ode/ode.h)
+if(UNIX)
+  # Check wether we should define -DdDOUBLE
+  find_program(_ode_config ode-config QUIET)
+  if(_ode_config)
+    # execute ode-config --cflags
+    execute_process(COMMAND ${_ode_config} "--cflags"
+      OUTPUT_VARIABLE _output
+    )
+    string(REGEX MATCH "-DdSINGLE" _single ${_output})
+    if(_single)
+      add_definitions("-DdSINGLE")
+    endif()
+    string(REGEX MATCH "-DdDOUBLE" _double ${_output})
+    if(_double)
+      add_definitions("-DdDOUBLE")
+    endif()
+  endif()
+endif()
 flib(ODE ode)
 export_lib(ODE)
