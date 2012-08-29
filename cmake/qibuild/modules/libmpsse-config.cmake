f2abe1b9a0ad0e7575339d714b54497ed7560bd4
Samuel Martin <smartin@aldebaran-robotics.com>
2012-08-29 19:19:29 +0200

    cmake: add libmpsse module
    Change-Id: I67b91642e94d534c8487aea7a918bc3a00943ee5
    Reviewed-on: http://gerrit.aldebaran.lan:8080/4440
    Reviewed-by: cgestes <gestes@aldebaran-robotics.com>
    Tested-by: cgestes <gestes@aldebaran-robotics.com>

---
+++ cmake/qibuild/modules/libmpsse-config.cmake
@@ -0,0 +1,8 @@
+## Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+clean(LIBMPSSE)
+fpath(LIBMPSSE mpsse.h)
+flib(LIBMPSSE mpsse)
+export_lib(LIBMPSSE)
