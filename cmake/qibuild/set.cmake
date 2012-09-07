69245233a3a7af0a494c1ac13b70b9e110275b2f
Cedric GESTES <gestes@aldebaran-robotics.com>
2012-09-08 00:40:58 +0200

    cmake: add functions for handling global and persistent variables
    Change-Id: Ice07806e6eb079272a4c830b379742568ddf0522

--- cmake/qibuild/set.cmake
+++ cmake/qibuild/set.cmake
@@ -2,6 +2,68 @@
 ## Use of this source code is governed by a BSD-style license that can be
 ## found in the COPYING file.
 
+#WARNING you should always call qi_global_get to get a global variable
+macro(qi_global_set name)
+  set_property(GLOBAL PROPERTY "${name}" ${ARGN})
+endmacro()
+
+macro(qi_global_get OUT_var name)
+  #macro so no need for PARENT_SCOPE
+  get_property("${OUT_var}" GLOBAL PROPERTY "${name}")
+endmacro()
+
+macro(qi_global_is_set OUT_var name)
+  #macro so no need for PARENT_SCOPE
+  get_property("${OUT_var}" GLOBAL PROPERTY "${name}" SET)
+endmacro()
+
+
+#persistent across run
+macro(qi_persistent_set _name)
+  set("${_name}" ${ARGN} CACHE INTERNAL "" FORCE)
+endmacro()
+
+macro(qi_persistent_get OUT_var name)
+  #macro so no need for PARENT_SCOPE
+  get_property("${OUT_var}" CACHE PROPERTY "${name}")
+endmacro()
+
+macro(qi_persistent_is_set OUT_var name)
+  #macro so no need for PARENT_SCOPE
+  get_property("${OUT_var}" CACHE PROPERTY "${name}" SET)
+endmacro()
+
+macro(qi_persistent_append _name)
+  list(APPEND "${_name}" ${ARGN})
+  set("${_name}" "${${_name}}" CACHE INTERNAL "" FORCE)
+endmacro()
+
+macro(qi_persistent_prepend _name)
+  list(INSERT "${_name}" 0 ${ARGN})
+  set("${_name}" "${${_name}}" CACHE INTERNAL "" FORCE)
+endmacro()
+
+function(qi_persistent_append_uniq _name)
+  foreach(_value ${ARGN})
+    list(FIND "${_name}" ${_value} _found)
+    if (_found STREQUAL "-1")
+      qi_persistent_append("${_name}" ${_value})
+    endif()
+  endforeach()
+endfunction()
+
+function(qi_persistent_prepend_uniq _name)
+  foreach(_value ${ARGN})
+    list(FIND "${_name}" ${_value} _found)
+    if (_found STREQUAL "-1")
+      qi_persistent_prepend("${_name}" ${_value})
+    endif()
+  endforeach()
+endfunction()
+
+## DEPRECATED ##
+
+
 # write the variable into cache, variable wont be visible
 macro(qi_set_global name)
   set("${name}" ${ARGN} CACHE INTERNAL "" FORCE)
