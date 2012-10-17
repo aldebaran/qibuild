df69a10d5768a4510aede1860bf65a9f1275831f
agarcia <agarcia@aldebaran-robotics.com>
2012-10-17 17:26:27 +0200

    cmake: add hdf5 and hdf5_cpp
    Change-Id: I56e81c1f1bf4a072b2d950d2cf6a6303ccc8ab42
    Reviewed-on: http://gerrit.aldebaran.lan:8080/6914
    Reviewed-by: sbarthelemy <sbarthelemy@aldebaran-robotics.com>
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Tested-by: agarcia <agarcia@aldebaran-robotics.com>

---
+++ cmake/qibuild/modules/hdf5-config.cmake
@@ -0,0 +1,9 @@
+## Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+clean(HDF5)
+fpath(HDF5 hdf5.h)
+flib(HDF5 hdf5)
+qi_set_global(HDF5_DEPENDS "PTHREAD" "ZLIB" "M")
+export_lib(HDF5)
---
+++ cmake/qibuild/modules/hdf5_cpp-config.cmake
@@ -0,0 +1,9 @@
+## Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+clean(HDF5_CPP)
+fpath(HDF5_CPP H5Cpp.h)
+flib(HDF5_CPP hdf5_cpp)
+qi_set_global(HDF5_CPP_DEPENDS "HDF5")
+export_lib(HDF5_CPP)
