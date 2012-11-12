32eb1801bd2082a286ca9ddce953fa024c65339f
Matthieu Nottale <mnottale@aldebaran-robotics.com>
2012-11-12 16:07:45 +0100

    cmake: module: ortp.
    Change-Id: I588d4485622c9c6d32e461b8778389e11e134e1d
    Reviewed-on: http://gerrit.aldebaran.lan:8080/7830
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Tested-by: mnottale <mnottale@aldebaran-robotics.com>

---
+++ cmake/qibuild/modules/ortp-config.cmake
@@ -0,0 +1,8 @@
+## Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+clean(ORTP)
+fpath(ORTP "ortp.h" PATH_SUFFIXES "ortp")
+flib(ORTP "ortp" NAMES ortp)
+export_lib(ORTP)
