1fce016442d448498306506491a28bb2a79b620d
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2012-11-28 19:25:56 +0100

    cmake/qibuild/modules: support opencv 2.4.3
    Change-Id: I1a7f873c925ea32fe95bd0c02c1fade9baf8d703
    Reviewed-on: http://gerrit.aldebaran.lan:8080/8411
    Reviewed-by: llec <llec@aldebaran-robotics.com>
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Tested-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

--- cmake/qibuild/modules/opencv2utils.cmake
+++ cmake/qibuild/modules/opencv2utils.cmake
@@ -2,7 +2,7 @@
 ## Use of this source code is governed by a BSD-style license that can be
 ## found in the COPYING file.
 
-set(_cv_versions "2.3.1")
+set(_cv_versions "2.3.1" "2.4.3")
 
 function(opencv2_flib name)
   cmake_parse_arguments(ARG "" "" "DEPENDS" ${ARGN})
