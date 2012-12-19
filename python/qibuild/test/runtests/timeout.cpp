035a8cbdb627736ca7be3c6854ae85b79d483bab
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2012-12-19 15:36:34 +0100

    fix compilation af test code on windows
    Change-Id: Ie7388b3a3af85e713c0b2b0156f37c68781dc1d8
    Reviewed-on: http://gerrit.aldebaran.lan:8080/9262
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Tested-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

--- python/qibuild/test/runtests/timeout.cpp
+++ python/qibuild/test/runtests/timeout.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-#include <unistd.h>
+#ifdef _MSC_VER
+# include <Windows.h>
+#else
+# include <unistd.h>
+#endif
 
 int main() {
   std::cout << "timeout" << std::endl;
+#ifdef _MSC_VER
+  Sleep(2000);
+#else
   sleep(2);
+#endif
 }
 
