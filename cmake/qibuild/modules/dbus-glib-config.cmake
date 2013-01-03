578acbe54dadc416c27de779cf9df8e5784fb6e9
jmassot <jmassot@aldebaran-robotics.com>
2013-01-03 17:51:48 +0100

    Revert "cmake: dbus/dbus-1 compatibility layer"
    This reverts commit a90459cd9b4399df0f3abdf07fca056dab3fd967
    
    accidental push
    
    Change-Id: Ifa119642138190ddb0115406618fb932c2d5e3ce
    Reviewed-on: http://gerrit.aldebaran.lan:8080/9664
    Reviewed-by: jmassot <jmassot@aldebaran-robotics.com>
    Tested-by: jmassot <jmassot@aldebaran-robotics.com>

--- cmake/qibuild/modules/dbus-glib-config.cmake
+++
@@ -1,5 +0,0 @@
-## Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
-## Use of this source code is governed by a BSD-style license that can be
-## found in the COPYING file.
-
-find_package(DBUS-GLIB-1)
