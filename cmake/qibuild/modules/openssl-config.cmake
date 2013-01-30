876c39ffb723285d4e0c40dddf8921f53bd7bb36
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2013-01-30 14:33:25 +0100

    openssl: add depency to DL
    Upstream does not do it
    
    Change-Id: Iaa11ed4cb0c5797fcdcab067602a4eb6f8affacc
    Reviewed-on: http://gerrit.aldebaran.lan:8080/11887
    Tested-by: gerrit
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Tested-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

--- cmake/qibuild/modules/openssl-config.cmake
+++ cmake/qibuild/modules/openssl-config.cmake
@@ -3,7 +3,10 @@
 ## found in the COPYING file.
 
 clean(OPENSSL)
-  find_package(OpenSSL QUIET)
-  qi_set_global(OPENSSL_INCLUDE_DIRS ${OPENSSL_INCLUDE_DIR})
-  qi_set_global(OPENSSL_LIBRARIES    ${OPENSSL_LIBRARIES})
+find_package(OpenSSL QUIET)
+qi_set_global(OPENSSL_INCLUDE_DIRS ${OPENSSL_INCLUDE_DIR})
+qi_set_global(OPENSSL_LIBRARIES    ${OPENSSL_LIBRARIES})
+if(UNIX AND NOT APPLE)
+  qi_set_global(OPENSSL_DEPENDS DL)
+endif()
 export_lib(OPENSSL)
