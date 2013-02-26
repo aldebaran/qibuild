83eaa886e0c8e4bbdbe0c8876f8c0c71b6ad131e
Your name <you@example.com>
2013-02-26 11:16:16 +0100

    Adapat boost_flib for libraries being only headers
    Change-Id: I41b0604ff50b8d4b7e118781b262e940ca75a6d9
    Reviewed-on: http://gerrit.aldebaran.lan:8080/13320
    Tested-by: gerrit
    Reviewed-by: ncornu <ncornu@aldebaran-robotics.com>

---
+++ cmake/qibuild/modules/boost_lockfree-config.cmake
@@ -0,0 +1,12 @@
+## Copyright (c) 2013 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+#get the root folder of this sdk
+get_filename_component(_ROOT_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
+include("${_ROOT_DIR}/boostutils.cmake")
+
+if (Boost_VERSION GREATER 105300 OR Boost_VERSION EQUAL 105300)
+boost_flib("lockfree")
+qi_set_global(BOOST_LOCKFREE_DEPENDS BOOST_ATOMIC)
+endif()
