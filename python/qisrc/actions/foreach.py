3d62af49dca87876bb28636a2614e1e21b9c4672
Nicolas Cornu <ncornu@aldebaran-robotics.com>
2013-04-02 16:22:27 +0200

    qisrc foreach can be run on specified projects only
    Change-Id: I27691146d3270e315485d4f6f41568c1cdbbbb2e
    Reviewed-on: http://gerrit.aldebaran.lan:8080/15430
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Tested-by: gerrit

--- python/qisrc/actions/foreach.py
+++ python/qisrc/actions/foreach.py
@@ -14,18 +14,38 @@ import qisys
 from qisys import ui
 
 def configure_parser(parser):
-    """Configure parser for this action """
+    """Configure parser for this action."""
     qisys.parsers.worktree_parser(parser)
+    qisys.parsers.project_parser(parser, positional=False)
     parser.add_argument("command", metavar="COMMAND", nargs="+")
     parser.add_argument("--ignore-errors", "--continue",
         action="store_true", help="continue on error")
 
 def do(args):
-    """Main entry point"""
+    """Main entry point."""
     qiwt = qisys.worktree.open_worktree(args.worktree)
     errors = list()
-    ui.info(ui.green, "Running `%s` on every project" % " ".join(args.command))
-    git_projects = qisrc.git.get_git_projects(qiwt.projects)
+
+    # Found on which projects launch the command
+    git_projects = qisrc.cmdparse.projects_from_args(args, qiwt)
+    git_projects = qisrc.git.get_git_projects(git_projects)
+    nbr_git_projects = len(git_projects)
+
+    # Compute the total of git projects
+    nbr_all_git_projects = len(qisrc.git.get_git_projects(qiwt.projects))
+
+    if not git_projects:
+        ui.error("There is no Git projects here.")
+        return
+
+    # Print the command and the projects
+    ui.info(ui.green, "Running `%s` on " % " ".join(args.command), end="")
+    if nbr_git_projects == nbr_all_git_projects:
+        ui.info(ui.green, "all", end="")
+    else:
+        ui.info(ui.green, "%i" % nbr_git_projects, end="")
+    ui.info(ui.green, "projects")
+
     count = len(git_projects)
     for i, project in enumerate(git_projects, start=1):
         command = args.command[:]
