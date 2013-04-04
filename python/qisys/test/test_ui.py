8f7f20c29a522c8f4be79df32aea56a25a3521c0
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2013-04-04 12:56:44 +0200

    move tests around
    Change-Id: I2ed574b69ff8ed6d7871ffc43c26498e193714ec

--- python/qisys/test/test_ui.py
+++ python/qisys/test/test_ui.py
@@ -1,26 +1,28 @@
-## Copyright (c) 2013 Aldebaran Robotics. All rights reserved.
+## Copyright (c) 2012, 2013 Aldebaran Robotics. All rights reserved.
 ## Use of this source code is governed by a BSD-style license that can be
 ## found in the COPYING file.
 
-"""Automatic tests for ui."""
+""" Just some tests for ui """
 
-import pytest
+import qisys.ui as ui
 
-from qisys import ui
+def main():
+    ui.info(ui.red, "This is a an error message\n",
+        ui.reset, "And here are the details")
+    ui.error("could not build")
+    ui.warning("-j ignored for this generator")
+    ui.info("building foo")
+    ui.debug("debug message")
+    ui.info(ui.brown, "this is brown")
+    ui.info(ui.bold, ui.brown, "this is bold brown")
+    ui.info(ui.red, "red is dead")
+    ui.info(ui.darkred, "darkred is really dead")
+    ui.info(ui.yellow, "this is yellow")
 
-def test_indent_iterable():
-    list_given = []
-    list_given = ui.indent_iterable(list_given)
-    list_expected = []
-    assert list_given == list_expected
-
-    list_given = ["a", "b", "c"]
-    list_given = ui.indent_iterable(list_given)
-    list_expected = ["  a", "  b", "  c"]
-    assert list_given == list_expected
-
-    list_given = ["a", "b", "c"]
-    list_given = ui.indent_iterable(list_given, num=4)
-    list_expected = ["    a", "    b", "    c"]
-    assert list_given == list_expected
-
+if __name__ == "__main__":
+    import sys
+    if "-v" in  sys.argv:
+        ui.CONFIG["verbose"] = True
+    if "-q" in sys.argv:
+        ui.CONFIG["quiet"]  = True
+    main()
