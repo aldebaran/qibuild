3987f059632c64058862425407cdc165d4f3182b
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2013-04-09 13:44:47 +0200

    Add test for qisrc list
    Change-Id: I04c08f60044ffb0ba2ff63141d085e4dc2545455

--- python/qisrc/test/test_qisrc_list.py
+++ python/qisrc/test/test_qisrc_list.py
@@ -4,3 +4,13 @@ def test_list_tips_when_empty(qisrc_action, record_messages):
     qisrc_action("init")
     qisrc_action("list")
     assert ui.find_message("Tips")
+
+def test_list_with_pattern(qisrc_action, record_messages):
+    qisrc_action.git_worktree.create_git_project("foo")
+    qisrc_action.git_worktree.create_git_project("baz")
+    qisrc_action.git_worktree.create_git_project("foobar")
+    record_messages.reset()
+    qisrc_action("list", "foo.*")
+    assert ui.find_message("foo")
+    assert ui.find_message("foobar")
+    assert not ui.find_message("baz")
