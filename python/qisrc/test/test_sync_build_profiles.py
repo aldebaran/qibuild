31590e2a9adc8e03d23d5c39c75bbf45146ed501
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2013-05-10 19:23:57 +0200

    test_sync_build_profiles: do not use stringio
    this will too brittle, use real files instead
    
    Change-Id: Ib5cb8213bc686646c6cfcc94a24e2a4bfae8ebec

--- python/qisrc/test/test_sync_build_profiles.py
+++ python/qisrc/test/test_sync_build_profiles.py
@@ -1,18 +1,13 @@
-## Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
+## Copyright (c) 2012, 2013 Aldebaran Robotics. All rights reserved.
 ## Use of this source code is governed by a BSD-style license that can be
 ## found in the COPYING file.
-from StringIO import StringIO
 
-import qisys
-import qisys.worktree
 import qibuild.profile
 import qisrc.sync_build_profiles
 
-from qisrc.test.conftest import TestGitWorkTree
 from qibuild.test.conftest import TestBuildWorkTree
 
 def test_remote_added(cd_to_tmpdir):
-    git_worktee = TestGitWorkTree()
     build_worktree = TestBuildWorkTree()
     xml = """
 <manifest>
@@ -21,8 +16,11 @@ def test_remote_added(cd_to_tmpdir):
   </profiles>
 </manifest>
 """
+    remote_xml = cd_to_tmpdir.join("remote.xml")
+    remote_xml.write(xml)
     qibuild_xml = build_worktree.qibuild_xml
-    qisrc.sync_build_profiles.sync_build_profiles(build_worktree, StringIO(xml))
+    qisrc.sync_build_profiles.sync_build_profiles(build_worktree,
+                                                  remote_xml.strpath)
     profiles = qibuild.profile.parse_profiles(qibuild_xml)
     assert len(profiles) == 1
     assert "foo" in profiles
@@ -42,11 +40,15 @@ def test_remote_updated(cd_to_tmpdir, record_messages):
   </profiles>
 </manifest>
 """
+    remote_xml = cd_to_tmpdir.join("remote.xml")
+    remote_xml.write(xml)
+    qibuild_xml = build_worktree.qibuild_xml
     qibuild_xml = build_worktree.qibuild_xml
     qibuild.profile.configure_build_profile(qibuild_xml,
                                             "foo",
                                             [('SPAM', 'EGGS')])
-    qisrc.sync_build_profiles.sync_build_profiles(build_worktree, StringIO(xml))
+    qisrc.sync_build_profiles.sync_build_profiles(build_worktree,
+                                                  remote_xml.strpath)
     assert record_messages.find("updated remotely")
 
     profiles = qibuild.profile.parse_profiles(qibuild_xml)
@@ -68,10 +70,13 @@ def test_same_remote(cd_to_tmpdir, record_messages):
   </profiles>
 </manifest>
 """
+    remote_xml = cd_to_tmpdir.join("remote.xml")
+    remote_xml.write(xml)
     qibuild_xml = build_worktree.qibuild_xml
     qibuild.profile.configure_build_profile(qibuild_xml,
                                             "foo",
                                             [('FOO', 'BAR')])
-    qisrc.sync_build_profiles.sync_build_profiles(build_worktree, StringIO(xml))
+    qisrc.sync_build_profiles.sync_build_profiles(build_worktree,
+                                                  remote_xml.strpath)
     assert not record_messages.find("updated remotely")
 
