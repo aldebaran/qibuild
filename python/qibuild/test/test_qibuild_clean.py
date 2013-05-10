ee0cca941f09535097a493b2a78db1c364d6497a
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2013-05-10 18:43:35 +0200

    qibuild clean: get changes from qibuild 2.3
    Change-Id: I10ec177c7985772eb90fe83a171e65789bdd8ad4

--- python/qibuild/test/test_qibuild_clean.py
+++ python/qibuild/test/test_qibuild_clean.py
@@ -1,4 +1,5 @@
 import os
+import py
 
 def test_clean_build_dir(qibuild_action):
     world_proj = qibuild_action.add_test_project("world")
@@ -37,3 +38,24 @@ def test_cleaning_all_build_dirs(qibuild_action, toolchains):
     build_config.build_type = "Release"
     assert not os.path.exists(world_proj.build_directory)
 
+
+def test_clean_profiles(qibuild_action, toolchains, interact):
+    build_worktree = qibuild_action.build_worktree
+    build_worktree.configure_build_profile("a", [("A", "ON")])
+    build_worktree.configure_build_profile("b", [("B", "ON")])
+    toolchains.create("foo")
+    world_proj = qibuild_action.add_test_project("world")
+    # pylint: disable-msg=E1101
+    world_path = py.path.local(world_proj.path)
+    build_foo_a = world_path.ensure("build-foo-a", dir=True)
+    build_foo_b = world_path.ensure("build-foo-b", dir=True)
+    build_foo_a_b = world_path.ensure("build-foo-a-b", dir=True)
+    build_foo_c = world_path.ensure("build-foo-c", dir=True)
+    qibuild_action("clean", "-z", "--all", "--force")
+    assert build_foo_a.check(dir=False)
+    assert build_foo_b.check(dir=False)
+    assert build_foo_a_b.check(dir=False)
+    assert build_foo_c.check(dir=True)
+    interact.answers = [True]
+    qibuild_action("clean", "-x", "--all", "--force")
+    assert build_foo_c.check(dir=False)
