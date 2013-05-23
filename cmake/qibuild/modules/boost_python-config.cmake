0bfc545dc699216a78bdf84755ebeb9402ff909b
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2013-05-23 14:44:55 +0200

    cmake: fix boost_python and python deps
    Change-Id: I6d043858ee03754ba31273770f807e586fc4724b
    Reviewed-on: http://gerrit.aldebaran.lan:8080/17805
    Tested-by: gerrit
    Tested-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

--- cmake/qibuild/modules/boost_python-config.cmake
+++ cmake/qibuild/modules/boost_python-config.cmake
@@ -7,4 +7,4 @@ get_filename_component(_ROOT_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
 include("${_ROOT_DIR}/boostutils.cmake")
 
 boost_flib("python")
-qi_set_global(BOOST_PYTHON_DEPENDS "PYTHON")
+qi_persistent_set(BOOST_PYTHON_DEPENDS "PYTHON")
--- cmake/qibuild/modules/python-config.cmake
+++ cmake/qibuild/modules/python-config.cmake
@@ -8,6 +8,17 @@ clean(PYTHON)
 # which prevent us from using boost_python
 fpath(PYTHON Python.h PATH_SUFFIXES "python2.7")
 flib(PYTHON NAMES python27 python2.7 Python)
+set(_python_deps)
+if(UNIX)
+  list(APPEND _python_deps PTHREAD)
+  if(APPLE)
+  else()
+    # linux
+    list(APPEND _python_deps DL UTIL)
+  endif()
+endif()
+
+qi_persistent_set(PYTHON_DEPENDS ${_python_deps})
 
 if(NOT PYTHON_LIBRARIES OR NOT PYTHON_INCLUDE_DIRS)
   # If it does not work, use upstream cmake code
