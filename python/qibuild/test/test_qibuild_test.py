46202f78b364c46213e3179a1e6aa6e6e45067b4
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2013-05-28 11:05:29 +0200

    test/qibuild_test: fix for Windows
    Change-Id: I8ac2608b5759cbb168d5e0649c442eb7d4358aea

--- python/qibuild/test/test_qibuild_test.py
+++ python/qibuild/test/test_qibuild_test.py
@@ -1,3 +1,5 @@
+import os
+
 def test_various_outcomes(qibuild_action, record_messages):
     qibuild_action.add_test_project("testme")
     qibuild_action("configure", "testme")
@@ -12,7 +14,10 @@ def test_various_outcomes(qibuild_action, record_messages):
 
     record_messages.reset()
     rc = qibuild_action("test", "testme", "-k", "segfault", retcode=True)
-    assert record_messages.find("Segmentation fault")
+    if os.name == 'nt':
+        assert record_messages.find("Return code: 255")
+    else:
+        assert record_messages.find("Segmentation fault")
     assert rc == 1
 
     record_messages.reset()
