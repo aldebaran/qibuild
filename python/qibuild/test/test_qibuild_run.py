b78ff2e6c281c6b53a051b885b7ef805a4982c3a
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2013-05-31 13:44:00 +0200

    tests: fix testing on Windows
    Change-Id: I45053c372b04f028bf9281dec662650cf3ab5418
    Reviewed-on: http://gerrit.aldebaran.lan:8080/18243
    Tested-by: gerrit
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

--- python/qibuild/test/test_qibuild_run.py
+++ python/qibuild/test/test_qibuild_run.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 
 from qibuild.test.conftest import QiBuildAction
 from qitoolchain.test.conftest import QiToolchainAction
@@ -27,7 +28,11 @@ def test_run_segfaulting_binary(qibuild_action, record_messages):
     qibuild_action("make", "testme")
 
     retcode = qibuild_action("run", "segfault", retcode=True)
-    assert record_messages.find("Process crashed")
+    if os.name != 'nt':
+        # on Windows the python process may be interrupted by
+        # the OS with a pop up 'segfault_d.exe has stopped working'
+        # so we may not get the error message
+        assert record_messages.find("Process crashed")
     assert retcode != 0
 
 def test_run_failure(qibuild_action):
