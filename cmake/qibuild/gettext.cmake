79b36fb751025f6ac2b2bf5daf7848c3a241ed91
Herve CUCHE <hcuche@aldebaran-robotics.com>
2013-06-04 18:32:59 +0200

    doc for qi_create_trad
    Change-Id: I647ae0eeac63c6552de291be65434ff367b54b00
    Reviewed-on: http://gerrit.aldebaran.lan:8080/18447
    Tested-by: gerrit
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Reviewed-on: http://gerrit.aldebaran.lan:8080/18440
    Tested-by: hcuche <hcuche@aldebaran-robotics.com>

--- cmake/qibuild/gettext.cmake
+++ cmake/qibuild/gettext.cmake
@@ -2,6 +2,16 @@
 ## Use of this source code is governed by a BSD-style license that can be
 ## found in the COPYING file.
 
+#! Create install rules for dictionnary
+#
+# Example of use: ::
+#
+#   qi_create_lib(my_application SHARED SRC main.cpp)
+#   qi_create_trad(my_application "po")
+#
+# \arg:app_name Name of the application you want to translate
+# \arg:po_dir Directory where you have the POTFILES.in and
+#             where files will be generated
 function(qi_create_trad app_name po_dir)
   qi_stage_dir(${po_dir})
   set(_locale_dir ${po_dir}/share/locale)
