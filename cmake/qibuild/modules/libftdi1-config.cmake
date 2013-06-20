67eac53239c91cf67762afff0f884069b17612ec
Nicolas Cornu <ncornu@aldebaran-robotics.com>
2013-06-20 17:43:08 +0200

    Add libftdi1-config.cmake
    Change-Id: I96439fcf50636fd917b5ba46f3d12f65ee6bce9d
    Reviewed-on: http://gerrit.aldebaran.lan:8080/19317
    Reviewed-by: ncornu <ncornu@aldebaran-robotics.com>
    Tested-by: ncornu <ncornu@aldebaran-robotics.com>
    Reviewed-on: http://gerrit.aldebaran.lan/19390
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

---
+++ cmake/qibuild/modules/libftdi1-config.cmake
@@ -0,0 +1,10 @@
+## Copyright (c) 2013 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+clean(LIBFTDI1)
+
+fpath(LIBFTDI1 libftdi1/ftdi.h)
+flib(LIBFTDI1 ftdi1)
+
+export_lib(LIBFTDI)
