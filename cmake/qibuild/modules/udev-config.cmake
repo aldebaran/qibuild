8f30148459727947cdffabbcbc31a78966b6ebea
Vincent Rabaud <vrabaud@aldebaran-robotics.com>
2013-07-29 16:02:17 +0200

    Qibuild: add a CMake config file for udev
    Change-Id: I3c753d5a03c0513294c248dd9349444c2f007ea7
    Reviewed-on: http://gerrit.aldebaran.lan/23189
    Reviewed-by: vrabaud <vrabaud@aldebaran-robotics.com>
    Tested-by: vrabaud <vrabaud@aldebaran-robotics.com>
    Tested-by: gerrit
    Reviewed-by: smartin <smartin@aldebaran-robotics.com>

---
+++ cmake/qibuild/modules/udev-config.cmake
@@ -0,0 +1,16 @@
+## Copyright (c) 2012, 2013 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+# We tried:
+#clean(UDEV)
+#find_package(PkgConfig)
+#pkg_check_modules(UDEV libudev)
+#export_lib_pkgconfig(UDEV)
+# But that did not work because UDEV_FOUND is set to 1 but UDEV_PACKAGE_FOUND is set to False
+# So we use the following
+
+clean(UDEV)
+fpath(UDEV libudev.h)
+flib(UDEV udev)
+export_lib(UDEV)
