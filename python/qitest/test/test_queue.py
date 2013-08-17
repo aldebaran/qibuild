c9e8a5b9afab1877fd64e0a0678998fc37202e81
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2013-08-17 12:31:36 +0200

    qitest: implements a clean test queue
    Change-Id: I2c2adadd0b18e7d42ed1596b4d8f2627eebedc4f

---
+++ python/qitest/test/test_queue.py
@@ -0,0 +1,57 @@
+from qisys import ui
+import time
+import qitest.test_queue
+import qitest.launcher
+
+class DummyLauncher(qitest.launcher.TestLauncher):
+    def __init__(self):
+        self.results = dict()
+
+    def launch(self, test):
+        default_time = 0.2
+        default_result = True, (ui.green, "[OK]")
+        sleep_time = default_time
+        result = default_result
+        fate = self.results.get(test["name"])
+        if fate:
+            sleep_time = fate.get("sleep_time", default_time)
+            result = fate.get("result", default_result)
+            if fate.get('raises'):
+                raise Exception("Kaboom!")
+        time.sleep(sleep_time)
+        return result
+
+def test_queue_happy():
+    tests = [
+     {"name" : "one"},
+     {"name" : "two"},
+     {"name" : "three"},
+     {"name" : "four"},
+     {"name" : "five"},
+    ]
+    test_queue = qitest.test_queue.TestQueue(tests)
+    dummy_launcher = DummyLauncher()
+    test_queue.launcher = dummy_launcher
+    test_queue.run(num_jobs=3)
+    assert test_queue.ok
+
+
+def test_queue_sad():
+    tests = [
+     {"name" : "one"},
+     {"name" : "two"},
+     {"name" : "three"},
+     {"name" : "four"},
+     {"name" : "five"},
+    ]
+    test_queue = qitest.test_queue.TestQueue(tests)
+    dummy_launcher = DummyLauncher()
+    dummy_launcher.results = {
+        "two" : {"result" : (False, (ui.red, "[FAIL]"))},
+        "three" : {"raises" : True},
+        "four" : {"sleep_time" : 0.4},
+    }
+
+    test_queue.launcher = dummy_launcher
+    test_queue.run(num_jobs=3)
+    assert not test_queue.ok
