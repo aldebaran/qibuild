f355de92dfbc31917ad295eae4b99c682e824517
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2013-08-24 12:07:59 +0200

    qi_add_test: add support for working directory
    Change-Id: I57b1b297962ce5ec897858ab41048864712b4ad1

---
+++ python/qibuild/test/projects/testme/cwd.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <limits.h>
+#include <unistd.h>
+
+std::string get_working_directory()
+{
+  char buf[PATH_MAX];
+  char* res = getcwd(buf, PATH_MAX);
+  if (res != NULL) {
+    return std::string(buf);
+  }
+  return std::string();
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc < 2) {
+    std::cerr << "Usage: cwd EXPECTED" << std::endl;
+    return 2;
+  }
+
+  std::string actual = get_working_directory();
+  std::string expected = argv[1];
+  if (actual != expected) {
+    std::cerr << "Expecting:" << std::endl
+              << expected << std::endl
+              << "Got:"
+              << actual;
+    return 1;
+  }
+  return 0;
+
+}
