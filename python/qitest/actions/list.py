08f880b4a585b9a1f916acfa53bb0c9978c65af4
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2013-09-03 18:11:25 +0200

    qitest list, qitest run: do something meaningful
    Change-Id: I2a609af8ce1124cffa098c2a5f9faec31038bfef

--- python/qitest/actions/list.py
+++ python/qitest/actions/list.py
@@ -5,21 +5,16 @@ import os
 import sys
 from qisys import ui
 
-import qibuild.parsers
+import qisys.parsers
 import qitest.conf
 
 def configure_parser(parser):
-    qibuild.parsers.build_parser(parser)
-    qibuild.parsers.project_parser(parser)
+    qisys.parsers.default_parser(parser)
+    group = parser.add_argument_group("qitest list options")
+    group.add_argument("qitest_json", help="path to a qitest.json file")
 
 def do(args):
-    build_worktree = qibuild.parsers.get_build_worktree(args)
-    project = qibuild.parsers.get_one_build_project(build_worktree, args)
-    qitest_jsoin = project.qitest_json
-    if not os.path.exists(qitest_json):
-        ui.error("No tests found for project", project.name)
-        sys.exit(1)
-    ui.info(ui.green, "Lists of tests for", ui.blue, project.name)
+    qitest_json = args.qitest_json
     tests = qitest.conf.parse_tests(qitest_json)
     for i, test in enumerate(tests):
         ui.info_count(i, len(tests), test["name"])
--- python/qitest/actions/run.py
+++ python/qitest/actions/run.py
@@ -11,12 +11,35 @@ import os
 import sys
 
 from qisys import ui
-import qibuild.parsers
+import qisys.parsers
+import qibuild.test_runner
+import qitest.parsers
 import qitest.actions.list
 
 def configure_parser(parser):
     """Configure parser for this action"""
+    qisys.parsers.default_parser(parser)
+    qitest.parsers.test_parser(parser)
+    parser.add_argument("qitest_json", help="path to a qitest.json file")
 
 def do(args):
     """Main entry point"""
+    qitest_json = args.qitest_json
+    test_project = TestProject(qitest_json)
+    test_runner = qibuild.test_runner.ProjectTestRunner(test_project)
+    test_runner.pattern = args.pattern
+    test_runner.perf = args.perf
+    test_runner.coverage = args.coverage
+    test_runner.valgrind = args.valgrind
+    test_runner.verbose = args.verbose_tests
+    test_runner.num_cpus = args.num_cpus
+    test_runner.num_jobs = args.num_jobs
+    res = test_runner.run()
+    sys.exit(res)
 
+
+class TestProject(object):
+    """ A class to keep ProjectTestRunner happy """
+    def __init__(self, qitest_json):
+        self.qitest_json = qitest_json
+        self.build_directory = os.path.dirname(qitest_json)
