a603d83e24dafd4eb9b3d24eeb7fdacc22809bad
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2013-09-06 18:44:52 +0200

    remove old and broken example
    Change-Id: Id761eabad379f4da3b0d254723e37f8188b3df54

--- doc/source/samples/uselib/foosdk/share/cmake/Foo/FooConfig.cmake
+++
@@ -1,3 +0,0 @@
-## Copyright (C) 2011 Aldebaran Robotics
-
-message(STATUS "Entering: MyCMakeProject1Config")
--- doc/source/samples/uselib/foosdk/sourceme.cmake
+++
@@ -1,5 +0,0 @@
-## Copyright (C) 2011 Aldebaran Robotics
-
-#export share/cmake, include, lib
-get_filename_component(_ROOT_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
-list(APPEND CMAKE_PREFIX_PATH "${_ROOT_DIR}")
