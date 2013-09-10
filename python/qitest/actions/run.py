fbfcfa636ee01ed1569dfbde36a6e74fb17981fa
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2013-09-10 11:22:22 +0200

    qitest.run: small bug fix
    Change-Id: Ib7ccb2fd7f48e24afc07a61ff43caa04d8f6ff9d

--- python/qitest/actions/run.py
+++ python/qitest/actions/run.py
@@ -34,7 +34,7 @@ def do(args):
     test_runner.verbose = args.verbose_tests
     test_runner.num_cpus = args.num_cpus
     test_runner.num_jobs = args.num_jobs
-    test_runner.nightly = arg.nightly
+    test_runner.nightly = args.nightly
     res = test_runner.run()
     if not res:
         sys.exit(1)
