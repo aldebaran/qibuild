f70ba1d5986a96d1c357dcc9108f23a278ec46d7
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2013-10-07 15:50:30 +0200

    qibuild test: fix XML generation
    Also, add more tests
    
    Change-Id: I3b8f2f1c27882daf05d02f3c62960d442cdbfde1
    Reviewed-on: http://gerrit.aldebaran.lan/28840
    Tested-by: gerrit
    Reviewed-by: vbarbaresi <vbarbaresi@aldebaran-robotics.com>

--- python/qibuild/test/test_run_tests.py
+++ python/qibuild/test/test_run_tests.py
@@ -1,8 +1,15 @@
+import os
+import qisys.qixml
 
 def test_run(build_worktree):
     testme = build_worktree.add_test_project("testme")
     testme.configure()
-    testme.build()
+    testme.build(target="tests")
     ok = testme.run_tests()
     assert not ok
-
+    test_dir = os.path.join(testme.build_directory, "test-results")
+    xml_files = os.listdir(test_dir)
+    xml_files = [x for x in xml_files if x.endswith(".xml")]
+    for xml_file in xml_files:
+        full_path = os.path.join(test_dir, xml_file)
+        qisys.qixml.read(full_path)
