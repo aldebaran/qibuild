1699978800a147485cf313fc222575fd690de8bf
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2013-11-21 10:33:43 +0100

    Remove old install stuff
    setuptools already does all of that, and in a better way
    
    Change-Id: I1ecb9f49f684fca497c73237dbf163557bde2c46
    Reviewed-on: http://gerrit.aldebaran.lan/31441
    Tested-by: gerrit
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

--- install-qibuild.bat
+++
@@ -1,7 +0,0 @@
-rem Copyright (c) 2012, 2013 Aldebaran Robotics. All rights reserved.
-rem Use of this source code is governed by a BSD-style license that can be
-rem found in the COPYING file.
-@echo off
-python tools/install-qibuild.py --windows
-pause
-
