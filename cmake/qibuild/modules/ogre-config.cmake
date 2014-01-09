016fe2c651669867e9f8bcbdb28878344086e37d
Cedric GESTES <gestes@aldebaran-robotics.com>
2014-01-09 15:48:40 +0100

    ogre: support ogre 1.9.0
    Change-Id: If9842f1636f80b9c430ea907f28fd1bc880bcea8
    Reviewed-on: http://gerrit.aldebaran.lan/32564
    Tested-by: gerrit
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

--- cmake/qibuild/modules/ogre-config.cmake
+++ cmake/qibuild/modules/ogre-config.cmake
@@ -4,8 +4,36 @@
 
 clean(OGRE)
 fpath(OGRE OgreRoot.h PATH_SUFFIXES ogre OGRE)
+find_path(OGRE_PREQ_PATH OgrePrerequisites.h PATH_SUFFIXES ogre OGRE)
+
 flib(OGRE OPTIMIZED NAMES OgreMain Ogre)
 flib(OGRE DEBUG     NAMES OgreMain_d OgreMain Ogre)
+
+function(get_preprocessor_entry VARIABLE)
+  string(REGEX MATCH "# *define +${VARIABLE} +((\"([^\n]*)\")|([^ \n]*))" _there "${ARGN}")
+  if (CMAKE_MATCH_3)
+    set(${VARIABLE} "${CMAKE_MATCH_3}" PARENT_SCOPE)
+  else(CMAKE_MATCH_4)
+    set(${VARIABLE} "${CMAKE_MATCH_4}" PARENT_SCOPE)
+  else()
+    set(${VARIABLE} "0" PARENT_SCOPE)
+  endif ()
+endfunction()
+
+# determine Ogre version
+file(READ ${OGRE_PREQ_PATH}/OgrePrerequisites.h OGRE_TEMP_VERSION_CONTENT)
+get_preprocessor_entry(OGRE_VERSION_MAJOR ${OGRE_TEMP_VERSION_CONTENT})
+get_preprocessor_entry(OGRE_VERSION_MINOR ${OGRE_TEMP_VERSION_CONTENT})
+get_preprocessor_entry(OGRE_VERSION_PATCH ${OGRE_TEMP_VERSION_CONTENT})
+set(OGRE_VERSION "${OGRE_VERSION_MAJOR}.${OGRE_VERSION_MINOR}.${OGRE_VERSION_PATCH}")
+
+message(STATUS "Found Ogre (${OGRE_VERSION})")
+
+if (${OGRE_VERSION} VERSION_GREATER "1.8.99")
+  flib(OGRE OPTIMIZED NAMES OgreOverlay)
+  flib(OGRE DEBUG     NAMES OgreOverlay_d OgreOverlay)
+endif()
+
 if (UNIX AND NOT APPLE)
   flib(OGRE freeimage)
 endif()
