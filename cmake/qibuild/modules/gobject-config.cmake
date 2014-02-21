bdf65739399f3af8ec66f79cb54036ffc4e275d8
Samuel Martin <smartin@aldebaran-robotics.com>
2014-02-21 15:36:49 +0100

    cmake: add g* modules
    Add modules for gmodule, gobject and gthread
    
    Change-Id: I0f19b7e61f2abc936fb0c3ec6dd9e3d40927ac5b
    Reviewed-on: http://gerrit.aldebaran.lan/36493
    Reviewed-by: jmassot <jmassot@aldebaran-robotics.com>
    Tested-by: jmassot <jmassot@aldebaran-robotics.com>
    Tested-by: gerrit
    Reviewed-by: hkouki <hkouki@aldebaran-robotics.com>
    Reviewed-by: smartin <smartin@aldebaran-robotics.com>
    Tested-by: smartin <smartin@aldebaran-robotics.com>

---
+++ cmake/qibuild/modules/gobject-config.cmake
@@ -0,0 +1,7 @@
+## Copyright (c) 2012-2014 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+find_package(PkgConfig)
+pkg_check_modules(GOBJECT gobject-2.0)
+export_lib_pkgconfig(GOBJECT)
