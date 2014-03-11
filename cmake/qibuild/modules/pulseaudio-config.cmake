13ba44e824a1d9245b6aad5088cd634d1fc31d36
hkouki <hkouki@aldebaran-robotics.com>
2014-03-11 10:44:23 +0100

    add pulseaudio finder
    Change-Id: I5d860673541030aef57a3fbc60fdd4864a2ab3db
    Reviewed-on: http://gerrit.aldebaran.lan/37171
    Approved: hkouki <hkouki@aldebaran-robotics.com>
    Reviewed-by: hkouki <hkouki@aldebaran-robotics.com>
    Tested-by: hkouki <hkouki@aldebaran-robotics.com>

---
+++ cmake/qibuild/modules/pulseaudio-config.cmake
@@ -0,0 +1,8 @@
+## Copyright (c) 2012-2014 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+clean(PULSEAUDIO)
+fpath(PULSEAUDIO pulse/pulseaudio.h)
+flib(PULSEAUDIO pulse)
+export_lib(PULSEAUDIO)
