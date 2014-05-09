c2929b93dc237a06f878557920d82c37ce4ee102
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2014-05-09 10:07:55 +0200

    FIx tests for new qibuild deploy API
    Change-Id: Ia0ebf4cc7138e793d9cf9de8c73f14a7088f5026

--- python/qibuild/test/test_qibuild_deploy.py
+++ python/qibuild/test/test_qibuild_deploy.py
@@ -39,7 +39,7 @@ def test_deploying_to_localhost(qibuild_action, tmpdir):
     qibuild_action.add_test_project("hello")
     qibuild_action("configure", "hello")
     qibuild_action("make", "hello")
-    qibuild_action("deploy", "hello", url)
+    qibuild_action("deploy", "hello", "--url", url)
 
     assert tmpdir.join("lib").check(dir=True)
     assert tmpdir.join("bin").check(dir=True)
@@ -57,7 +57,7 @@ def test_deploying_to_several_urls(qibuild_action, tmpdir):
     qibuild_action.add_test_project("hello")
     qibuild_action("configure", "hello")
     qibuild_action("make", "hello")
-    qibuild_action("deploy", "hello", first_url, "--url", second_url)
+    qibuild_action("deploy", "hello", "--url", first_url, "--url", second_url)
 
     assert tmpdir.join("first").join("lib").check(dir=True)
     assert tmpdir.join("first").join("bin").check(dir=True)
@@ -72,9 +72,9 @@ def test_deploying_tests(qibuild_action, tmpdir):
     qibuild_action("configure", "testme")
     qibuild_action("make", "testme")
     # default is no tests:
-    qibuild_action("deploy", "testme", url)
+    qibuild_action("deploy", "testme", "--url", url)
     assert tmpdir.join("bin/ok").check(file=False)
-    qibuild_action("deploy", "--with-tests", "testme", url)
+    qibuild_action("deploy", "--with-tests", "testme", "--url", url)
     assert tmpdir.join("bin/ok").check(file=True)
 
 
@@ -86,5 +86,5 @@ def test_deploy_builds_build_deps(qibuild_action, tmpdir):
     bar_proj = qibuild_action.create_project("bar", build_depends=["foo"])
     qibuild_action("configure", "bar")
     qibuild_action("make", "bar")
-    qibuild_action("deploy", "bar", url)
+    qibuild_action("deploy", "bar", "--url", url)
     qibuild.find.find([foo_proj.sdk_directory], "foo", expect_one=True)
