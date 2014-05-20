396b4ace05cfa6a7ec2c4b15a679e21f7be39b1c
Cedric GESTES <gestes@aldebaran-robotics.com>
2014-05-20 12:15:18 +0200

    cmake: find clang
    Change-Id: I5efc4a9e57220ebcaaaae59817c6e97d47474c9a

---
+++ cmake/qibuild/modules/clang-config.cmake
@@ -0,0 +1,17 @@
+## Copyright (c) 2013 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+clean(CLANG)
+
+fpath(CLANG clang-c/Index.h PATHS ${CLANG_ROOT}/include NO_DEFAULT_PATH)
+if (NOT CLANG_INCLUDE_DIRS)
+  fpath(CLANG clang-c/Index.h)
+endif()
+
+flib(CLANG clang PATHS ${CLANG_ROOT}/lib NO_DEFAULT_PATH)
+if (NOT CLANG_LIBRARIES)
+  flib(CLANG clang)
+endif()
+
+export_lib(CLANG)
