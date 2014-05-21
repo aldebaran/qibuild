e300827c3efa15001ba4bea1d6329a32c6ce7533
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2014-05-21 16:13:47 +0200

    Add qipy clean
    Change-Id: I0d2f114c8dc00ddd4a032f0a36e4c253b1bc1b2f
    Reviewed-on: http://gerrit.aldebaran.lan/40339
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Tested-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

---
+++ python/qipy/actions/clean.py
@@ -0,0 +1,27 @@
+""" Remove a complete virtualenv
+
+"""
+import sys
+import os
+
+from qisys import ui
+import qisys.sh
+import qisys.command
+import qisys.parsers
+import qibuild.parsers
+import qipy.parsers
+import qipy.worktree
+
+def configure_parser(parser):
+    qibuild.parsers.cmake_build_parser(parser)
+    parser.add_argument("-f", "--force", dest="force", action="store_true")
+
+def do(args):
+    python_builder = qipy.parsers.get_python_builder(args)
+    python_worktree = python_builder.python_worktree
+    venv_path = python_worktree.venv_path
+    if not args.force:
+        ui.info("Would delete", venv_path)
+    else:
+        ui.info("Removing", venv_path)
+        qisys.sh.rm(venv_path)
