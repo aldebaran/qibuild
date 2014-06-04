9f20a865d05c44a7b00f5909eec47ceaa9685ed4
Cedric GESTES <gestes@aldebaran-robotics.com>
2014-06-04 15:10:28 +0200

    qipy: add pip action
    Change-Id: I0cbf440b751cb53926d6ac979e0c6ed1a6d7cb05
    Reviewed-on: http://gerrit.aldebaran.lan/41148
    Reviewed-by: vbarbaresi <vbarbaresi@aldebaran-robotics.com>
    Tested-by: vbarbaresi <vbarbaresi@aldebaran-robotics.com>

---
+++ python/qipy/actions/pip.py
@@ -0,0 +1,26 @@
+""" Run pip from the correct virtualenv
+
+"""
+
+import sys
+import os
+import subprocess
+
+from qisys import ui
+import qisys.sh
+import qisys.command
+import qisys.parsers
+import qibuild.parsers
+import qipy.parsers
+import qipy.worktree
+
+def configure_parser(parser):
+    qibuild.parsers.cmake_build_parser(parser)
+    parser.add_argument("pip_options", metavar="COMMAND", nargs="*")
+
+def do(args):
+    pip_options = args.pip_options
+    python_builder = qipy.parsers.get_python_builder(args)
+    python_worktree = python_builder.python_worktree
+    pip = python_worktree.pip
+    subprocess.check_call([pip] + pip_options)
