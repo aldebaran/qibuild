f349d3d764fe851fb9d7ded7d6a9c9941fa8c223
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2014-06-12 16:31:12 +0200

    cmake: add bison and flex
    Upstream's cmake files will use whatever is in $PATH,
    and we need to find the executable in the toolchain instead
    
    Change-Id: I60e0bba914c68af7285fec66a514971aa72e4969

---
+++ cmake/qibuild/modules/flex-config.cmake
@@ -0,0 +1,5 @@
+## Copyright (c) 2012-2014 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+find_program(FLEX_EXECUTABLE flex)
