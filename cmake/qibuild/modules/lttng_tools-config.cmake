d647f80a4c2bd8db75098aa3793062c6fd7fc073
hkouki <hkouki@aldebaran-robotics.com>
2014-06-16 15:35:07 +0200

    add lttng_tools finder
    Change-Id: Ieb1b63909a49e35a34f29fa4003cb290105c611b
    Reviewed-on: http://gerrit.aldebaran.lan/41626
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Tested-by: gerrit

---
+++ cmake/qibuild/modules/lttng_tools-config.cmake
@@ -0,0 +1,9 @@
+## Copyright (C) 2011-2014 Aldebaran Robotics
+
+clean(LTTNG_TOOLS)
+fpath(LTTNG_TOOLS "lttng/lttng.h")
+flib(LTTNG_TOOLS "lttng-ctl")
+
+export_header(LTTNG_TOOLS)
+export_lib(LTTNG_TOOLS)
+
