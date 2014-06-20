7c25719fb36514793357cea064436983876ae361
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2014-06-20 17:16:52 +0200

    add qisrc list-groups
    Change-Id: I1d14fa9725b6ebaf6dc61d38dcb448744e54ec5b

---
+++ python/qisrc/actions/list_groups.py
@@ -0,0 +1,32 @@
+""" List the available groups
+
+"""
+
+from qisys import ui
+import qisrc.parsers
+import qisys.qixml
+
+import sys
+import os
+
+def configure_parser(parser):
+    qisrc.parsers.worktree_parser(parser)
+
+
+def do(args):
+    git_worktree = qisrc.parsers.get_git_worktree(args)
+    local_groups = git_worktree._syncer.manifest.groups
+
+    all_groups= list()
+    groups_xml = os.path.join(git_worktree.root, ".qi", "groups.xml")
+    tree = qisys.qixml.read(groups_xml)
+    root = tree.getroot()
+    groups_elems = root.findall("group")
+    for group_elem in groups_elems:
+        all_groups.append(group_elem.get("name"))
+
+    for group in all_groups:
+        if group in local_groups:
+            ui.info("* ", ui.green, group)
+        else:
+            ui.info("  ", group)
