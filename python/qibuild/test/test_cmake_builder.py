6ab6d31482552c70f7207f5d8753571cb2a65b72
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2014-06-30 15:33:07 +0200

    remove mocked tests of the CMakeBuilder
    Too close to the implementation, and actually not testing anything
    
    Change-Id: Ic6f53fe2b07442b5fd97fb00be08a3dc91aaaa10
    Reviewed-on: http://gerrit.aldebaran.lan/42434
    Tested-by: gerrit
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

--- python/qibuild/test/test_cmake_builder.py
+++ python/qibuild/test/test_cmake_builder.py
@@ -4,49 +4,6 @@ import qibuild.parsers
 import mock
 import pytest
 
-def test_calls_configure():
-    build_worktree = mock.Mock()
-    hello_proj = mock.Mock()
-    world_proj = mock.Mock()
-    deps_solver = mock.Mock()
-    deps_solver.get_dep_projects.return_value = [world_proj, hello_proj]
-
-    cmake_builder = qibuild.cmake_builder.CMakeBuilder(build_worktree,
-                                                       [hello_proj])
-    cmake_builder.deps_solver = deps_solver
-
-    cmake_builder.configure()
-    assert world_proj.configure.called
-    assert hello_proj.configure.called
-
-def test_forward_args_to_configure():
-    build_worktree = mock.Mock()
-    deps_solver = mock.Mock()
-    hello_proj = mock.Mock()
-    deps_solver.get_dep_projects.return_value = [hello_proj]
-    cmake_builder = qibuild.cmake_builder.CMakeBuilder(build_worktree,
-                                                       [hello_proj])
-    cmake_builder.deps_solver = deps_solver
-
-    cmake_builder.configure(trace_cmake=True)
-    hello_proj.configure.assert_called_with(trace_cmake=True)
-
-def test_write_dependencies_cmake_first():
-    build_worktree = mock.Mock()
-    deps_solver = mock.Mock()
-    hello_proj = mock.Mock()
-    sdk_dirs = [
-        "/path/to/hello/build/sdk",
-        "/path/to/world/build/sdk"
-    ]
-    deps_solver.get_dep_projects.return_value = [hello_proj]
-    deps_solver.get_sdk_dirs.return_value = sdk_dirs
-    cmake_builder = qibuild.cmake_builder.CMakeBuilder(build_worktree,
-                                                       [hello_proj])
-    cmake_builder.deps_solver = deps_solver
-    cmake_builder.configure()
-    hello_proj.write_dependencies_cmake.assert_called_with(sdk_dirs)
-
 def test_check_configure_has_been_called_before_building(build_worktree):
     hello_proj = build_worktree.create_project("hello")
     cmake_builder = qibuild.cmake_builder.CMakeBuilder(build_worktree, [hello_proj])
