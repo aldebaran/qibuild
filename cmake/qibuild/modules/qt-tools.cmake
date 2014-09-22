30d447b3f6028f67308c73cd246e2473b963fe6f
vpaleologue <vpaleologue@aldebaran-robotics.com>
2014-09-22 16:32:22 +0200

    qt-tools.cmake: do not call find_package(Qt4)
    The risk is that upstream CMake module
    (in /usr/share/cmake-3.0/Modules/FindQt4.cmake") sets some
    qt variables that conflict with the one we set
    in our wrapper modules (in qibuild/cmake/qibuild/modules)
    
    Instead, just call ``include(Qt4Macros.cmake)`` directly,
    after having looked for uic, moc and rcc.
    
    Change-Id: Iaced1bfa3ae0e55cca0a1a96d5b89821bbc260ec

--- cmake/qibuild/modules/qt-tools.cmake
+++ cmake/qibuild/modules/qt-tools.cmake
@@ -8,33 +8,12 @@
 # look for moc and uic, and the copy-paste the macros from
 # QT_USE_FILE ...
 
-# Note: when cross-compiling, you should set QT_USE_QMAKE
-# to false so that we do not use qmake from the system.
-
 get_filename_component(_ROOT_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
-if(NOT DEFINED QT_USE_QMAKE)
-  find_program(QT_QMAKE NAMES qmake-qt4 qmake)
-  if(QT_QMAKE)
-    set(QT_USE_QMAKE TRUE CACHE INTERNAL "" FORCE)
-  else()
-    set(QT_USE_QMAKE FALSE CACHE INTERNAL "" FORCE)
-  endif()
-endif()
 
-if(QT_USE_QMAKE)
-  # Use upstream cmake files:
-  find_package(Qt4 COMPONENTS "")
-  include(Qt4Macros)
-else()
-  # Using a qt package from a desktop toolchain:
-  # look for moc, uic and rcc in the package before
-  # including Qt4Macros
-  find_program(QT_MOC_EXECUTABLE NAMES moc-qt4 moc)
-  find_program(QT_UIC_EXECUTABLE NAMES uic-qt4 uic)
-  find_program(QT_RCC_EXECUTABLE NAMES rcc-qt4 rcc)
-  include("${_ROOT_DIR}/Qt4Macros.cmake")
-endif()
-
+find_program(QT_MOC_EXECUTABLE NAMES moc-qt4 moc)
+find_program(QT_UIC_EXECUTABLE NAMES uic-qt4 uic)
+find_program(QT_RCC_EXECUTABLE NAMES rcc-qt4 rcc)
+include("${_ROOT_DIR}/Qt4Macros.cmake")
 
 #! Generate qt.conf. Assumes qi_use_lib(... QT_QTCORE) has been called
 #
