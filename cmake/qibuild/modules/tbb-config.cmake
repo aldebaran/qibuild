f608d36cb702d630ed76fef8696748bd81a875a8
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2014-09-24 15:41:12 +0200

    cmake modules: add tbb
    Change-Id: I823cd8ce2f73eeabbca31f0e1f07ed4d7c6b3b05
    Reviewed-on: http://gerrit.aldebaran.lan/46210
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Tested-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

---
+++ cmake/qibuild/modules/tbb-config.cmake
@@ -0,0 +1,9 @@
+## Copyright (c) 2012-2014 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+clean(TBB)
+fpath(TBB tbb/tbb.h)
+flib(TBB tbb)
+
+export_lib(TBB)
