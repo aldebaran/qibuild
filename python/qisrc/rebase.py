b3cf405cd6bcd552537e7f2f7956ec5b261b8173
Samuel Martin <smartin@aldebaran-robotics.com>
2014-10-06 10:24:23 +0200

    python: qisrc.rebase: fix output bug in rebase_project function
    Change-Id: I6ab6ef770ce8afe3251e39c81ff12ba089286559
    Reviewed-on: http://gerrit.aldebaran.lan/47004
    Tested-by: gerrit
    Reviewed-by: smartin <smartin@aldebaran-robotics.com>
    Tested-by: smartin <smartin@aldebaran-robotics.com>

--- python/qisrc/rebase.py
+++ python/qisrc/rebase.py
@@ -50,9 +50,10 @@ def rebase_projects(git_projects, upstream_projects, branch, verbose):
     rebased_projects = list()
     errors = list()
     max_src = max(len(x.src) for x in git_projects)
+    eol = "\n" if verbose else ""
     fetch_quiet = False if verbose else True
     for i, git_project in enumerate(git_projects):
-        ui.info_count(i, len(git_projects), git_project.src, end="\0")
+        ui.info_count(i, len(git_projects), git_project.src, end=eol)
         git = qisrc.git.Git(git_project.path)
         git.fetch(quiet=fetch_quiet)
         if not git_project.default_remote:
