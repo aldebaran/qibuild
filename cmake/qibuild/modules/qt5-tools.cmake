5a1644327678064550a7c1c52c5f565e2b36051c
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2014-11-13 14:21:53 +0100

    qt5: remove the need to call find_package() at each run
    Instead we use a get_target_property() call in
    qt5utils
    
    Change-Id: I30fecf4165bee627a90b16a1339cd1a3efe60d68

--- cmake/qibuild/modules/qt5-tools.cmake
+++ cmake/qibuild/modules/qt5-tools.cmake
@@ -9,14 +9,3 @@ include("${Qt5Core_DIR}/Qt5CoreMacros.cmake")
 find_package(Qt5Widgets REQUIRED)
 include("${Qt5Widgets_DIR}/Qt5WidgetsMacros.cmake")
 
-# Since upstream uses imported targets (with Qt5::Widgets for instance),
-# we need to call find_package() at every run so that imported targets are found.
-# We cannot do it in the qibuild/modules/qt5_*-config.cmake because
-# of the PACKAGE_FOUND optimization used by qi_use_lib()
-find_package(Qt5Concurrent)
-find_package(Qt5Ftp)
-find_package(Qt5Network)
-find_package(Qt5Script)
-find_package(Qt5Sql)
-find_package(Qt5Test)
-find_package(Qt5Xml)
--- cmake/qibuild/modules/qt5utils.cmake
+++ cmake/qibuild/modules/qt5utils.cmake
@@ -6,8 +6,24 @@
 # qt5_flib(QT5_CORE Qt5Core)
 
 function(qt5_flib prefix name)
+  set(${prefix}_LIBRARIES)
+  # upstream uses Qt5::Core notation but we need to dereference this
+  # because we won't be calling find_package() again
   find_package(${name})
-  set(${prefix}_LIBRARIES ${${name}_LIBRARIES} CACHE INTERNAL "" FORCE)
+  foreach(_lib ${${name}_LIBRARIES})
+    if(WIN32)
+      get_target_property(_lib_loc_debug ${_lib} LOCATION_DEBUG)
+      get_target_property(_lib_loc_release ${_lib} LOCATION_RELEASE)
+      list(APPEND ${prefix}_LIBRARIES
+      debug "${_lib_loc_debug}"
+      optimized "${_lib_loc_release}")
+    else()
+      get_target_property(_lib_loc ${_lib} LOCATION)
+      list(APPEND ${prefix}_LIBRARIES ${_lib_loc})
+    endif()
+  endforeach()
+  set(${prefix}_LIBRARIES ${${prefix}_LIBRARIES} CACHE INTERNAL "" FORCE)
   set(${prefix}_INCLUDE_DIRS ${${name}_INCLUDE_DIRS} CACHE INTERNAL "" FORCE)
+
   export_lib(${prefix})
 endfunction()
