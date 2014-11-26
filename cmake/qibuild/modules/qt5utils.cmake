e6a55dff8ee86e640a0987d2320f77f703d440d6
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2014-11-26 13:57:16 +0100

    find finding qt5 twice
    Change-Id: I952ff996918e18356dbbb2d05167f84f7a235ab1

--- cmake/qibuild/modules/qt5utils.cmake
+++ cmake/qibuild/modules/qt5utils.cmake
@@ -10,6 +10,7 @@ function(qt5_flib prefix name)
   # upstream uses Qt5::Core notation but we need to dereference this
   # because we won't be calling find_package() again
   find_package(${name})
+  qi_persistent_set(${name}_INCLUDE_DIRS ${${name}_INCLUDE_DIRS})
   qi_persistent_set(${prefix}_LIBRARIES ${${name}_LIBRARIES})
   qi_persistent_set(${prefix}_INCLUDE_DIRS ${${name}_INCLUDE_DIRS})
   set(_define ${prefix}_LIB)
