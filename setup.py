b81f933ef603206e8917056af28630ff7c60d6cd
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2014-12-09 11:48:45 +0100

    qibuild 3.7
    Change-Id: If82a492896a548f403dbe39b018180948a687b92

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_qibuild_cmake_files():
 data_files = get_qibuild_cmake_files()
 
 setup(name="qibuild",
-      version="next",
+      version="3.7",
       description="The meta build framework",
       url="https://community.aldebaran-robotics.com/doc/qibuild/",
       author="Aldebaran Robotics",
