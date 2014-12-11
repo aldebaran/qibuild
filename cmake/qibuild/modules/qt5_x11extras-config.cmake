fc04d0a83f3668c55143dc53942b3e7be9cfc75d
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2014-12-11 14:47:10 +0100

    cmake : add Qt5X11Extras
    Change-Id: Iaa5c44f15898dac4f68f2d0669043efb5e42cb51

---
+++ cmake/qibuild/modules/qt5_x11extras-config.cmake
@@ -0,0 +1,8 @@
+## Copyright (c) 2012-2014 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+get_filename_component(_ROOT_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
+include("${_ROOT_DIR}/qt5utils.cmake")
+
+qt5_flib(QT5_X11EXTRAS Qt5X11Extras)
