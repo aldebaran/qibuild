2ba348a0731e34e748dd8cdce2e73d46434c83b5
hkouki <hkouki@aldebaran-robotics.com>
2015-01-14 10:39:17 +0100

    add v4l finder
    Change-Id: Ibaf1b4f702ca3d4c4efcf85af0cb950ed7e479f4
    Reviewed-on: http://gerrit.aldebaran.lan/52157
    Approved: hkouki <hkouki@aldebaran-robotics.com>
    Reviewed-by: hkouki <hkouki@aldebaran-robotics.com>
    Tested-by: hkouki <hkouki@aldebaran-robotics.com>

---
+++ cmake/qibuild/modules/v4l-config.cmake
@@ -0,0 +1,10 @@
+## Copyright (c) 2012-2014 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+clean(V4L)
+fpath(V4L libv4l1.h)
+
+flib(V4L NAMES v4l1)
+export_lib(V4L)
+
