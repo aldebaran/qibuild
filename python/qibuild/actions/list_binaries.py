01fd18fd05a8afecc0f45dc411099d43d9ed22b3
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-03-12 15:21:43 +0100

    Add qibuild list-binaries
    Useful for `qibuild run` autocompletion
    
    Change-Id: I3092732103e8a5dfe72b9c90b55adf92c2381931
    Reviewed-on: http://gerrit.aldebaran.lan/55561
    Tested-by: gerrit
    Reviewed-by: ncornu <ncornu@aldebaran-robotics.com>

---
+++ python/qibuild/actions/list_binaries.py
@@ -0,0 +1,38 @@
+## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+""" List every all the binaries in the given worktree.
+
+"""
+# Mainly useful to auto-complete ``qibuild run``
+
+import os
+
+from qisys import ui
+import qibuild.parsers
+
+def configure_parser(parser):
+    """ Configure parser for this action """
+    qibuild.parsers.cmake_build_parser(parser)
+
+def do(args):
+    """ Main entry point """
+    build_worktree = qibuild.parsers.get_build_worktree(args)
+    sdk_dirs = [x.sdk_directory for x in build_worktree.build_projects]
+    bin_dirs = [os.path.join(x, "bin") for x in sdk_dirs]
+    res = list()
+    for bin_dir in bin_dirs:
+        if os.path.exists(bin_dir):
+            binaries = os.listdir(bin_dir)
+        else:
+            binaries = list()
+        if os.name == 'nt':
+            binaries = [x for x in binaries if x.endswith(".exe")]
+            binaries = [x.replace("_d.exe", "") for x in binaries]
+            binaries = [x.replace(".exe", "") for x in binaries]
+        res.extend(binaries)
+
+    res.sort()
+    for binary in res:
+        ui.info(binary)
---
+++ python/qibuild/test/test_qibuild_list_binaries.py
@@ -0,0 +1,13 @@
+## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+def test_list_binaries(qibuild_action, record_messages):
+    qibuild_action.add_test_project("world")
+    qibuild_action.add_test_project("testme")
+    qibuild_action("configure", "testme")
+    qibuild_action("make", "testme")
+    record_messages.reset()
+    qibuild_action("list-binaries")
+    assert record_messages.find("^ok$")
+    assert record_messages.find("^fail$")
