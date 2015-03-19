c61519b353c02d00a9a45debe0a05d42b7533176
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-03-19 16:13:33 +0100

    cmake/internal: add _qi_list_prepend_uniq
    Change-Id: I56cf0d74d5095f2dfcc858d3fdf84b5b7a13a1b2
    Reviewed-on: http://gerrit.aldebaran.lan/55919
    Tested-by: gerrit
    Reviewed-by: vbarbaresi <vbarbaresi@aldebaran-robotics.com>

--- cmake/qibuild/internal/list.cmake
+++ cmake/qibuild/internal/list.cmake
@@ -1,11 +1,22 @@
 ## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
 ## Use of this source code is governed by a BSD-style license that can be
 ## found in the COPYING file.
+
 function(_qi_list_append_uniq _list)
   foreach(_elem ${ARGN})
     list(FIND ${_list} ${_elem} _found)
     if(_found STREQUAL "-1")
       set(${_list} ${${_list}} ${_elem})
+    endif()
+  endforeach()
+  set(${_list} ${${_list}} PARENT_SCOPE)
+endfunction()
+
+function(_qi_list_prepend_uniq _list)
+  foreach(_elem ${ARGN})
+    list(FIND ${_list} ${_elem} _found)
+    if(_found STREQUAL "-1")
+      list(INSERT ${_list} 0 ${_elem})
     endif()
   endforeach()
   set(${_list} ${${_list}} PARENT_SCOPE)
@@ -22,4 +33,3 @@ function(_qi_list_append_path _list)
   endforeach()
   set(${_list} ${${_list}} PARENT_SCOPE)
 endfunction()
-
