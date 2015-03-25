f61c8b6a17b64d49bdc786ef5b29f55108484448
hkouki <hkouki@aldebaran-robotics.com>
2015-03-25 16:25:02 +0100

    add cgos finder
    Change-Id: Id45b0c9a43b2ff0ba049da2e7f0a812fa80b10bc

---
+++ cmake/qibuild/modules/cgos-config.cmake
@@ -0,0 +1,8 @@
+## Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+clean(CGOS)
+fpath(CGOS Cgos.h)
+flib(CGOS cgos)
+export_lib(CGOS)
