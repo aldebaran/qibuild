b51dd927a63e9e68184ef31df61946b8ec6ca2c0
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-03-26 16:36:50 +0100

    start next development
    Change-Id: I73c4e769bc398eb499ce86456ab5ca8ebbb78c12

--- cmake/qibuild/version.cmake
+++ cmake/qibuild/version.cmake
@@ -1,4 +1,4 @@
 ## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
 ## Use of this source code is governed by a BSD-style license that can be
 ## found in the COPYING file.
-set(QIBUILD_VERSION "3.8" CACHE STRING "qibuild version" FORCE)
+set(QIBUILD_VERSION "3.9" CACHE STRING "qibuild version" FORCE)
