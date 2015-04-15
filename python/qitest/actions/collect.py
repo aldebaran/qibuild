dff89c90a95b1ae5c4714d4cbf34e132024cd6ba
ngargaud <ngargaud@aldebaran-robotics.com>
2015-04-15 17:38:43 +0200

    new function to collect all python test in pytest.json
    Change-Id: I834125d19dd14022961c973acc7d6939a3a04ae7
    Reviewed-on: http://gerrit.aldebaran.lan/56842
    Reviewed-by: ngargaud <ngargaud@aldebaran-robotics.com>
    Tested-by: ngargaud <ngargaud@aldebaran-robotics.com>
    Tested-by: gerrit
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

---
+++ python/qitest/actions/collect.py
@@ -0,0 +1,20 @@
+## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+""" Collect all python test in worktree """
+
+from qisys import ui
+import qisys.parsers
+import qipy.parsers
+import qibuild.parsers
+from qitest.collector import PythonTestCollector
+
+def configure_parser(parser):
+    qibuild.parsers.cmake_build_parser(parser)
+
+def do(args):
+    python_worktree = qipy.parsers.get_python_worktree(args)
+    ui.info(ui.green, "python projects in:", ui.blue, python_worktree.root)
+    collector = PythonTestCollector(python_worktree)
+    collector.collect()
+
