c0f5fc970603baf05541e352e315a922b6d8ca23
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-04-22 15:34:55 +0200

    cmake modules: add COREFOUNDATION
    Change-Id: I843d644756e41fa3a41f8324da9d8811c1fc346e
    Reviewed-on: http://gerrit.aldebaran.lan/57206
    Tested-by: gerrit
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Tested-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

---
+++ cmake/qibuild/modules/corefoundation-config.cmake
@@ -0,0 +1,8 @@
+## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+clean(COREFOUNDATION)
+flib(COREFOUNDATION CoreFoundation)
+fpath(COREFOUNDATION CoreFoundation/CoreFoundation.h)
+export_lib(COREFOUNDATION)
