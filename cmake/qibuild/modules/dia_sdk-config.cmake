dadafd48fdbd84b6082ed3d27973e0ffe83224e8
Vincent Barbaresi <vbarbaresi@aldebaran-robotics.com>
2015-05-13 17:29:05 +0200

    dia-sdk: fix for VS2013
    No mistake: VS2013 is VC12
    
    Change-Id: Idae8abaa3e424261f551a48546e7e0c72f38744f
    Reviewed-on: http://gerrit.aldebaran.lan/58078
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
    Tested-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

--- cmake/qibuild/modules/dia_sdk-config.cmake
+++ cmake/qibuild/modules/dia_sdk-config.cmake
@@ -2,10 +2,17 @@
 
 clean(DIA_SDK)
 
-#FIXME: fix for vs2013
 # Path is c:\Program Files (x86)\Microsoft Visual Studio 10\Common7\Tools,
 # we want c:\Program Files (x86)\Microsoft Visual Studio 10\DIA SDK
-set(_comntools_path "$ENV{VS100COMNTOOLS}")
+# We cannot use VSINSTALLDIR because it's not set when using
+# 'Visual Studio ...' generators
+if(MSVC10)
+  set(_comntools_path "$ENV{VS100COMNTOOLS}")
+endif()
+if(MSVC12)
+  set(_comntools_path "$ENV{VS120COMNTOOLS}")
+endif()
+
 get_filename_component(_visual_studio_path "${_comntools_path}" DIRECTORY)
 get_filename_component(_visual_studio_path "${_visual_studio_path}" DIRECTORY)
 set(_dia_sdk_dir "${_visual_studio_path}/DIA SDK")
