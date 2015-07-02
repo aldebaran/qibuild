ff25179c41ab7878beaff28db2a59413417b6769
gvallat <gvallat@aldebaran-robotics.com>
2015-07-02 12:51:35 +0200

    Cmake: add qt5 Qml and Quick
    Change-Id: I254f6e465bc8e99ae1c50302ec61c7af8af82a5f

---
+++ cmake/qibuild/modules/qt5_qml-config.cmake
@@ -0,0 +1,9 @@
+## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+get_filename_component(_ROOT_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
+include("${_ROOT_DIR}/qt5utils.cmake")
+
+qt5_flib(QT5_QML Qt5Qml)
+qi_persistent_set(QT5_QML_DEPENDS QT5_NETWORK)
---
+++ cmake/qibuild/modules/qt5_quick-config.cmake
@@ -0,0 +1,9 @@
+## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+get_filename_component(_ROOT_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
+include("${_ROOT_DIR}/qt5utils.cmake")
+
+qt5_flib(QT5_QUICK Qt5Quick)
+qi_persistent_set(QT5_QUICK_DEPENDS QT5_GUI QT5_QML)
