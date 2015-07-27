f6c13a2fb0033c990417719098b034892aa0b6b7
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-07-27 11:26:01 +0200

    qisrc log: fix crash when not on a branch
    And add some tests
    
    Change-Id: Ie3c69abec407a362e4e70569e73f37bbe064ef67
    Reviewed-on: http://gerrit.aldebaran.lan/60519
    Tested-by: gerrit
    Reviewed-by: vbarbaresi <vbarbaresi@aldebaran-robotics.com>

--- python/qisrc/diff.py
+++ python/qisrc/diff.py
@@ -21,23 +21,30 @@ def diff_worktree(git_worktree, git_projects, branch, cmd=None):
             continue
         git = qisrc.git.Git(git_project.path)
         local_branch = git.get_current_branch()
-        remote_branch = remote_project.default_branch.name
-        remote_ref = "%s/%s" % (remote_project.default_remote.name, remote_branch)
-        rc, out = git.call("merge-base", local_branch, remote_ref, raises=False)
-        if rc != 0:
-            continue
-        merge_base = out.strip()
-        full_cmd = cmd + ["%s..%s" % (merge_base, local_branch)]
+        if not local_branch:
+            message = (ui.brown, "Not on a branch")
+        else:
+            remote_branch = remote_project.default_branch.name
+            remote_ref = "%s/%s" % (remote_project.default_remote.name, remote_branch)
+            rc, out = git.call("merge-base", local_branch, remote_ref, raises=False)
+            if rc != 0:
+                message = (ui.red, "Calling git merge-base failed")
+            else:
+                merge_base = out.strip()
+                full_cmd = cmd + ["%s..%s" % (merge_base, local_branch)]
 
-        color = ui.config_color(sys.stdout)
-        if color:
-            full_cmd.append("--color=always")
-        rc, out = git.call(*full_cmd, raises=False)
-        if rc != 0:
-            continue
-        if not out:
-            continue
+                color = ui.config_color(sys.stdout)
+                if color:
+                    full_cmd.append("--color=always")
+                rc, out = git.call(*full_cmd, raises=False)
+                if rc != 0:
+                    message = (ui.red, "Calling git log failed")
+                else:
+                    if out:
+                        message = (out,)
+                    else:
+                        continue
         ui.info(ui.bold, git_project.src)
         ui.info(ui.bold, "-" * len(git_project.src))
-        ui.info(out)
+        ui.info(*message)
         ui.info()
---
+++ python/qisrc/test/test_qisrc_log.py
@@ -0,0 +1,48 @@
+## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+import qisrc.git
+
+from qisrc.test.conftest import TestGitWorkTree
+
+def test_calls_git_log(git_server, qisrc_action, record_messages):
+    git_server.create_repo("foo.git")
+    git_server.switch_manifest_branch("devel")
+    git_server.change_branch("foo.git", "devel")
+    git_server.push_file("foo.git", "devel",
+                         "this is devel\n", branch="devel",
+                         message="start developing")
+    qisrc_action("init", git_server.manifest_url, "--branch", "devel")
+    record_messages.reset()
+    qisrc_action("log", "--all", "master")
+    assert record_messages.find("start developing")
+
+def test_when_not_on_a_branch(git_server, qisrc_action, record_messages):
+    git_server.create_repo("foo.git")
+    git_server.switch_manifest_branch("devel")
+    git_server.change_branch("foo.git", "devel")
+    git_server.push_file("foo.git", "devel",
+                         "this is devel\n", branch="devel",
+                         message="start developing")
+    qisrc_action("init", git_server.manifest_url, "--branch", "devel")
+    git_worktree = TestGitWorkTree()
+    foo = git_worktree.get_git_project("foo")
+    git = qisrc.git.Git(foo.path)
+    git.checkout("HEAD~1")
+    record_messages.reset()
+    qisrc_action("log", "--all", "master")
+    assert record_messages.find("Not on a branch")
+
+def test_skips_when_no_diff(git_server, qisrc_action, record_messages):
+    git_server.create_repo("foo.git")
+    git_server.create_repo("bar.git")
+    git_server.switch_manifest_branch("devel")
+    git_server.change_branch("foo.git", "devel")
+    git_server.change_branch("bar.git", "devel")
+    git_server.push_file("foo.git", "devel", "this is devel\n", branch="devel")
+    qisrc_action("init", git_server.manifest_url, "--branch", "devel")
+    record_messages.reset()
+    qisrc_action("log", "--all", "master")
+    assert record_messages.find("foo")
+    assert not record_messages.find("bar")
