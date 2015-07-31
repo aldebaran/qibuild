6eb1be75d064d818308015bc519394a5e5cd2bb0
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-07-31 17:44:37 +0200

    Add test for `qibuild set-default`
    Change-Id: Ic27fe262b60549c38a7b8ea7d432ec6f61437c98
    Reviewed-on: http://gerrit.aldebaran.lan/60711
    Tested-by: gerrit
    Reviewed-by: vbarbaresi <vbarbaresi@aldebaran-robotics.com>

---
+++ python/qibuild/test/test_qibuild_set_default.py
@@ -0,0 +1,12 @@
+## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+import qibuild.config
+from qibuild.test.conftest import TestBuildWorkTree
+
+def test_simple(qibuild_action):
+    qibuild.config.add_build_config("foo")
+    qibuild_action("set-default", "foo")
+    build_worktree = TestBuildWorkTree()
+    assert build_worktree.build_config.active_build_config.name == "foo"
