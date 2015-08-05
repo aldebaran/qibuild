43a7d87c377a6756185d7c6774519513e88a82b0
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-08-05 16:48:23 +0200

    qt5utils: clean up
    Change-Id: I7c03cc1abfc8b1cc700420b71740f132d6900b90
    Reviewed-on: http://gerrit.aldebaran.lan/60824
    Tested-by: gerrit
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

--- cmake/qibuild/modules/qt5utils.cmake
+++ cmake/qibuild/modules/qt5utils.cmake
@@ -10,11 +10,9 @@
 # qt5_flib(QT5_CORE Qt5Core)
 
 function(qt5_flib prefix name)
-  set(${prefix}_LIBRARIES)
   # upstream uses Qt5::Core notation but we need to dereference this
   # because we won't be calling find_package() again
   find_package(${name})
-  qi_persistent_set(${name}_INCLUDE_DIRS ${${name}_INCLUDE_DIRS})
   qi_persistent_set(${prefix}_LIBRARIES ${${name}_LIBRARIES})
   qi_persistent_set(${prefix}_INCLUDE_DIRS ${${name}_INCLUDE_DIRS})
   set(_define ${prefix}_LIB)
