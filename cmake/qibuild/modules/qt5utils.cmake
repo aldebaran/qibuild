18d00f4a92fa2fdf0320f43471d05bfad88cd94b
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-08-11 15:57:10 +0200

    qt5utils: fix QT5_CORE_INCLUDE_DIRS becoming empty
    Introduced by the cleanup of qt5utils
    
    Change-Id: Idc7d2d77f7f877d4e20649afc78fa64e89fc4769
    Reviewed-on: http://gerrit.aldebaran.lan/61012
    Tested-by: gerrit
    Tested-by: pdaouadi <pdaouadi@aldebaran-robotics.com>
    Reviewed-by: dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>

--- cmake/qibuild/modules/qt5utils.cmake
+++ cmake/qibuild/modules/qt5utils.cmake
@@ -12,7 +12,21 @@
 function(qt5_flib prefix name)
   # upstream uses Qt5::Core notation but we need to dereference this
   # because we won't be calling find_package() again
+
+  # the first time we call upstream's find_package it defines
+  # a target and set _INCLUDE_DIRS and LIBRARIES variables,
+  # but the second time the target is already defined, and the
+  # var get unset, so store it in cache when they are not
+  # empty
   find_package(${name})
+  if(${name}_INCLUDE_DIRS)
+    qi_persistent_set(${name}_INCLUDE_DIRS ${${name}_INCLUDE_DIRS})
+  endif()
+  if(${name}_LIBRARIES)
+    qi_persistent_set(${name}_LIBRARIES ${${name}_LIBRARIES})
+  endif()
+
+  # now set the varibables with the correct prefix:
   qi_persistent_set(${prefix}_LIBRARIES ${${name}_LIBRARIES})
   qi_persistent_set(${prefix}_INCLUDE_DIRS ${${name}_INCLUDE_DIRS})
   set(_define ${prefix}_LIB)
