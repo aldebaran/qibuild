ffe9542756e24c7da98c6e7bd0367e43945a2a70
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-08-12 16:19:29 +0200

    cmake: get qt5 related fixes from 'next'
    Change-Id: I160e8a20aef942eb1133170faf39231b7a26290d

--- cmake/qibuild/modules/qt5_svg-config.cmake
+++ cmake/qibuild/modules/qt5_svg-config.cmake
@@ -5,5 +5,5 @@
 get_filename_component(_ROOT_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
 include("${_ROOT_DIR}/qt5utils.cmake")
 
-qt5_flib(QT5_GUI Qt5Gui)
-qi_persistent_set(QT5_GUI_DEPENDS QT5_CORE)
+qt5_flib(QT5_SVG Qt5Svg)
+qi_persistent_set(QT5_SVG_DEPENDS QT5_CORE QT5_GUI QT5_WIDGETS)
--- cmake/qibuild/modules/qt5utils.cmake
+++ cmake/qibuild/modules/qt5utils.cmake
@@ -10,11 +10,23 @@
 # qt5_flib(QT5_CORE Qt5Core)
 
 function(qt5_flib prefix name)
-  set(${prefix}_LIBRARIES)
   # upstream uses Qt5::Core notation but we need to dereference this
   # because we won't be calling find_package() again
+
+  # the first time we call upstream's find_package it defines
+  # a target and set _INCLUDE_DIRS and LIBRARIES variables,
+  # but the second time the target is already defined, and the
+  # var get unset, so store it in cache when they are not
+  # empty
   find_package(${name})
-  qi_persistent_set(${name}_INCLUDE_DIRS ${${name}_INCLUDE_DIRS})
+  if(${name}_INCLUDE_DIRS)
+    qi_persistent_set(${name}_INCLUDE_DIRS ${${name}_INCLUDE_DIRS})
+  endif()
+  if(${name}_LIBRARIES)
+    qi_persistent_set(${name}_LIBRARIES ${${name}_LIBRARIES})
+  endif()
+
+  # now set the varibables with the correct prefix:
   qi_persistent_set(${prefix}_LIBRARIES ${${name}_LIBRARIES})
   qi_persistent_set(${prefix}_INCLUDE_DIRS ${${name}_INCLUDE_DIRS})
   set(_define ${prefix}_LIB)
