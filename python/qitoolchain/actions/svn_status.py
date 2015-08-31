23b47e837013bdd70c81e1ccc2af9f49f7ea91b5
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-08-31 15:20:57 +0200

    Add qitoolchain svn-status
    Change-Id: I5e5e2b0c31e926900544c483987dbbcf0c98e132
    Reviewed-on: http://gerrit.aldebaran.lan/61778
    Tested-by: gerrit
    Reviewed-by: vbarbaresi <vbarbaresi@aldebaran-robotics.com>

---
+++ python/qitoolchain/actions/svn_status.py
@@ -0,0 +1,44 @@
+## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+""" Display status of subversion packages in the given toolchain """
+
+import os
+import sys
+
+from qisys import ui
+import qisys.parsers
+import qitoolchain
+import qisrc.svn
+
+def configure_parser(parser):
+    """ Configure parser for this action """
+    qisys.parsers.default_parser(parser)
+    parser.add_argument("name", help="Name of the toolchain")
+
+def do(args):
+    """ Main entry point """
+    toolchain = qitoolchain.get_toolchain(args.name)
+    svn_packages = list()
+    for package in toolchain.packages:
+        svn_dir = os.path.join(package.path, ".svn")
+        if os.path.exists(svn_dir):
+            svn_packages.append(package)
+    not_clean = list()
+    for i, svn_package in enumerate(svn_packages, start=1):
+        to_write = "Checking (%d/%d) " % (i, len(svn_packages))
+        sys.stdout.write(to_write + "\r")
+        sys.stdout.flush()
+        svn = qisrc.svn.Svn(svn_package.path)
+        rc, out = svn.call("status", raises=False)
+        if out:
+            not_clean.append((svn_package.name, out))
+    if not not_clean:
+        ui.info("\n", ui.green, "All OK")
+        sys.exit(0)
+    ui.warning("Some svn packages are not clean")
+    for name, message in not_clean:
+        ui.info(ui.green, "*", ui.reset, ui.blue, name)
+        ui.info(message)
+    sys.exit(1)
---
+++ python/qitoolchain/test/test_qitoolchain_svn_status.py
@@ -0,0 +1,42 @@
+## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+import os
+
+import qitoolchain.svn_package
+import qitoolchain.toolchain
+
+from qisrc.test.conftest import svn_server
+
+def test_simple(qitoolchain_action, svn_server, feed, record_messages):
+    toolchain = qitoolchain.toolchain.Toolchain("foo")
+    boost_url = svn_server.create_repo("boost")
+    svn_server.commit_file("boost", "version.hpp", "")
+    qt_url = svn_server.create_repo("qt")
+    boost_package = qitoolchain.svn_package.SvnPackage("boost")
+    boost_package.url = boost_url
+    qt_package = qitoolchain.svn_package.SvnPackage("qt")
+    qt_package.url = qt_url
+
+    feed.add_svn_package(boost_package)
+    feed.add_svn_package(qt_package)
+
+    toolchain.update(feed.url)
+    record_messages.reset()
+    rc = qitoolchain_action("svn-status", "foo", retcode=True)
+    assert rc == 0
+    assert not record_messages.find("qt")
+    assert not record_messages.find("boost")
+    assert record_messages.find("All OK")
+
+    # Now create a problem in the svn repo:
+    boost_path = toolchain.get_package("boost").path
+    version_hpp = os.path.join(boost_path, "version.hpp")
+    os.remove(version_hpp)
+
+    record_messages.reset()
+    rc = qitoolchain_action("svn-status", "foo", retcode=True)
+    assert rc != 0
+    assert record_messages.find("boost")
+    assert record_messages.find("version.hpp")
