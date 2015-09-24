a6d4ba0a80f9a070ced1d8c7e20b38edf045acbe
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-09-24 11:24:37 +0200

    qibuild info: cosmetic changes
    * Can be used without a project name
    * Do not display build worktree configuration
    
    Change-Id: I03b976284ab562cd6d70a2e22b6ff16ff96a4bd9
    Reviewed-on: http://gerrit.aldebaran.lan/62676
    Tested-by: gerrit
    Reviewed-by: vbarbaresi <vbarbaresi@aldebaran-robotics.com>

--- python/qibuild/actions/info.py
+++ python/qibuild/actions/info.py
@@ -12,18 +12,21 @@ import qibuild.parsers
 def configure_parser(parser):
     """ Configure parser for this action """
     qisys.parsers.worktree_parser(parser)
-    parser.add_argument("name")
-
+    parser.add_argument("name", nargs="?")
 
 def do(args):
     """ Main entry point """
     name = args.name
-    build_worktree = qibuild.parsers.get_build_worktree(args)
-    build_project = build_worktree.get_build_project(name, raises=True)
+    build_worktree = qibuild.parsers.get_build_worktree(args, verbose=False)
+    if name:
+        build_project = build_worktree.get_build_project(name, raises=True)
+    else:
+        args.projects = list()
+        build_project = qibuild.parsers.get_one_build_project(build_worktree, args)
     git_worktree = qisrc.parsers.get_git_worktree(args)
     git_project = qisys.parsers.find_parent_project(git_worktree.git_projects,
             build_project.path)
-    mess = "Build project: %s\n" % name
+    mess = "Build project: %s\n" % build_project.name
     mess += "src: %s\n" % build_project.src
     if git_project:
         mess += "repo: %s" % git_project.name
--- python/qibuild/test/test_qibuild_info.py
+++ python/qibuild/test/test_qibuild_info.py
@@ -7,8 +7,12 @@ from qisrc.test.conftest import git_server, qisrc_action
 def test_info(qibuild_action, qisrc_action, git_server, record_messages):
     git_server.create_repo("foo.git")
     qisrc_action("init", git_server.manifest_url)
-    qibuild_action.create_project("foo")
+    foo_project = qibuild_action.create_project("foo")
     record_messages.reset()
     qibuild_action("info", "foo")
     assert record_messages.find("src: foo")
     assert record_messages.find("repo: foo.git")
+    qibuild_action.chdir(foo_project.path)
+    record_messages.reset()
+    qibuild_action("info")
+    assert record_messages.find("Build project: foo")
