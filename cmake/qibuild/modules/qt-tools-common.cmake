9b501ad546fc50a6a288d85a20f25f5dcba7e418
gschweitzer <gschweitzer@aldebaran-robotics.com>
2015-09-24 14:06:27 +0200

    Fix qi_set_qt_plugin_properties to not lose previous compile definitions
    Change-Id: Ife2b87597fe43dc0c664550e7d3a746b1cfdd3be
    Reviewed-on: http://gerrit.aldebaran.lan/62687
    Reviewed-by: gvallat <gvallat@aldebaran-robotics.com>
    Tested-by: gerrit

--- cmake/qibuild/modules/qt-tools-common.cmake
+++ cmake/qibuild/modules/qt-tools-common.cmake
@@ -4,15 +4,18 @@
 
 function(qi_set_qt_plugin_properties target)
   if(CMAKE_MAJOR_VERSION LESS 3)
+    get_target_property(RELEASE_DEF "${target}" COMPILE_DEFINITIONS_RELEASE)
+    get_target_property(DEBUG_DEF "${target}" COMPILE_DEFINITIONS_DEBUG)
     set_target_properties("${target}"
       PROPERTIES
-      COMPILE_DEFINITIONS_RELEASE "QT_SHARED;QT_PLUGIN;QT_NO_DEBUG"
-      COMPILE_DEFINITIONS_DEBUG "QT_SHARED;QT_PLUGIN;QT_DEBUG"
+      COMPILE_DEFINITIONS_RELEASE "${RELEASE_DEF};QT_SHARED;QT_PLUGIN;QT_NO_DEBUG"
+      COMPILE_DEFINITIONS_DEBUG "${DEBUG_DEF};QT_SHARED;QT_PLUGIN;QT_DEBUG"
     )
   else()
+    get_target_property(DEF "${target}" COMPILE_DEFINITIONS)
     set_target_properties("${target}"
       PROPERTIES
-      COMPILE_DEFINITIONS "QT_SHARED;QT_PLUGIN;$<$<CONFIG:Release>:QT_NO_DEBUG>$<$<CONFIG:Debug>:QT_DEBUG>"
+      COMPILE_DEFINITIONS "${DEF};QT_SHARED;QT_PLUGIN;$<$<CONFIG:Release>:QT_NO_DEBUG>$<$<CONFIG:Debug>:QT_DEBUG>"
     )
   endif()
 endfunction()
