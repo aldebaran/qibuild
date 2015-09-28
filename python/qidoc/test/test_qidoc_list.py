0d1ce8eaf0329ab6efb6e1bbbb0edca2e78ce649
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-09-28 17:52:27 +0200

    Add a test for `qidoc list`
    Change-Id: I0fcd9fa17c2d69c5d90ec61eca872ca9a23f92b3
    Reviewed-on: http://gerrit.aldebaran.lan/62795
    Tested-by: gerrit
    Reviewed-by: vbarbaresi <vbarbaresi@aldebaran-robotics.com>

---
+++ python/qidoc/test/test_qidoc_list.py
@@ -0,0 +1,10 @@
+## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+def test_simple(qidoc_action, record_messages):
+    qidoc_action.add_test_project("world")
+    qidoc_action.add_test_project("libworld")
+    qidoc_action("list")
+    assert record_messages.find("\*\s+world")
+    assert record_messages.find("\*\s+libworld")
