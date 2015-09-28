a13f23b5f1b11b40db3325c46add3508f43cf649
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-09-28 17:52:38 +0200

    Add a test for `qilinguist list`
    Change-Id: Ia2b2e937732e80a231df9ca57a9d9f39d337da1f
    Reviewed-on: http://gerrit.aldebaran.lan/62796
    Tested-by: gerrit
    Reviewed-by: vbarbaresi <vbarbaresi@aldebaran-robotics.com>

---
+++ python/qilinguist/test/test_qilinguist_list.py
@@ -0,0 +1,12 @@
+## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+from qibuild.test.conftest import TestBuildWorkTree
+
+def test_simple(qilinguist_action, record_messages):
+    build_worktree = TestBuildWorkTree()
+    build_worktree.add_test_project("translateme/qt")
+    qilinguist_action("list")
+    assert record_messages.find("\*\s+helloqt")
+    assert record_messages.find("\*\s+translate")
