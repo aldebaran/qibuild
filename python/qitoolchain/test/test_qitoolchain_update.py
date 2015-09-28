5a1ad4a7f48098b14c6502e1d35eb7ab010bda24
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-09-28 17:51:42 +0200

    qitoolchain update: add more tests
    Change-Id: Ibb0c9da6b545e738c8b1e12aebab1fb92fad1502
    Reviewed-on: http://gerrit.aldebaran.lan/62792
    Tested-by: gerrit
    Reviewed-by: vbarbaresi <vbarbaresi@aldebaran-robotics.com>

--- python/qitoolchain/test/test_qitoolchain_update.py
+++ python/qitoolchain/test/test_qitoolchain_update.py
@@ -1,6 +1,7 @@
 ## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
 ## Use of this source code is governed by a BSD-style license that can be
 ## found in the COPYING file.
+
 def test_update_local_ctc(qitoolchain_action, tmpdir):
     ctc_path = tmpdir.join("ctc").ensure(dir=True)
     ctc_path.join("toolchain.xml").write("""
@@ -14,3 +15,15 @@ def test_update_local_ctc(qitoolchain_action, tmpdir):
     qitoolchain_action("create", "ctc", toolchain_xml.strpath)
     qitoolchain_action("update", "ctc", toolchain_xml.strpath)
     assert ctc_path.check(dir=True)
+
+def test_update_no_feed(qitoolchain_action):
+    qitoolchain_action("create", "foo")
+    error = qitoolchain_action("update", "foo", raises=True)
+    assert "Could not find feed" in error
+
+def test_udpate_all_toolchains(qitoolchain_action, feed, record_messages):
+    qitoolchain_action("create", "foo", feed.url)
+    qitoolchain_action("create", "bar")
+    qitoolchain_action("update")
+    assert record_messages.find("\[WARN \]:\s+No feed found for bar")
+    assert record_messages.find("Updating foo")
