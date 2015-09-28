292cb042f16d3ec86bbbc199e485fcbd97943631
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-09-28 17:52:19 +0200

    Add a test for `qidoc clean`
    Change-Id: Ia0ab2e005218a1c80be8fdd5d5e0a9b8e7936ec0
    Reviewed-on: http://gerrit.aldebaran.lan/62794
    Tested-by: gerrit
    Reviewed-by: vbarbaresi <vbarbaresi@aldebaran-robotics.com>

---
+++ python/qidoc/test/test_qidoc_clean.py
@@ -0,0 +1,16 @@
+## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+import os
+
+def test_simple(qidoc_action):
+    world_proj = qidoc_action.add_test_project("world")
+    build_dir = os.path.join(world_proj.path, "build-doc")
+    assert not os.path.exists(build_dir)
+    qidoc_action("build", "world")
+    assert os.path.exists(build_dir)
+    qidoc_action("clean", "world")
+    assert os.path.exists(build_dir)
+    qidoc_action("clean", "world", "--force")
+    assert not os.path.exists(build_dir)
