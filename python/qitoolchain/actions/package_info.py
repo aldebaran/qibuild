0eaada73fb4a18bf992c1727d0d9883ea0f9647f
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-10-02 17:44:45 +0200

    Add qitoolchain package-info
    Change-Id: I1db4fa4f62445ab01a9f392e8232995889edd4d7
    Reviewed-on: http://gerrit.aldebaran.lan/62971
    Tested-by: gerrit
    Reviewed-by: vbarbaresi <vbarbaresi@aldebaran-robotics.com>

---
+++ python/qitoolchain/actions/package_info.py
@@ -0,0 +1,25 @@
+## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+""" Display info about a toolchain package """
+
+from qisys import ui
+import qisys.parsers
+import qitoolchain.parsers
+
+def configure_parser(parser):
+    """ Configure parser for this action """
+    qisys.parsers.worktree_parser(parser)
+    qitoolchain.parsers.toolchain_parser(parser)
+    parser.add_argument("package_name")
+
+def do(args):
+    """ Main entry point """
+    toolchain = qitoolchain.parsers.get_toolchain(args)
+    package = toolchain.get_package(args.package_name, raises=True)
+    package.load_package_xml()
+    ui.info(package.name, package.version)
+    ui.info("path:", package.path)
+    if package.license:
+        ui.info("license:", package.license)
---
+++ python/qitoolchain/test/test_qitoolchain_package_info.py
@@ -0,0 +1,9 @@
+## Copyright (c) 2012-2015 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+def test_simple(qitoolchain_action, toolchains, record_messages):
+    toolchains.create("foo")
+    toolchains.add_package("foo", "boost", package_version="1.57-r3")
+    qitoolchain_action("package-info", "--toolchain", "foo", "boost")
+    assert record_messages.find("boost 1.57-r3")
