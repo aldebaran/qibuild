67726cab5b2a708b7128110332195624efc98cb5
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-10-21 17:01:43 +0200

    qibuild 3.10.1
    Change-Id: I125476a5fcc5e73224599cfd0bb466a9b372acc5

--- doc/source/conf.in.py
+++ doc/source/conf.in.py
@@ -3,7 +3,7 @@
 ## found in the COPYING file.
 
 project = u'qiBuild'
-version = '3.10'
+version = '3.10.1'
 sys.path.insert(0, os.path.abspath('../tools'))
 # for autodoc
 sys.path.insert(0, os.path.abspath('../../python'))
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_qibuild_cmake_files():
 data_files = get_qibuild_cmake_files()
 
 setup(name="qibuild",
-      version="3.10",
+      version="3.10.1",
       description="The meta build framework",
       url="http://doc.aldebaran.com/qibuild",
       author="Aldebaran Robotics",
