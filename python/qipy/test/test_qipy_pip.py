28064c8953fb6bfd88840d6bec2dc4c3e1b9e232
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2016-01-04 18:29:10 +0100

    qipy: fix tests in some cases
    we were not using --no-site-packages, causing
    the test to fail if jinja was already installed in the system
    
    Change-Id: I50a345f0c7dfa5af368798fe77cdb58ee1dee3e5

--- python/qipy/test/test_qipy_pip.py
+++ python/qipy/test/test_qipy_pip.py
@@ -7,11 +7,10 @@ import os
 import qipy.parsers
 
 def test_simple(qipy_action, args):
-    qipy_action("bootstrap")
+    qipy_action("bootstrap", "--no-site-packages")
     python_worktree = qipy.parsers.get_python_worktree(args)
     venv_path = python_worktree.venv_path
     jinja_path = os.path.join(venv_path, "lib", "python2.7", "site-packages", "jinja")
     assert not os.path.exists(jinja_path)
     qipy_action("pip", "install", "jinja")
     assert os.path.exists(jinja_path)
-
