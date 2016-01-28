0536ba032e20d8294bdeea638c2f1881b387a428
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2016-01-28 16:44:58 +0100

    qitoolchain: small fixes in UI
    This is a combination of 2 commits.
    
    The first commit's message is:
    
     - qitoolchain create: fix using -b
    
    This is the 2nd commit message:
    
    - Fix using -b with qitoolchain create
    
    Forward-ported from qibuild 3.11.1

--- python/qitoolchain/test/test_qitoolchain_update.py
+++ python/qitoolchain/test/test_qitoolchain_update.py
@@ -2,6 +2,10 @@
 ## Use of this source code is governed by a BSD-style license that can be
 ## found in the COPYING file.
 
+import qitoolchain.qipackage
+
+from qisrc.test.conftest import git_server
+
 def test_update_local_ctc(qitoolchain_action, tmpdir):
     ctc_path = tmpdir.join("ctc").ensure(dir=True)
     ctc_path.join("toolchain.xml").write("""
@@ -27,3 +31,16 @@ def test_udpate_all_toolchains(qitoolchain_action, feed, record_messages):
     qitoolchain_action("update")
     assert record_messages.find("These toolchains will be skipped because they have no feed: bar")
     assert record_messages.find("Updating foo")
+
+def test_switching_to_git_feed(qitoolchain_action, git_server, feed, record_messages):
+    boost_package = qitoolchain.qipackage.QiPackage("boost", version="1.44")
+    feed.add_package(boost_package)
+    qitoolchain_action("create", "foo", feed.feed_xml.strpath)
+    toolchain_repo = git_server.create_repo("toolchains.git")
+    boost_package.version = "1.45"
+    feed.add_package(boost_package)
+    git_server.push_file("toolchains.git", "feeds/foo.xml", feed.feed_xml.read())
+    qitoolchain_action("update", "--name", "foo", "foo", toolchain_repo.clone_url)
+    assert record_messages.find("from 1.44 to 1.45")
+    qitoolchain_action("info", "foo")
+    assert record_messages.find("on master")
