e016d6c45fb92e33df184e889d5370ca7106f04a
Dimitri Merejkowsky <dmerejkowsky@aldebaran.com>
2016-02-11 13:38:14 +0100

    oups, fix 497f582
    Change-Id: I277fcf3e3654c2077f2726feea112e3ea5e7d8f7

--- python/qisys/remote.py
+++ python/qisys/remote.py
@@ -246,7 +246,7 @@ def deploy(local_directory, remote_url, filelist=None):
 
 class URLParseError(qisys.error.Error):
     def __init__(self, message):
-        super(URLParseError).__init__(message)
+        super(URLParseError, self).__init__(message)
 
 class URL(object):
     def __init__(self, url_as_string):
