aldebaran/qibuild

commit 3f5fde3f9336699f74ff938b26c9cfd0d61868bb

fix subfeed in qitoolchain if git is used as feed source
Change-Id: I95f51641ab9758b3caf0353435d3dc1d9ac9e6f6
Reviewed-on: http://gerrit.aldebaran.lan/86197
Tested-by: ngargaud <ngargaud@aldebaran-robotics.com>
Reviewed-by: ngargaud <ngargaud@aldebaran-robotics.com>
Reviewed-by: mballarini <mballarini@aldebaran-robotics.com>

python/qitoolchain/feed.py
@@ -26,7 +26,6 @@ def is_url(location):
     """ Check that a given location is an URL """
     return "://" in location
 
-
 def raise_parse_error(package_tree, feed, message):
     """ Raise a nice pasing error about the given
     package_tree element.
@@ -73,8 +72,9 @@ def open_git_feed(toolchain_name, feed_url, name=None, branch="master", first_pa
             git.call("reset", "--hard", "--quiet", "origin/%s" % branch)
         else:
             git.clone(feed_url, "--quiet", "--branch", branch)
-
-    feed_path = os.path.join(git_path, "feeds", name + ".xml")
+        feed_path = os.path.join(git_path, "feeds", name + ".xml")
+    else:
+        feed_path = feed_url
     return feed_path
 
 class ToolchainFeedParser:
@@ -126,11 +126,10 @@ class ToolchainFeedParser:
         """
         tc_path = qisys.sh.get_share_path("qi", "toolchains", self.name)
         if branch and name:
-            feed_path = open_git_feed(self.name, feed, branch=branch, name=name,
+            feed = open_git_feed(self.name, feed, branch=branch, name=name,
                                       first_pass=first_pass)
-            tree = tree_from_feed(feed_path)
-        else:
-            tree = tree_from_feed(feed)
+
+        tree = tree_from_feed(feed)
         package_trees = tree.findall("package")
         package_trees.extend(tree.findall("svn_package"))
         for package_tree in package_trees:
@@ -146,15 +145,20 @@ class ToolchainFeedParser:
 
         feeds = tree.findall("feed")
         for feed_tree in feeds:
+            feed_name = feed_tree.get("name")
             feed_url = feed_tree.get("url")
-            if feed_url:
-                # feed_url can be relative to feed:
-                if not "://" in feed_url:
+            # feed_url can be relative to feed:
+            if feed_tree.get("path") and branch:
+                feed_path = os.path.join(tc_path + ".git", feed_tree.get("path"))
+                self.parse(feed_path)
+                if feed_name:
+                    self.parse(feed_path, branch=branch, name=feed_name, first_pass=False)
+            elif feed_url:
+                if not is_url(feed_url):
                     feed_url = urlparse.urljoin(feed, feed_url)
                 self.parse(feed_url)
-            feed_name = feed_tree.get("name")
-            if feed_name:
-                self.parse(feed, branch=branch, name=feed_name, first_pass=False)
+                if feed_name:
+                    self.parse(feed, branch=branch, name=feed_name, first_pass=False)
         select_tree = tree.find("select")
         if select_tree is not None:
             blacklist_trees = select_tree.findall("blacklist")

commit a0811fb255e45d979729654e016cb7214703f8c2

qitoolchain: Fix feed test that ensure local subfeeds work
This fixes a regression in qitoolchain/test/test_feed.py:test_git introduced
in 3f5fde3f93.

Change-Id: I500f19ffa3ce2037f885f6274a81546624ff160a
Reviewed-on: http://gerrit.aldebaran.lan/87142
Reviewed-by: mballarini <mballarini@aldebaran-robotics.com>
Reviewed-by: ngargaud <ngargaud@aldebaran-robotics.com>
Tested-by: ngargaud <ngargaud@aldebaran-robotics.com>

python/qitoolchain/feed.py
@@ -72,9 +72,8 @@ def open_git_feed(toolchain_name, feed_url, name=None, branch="master", first_pa
             git.call("reset", "--hard", "--quiet", "origin/%s" % branch)
         else:
             git.clone(feed_url, "--quiet", "--branch", branch)
-        feed_path = os.path.join(git_path, "feeds", name + ".xml")
-    else:
-        feed_path = feed_url
+
+    feed_path = os.path.join(git_path, "feeds", name + ".xml")
     return feed_path
 
 class ToolchainFeedParser:
@@ -126,10 +125,12 @@ class ToolchainFeedParser:
         """
         tc_path = qisys.sh.get_share_path("qi", "toolchains", self.name)
         if branch and name:
-            feed = open_git_feed(self.name, feed, branch=branch, name=name,
-                                      first_pass=first_pass)
+            feed_location = open_git_feed(self.name, feed, branch=branch, name=name,
+                                          first_pass=first_pass)
+        else:
+            feed_location = feed
 
-        tree = tree_from_feed(feed)
+        tree = tree_from_feed(feed_location)
         package_trees = tree.findall("package")
         package_trees.extend(tree.findall("svn_package"))
         for package_tree in package_trees:
@@ -147,18 +148,22 @@ class ToolchainFeedParser:
         for feed_tree in feeds:
             feed_name = feed_tree.get("name")
             feed_url = feed_tree.get("url")
-            # feed_url can be relative to feed:
-            if feed_tree.get("path") and branch:
-                feed_path = os.path.join(tc_path + ".git", feed_tree.get("path"))
-                self.parse(feed_path)
-                if feed_name:
-                    self.parse(feed_path, branch=branch, name=feed_name, first_pass=False)
-            elif feed_url:
+            feed_path = feed_tree.get("path")
+
+            if feed_url:
+                # feed_url can be relative to feed:
                 if not is_url(feed_url):
                     feed_url = urlparse.urljoin(feed, feed_url)
                 self.parse(feed_url)
-                if feed_name:
-                    self.parse(feed, branch=branch, name=feed_name, first_pass=False)
+
+            if feed_path and branch:
+                feed_path = os.path.join(tc_path + ".git", feed_path)
+
+            feed = feed_path or feed_url or feed
+
+            if feed_name:
+                self.parse(feed, branch=branch, name=feed_name, first_pass=False)
+
         select_tree = tree.find("select")
         if select_tree is not None:
             blacklist_trees = select_tree.findall("blacklist")