eacb82bc2bc21e3abf5f34b208aa1e46c560ecff
Julien Blondeau <julien.blondeau@external.softbankrobotics.com>
2018-02-07 11:26:01 +0100

    [#4] pylint fix deprecated-lambda
    Change-Id: I4dc4ec115503e9a3d820943ba1e450283796121d

--- python/qidoc/sphinx_project.py
+++ python/qidoc/sphinx_project.py
@@ -94,7 +94,7 @@ class SphinxProject(qidoc.project.DocProject):
         self.append_doxy_xml_path(path_list)
         return (
             "\nqiapidoc_srcs=[" +
-            ','.join(map(lambda x: "r'" + x + "'", path_list)) +
+            ','.join(["r'" + x + "'" for x in path_list]) +
             "]\n")
 
     def append_doxylink_settings(self, conf, rel_paths=False):
