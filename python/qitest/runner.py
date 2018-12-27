cb00ddbe4c5292824cb6c2e916f83848cf4d6d24
ngargaud <ngargaud@aldebaran-robotics.com>
2018-12-27 14:55:00 +0100

    extend copyright to 2019
    Change-Id: Iccaf4d9c8dec93790882e447871fdd5de7926729

--- python/qitest/runner.py
+++ python/qitest/runner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (c) 2012-2018 SoftBank Robotics. All rights reserved.
+# Copyright (c) 2012-2019 SoftBank Robotics. All rights reserved.
 # Use of this source code is governed by a BSD-style license (see the COPYING file).
 """ QiTest Runner """
 from __future__ import absolute_import
