269339a5f276a9507b1e52a758b6a7ff4f80ecc1
ngargaud <ngargaud@aldebaran-robotics.com>
2020-07-16 15:06:03 +0200

    fix bad version number in cmake

--- cmake/qibuild/version.cmake
+++ cmake/qibuild/version.cmake
@@ -1,4 +1,4 @@
 ## Copyright (c) 2012-2020 SoftBank Robotics. All rights reserved.
 ## Use of this source code is governed by a BSD-style license that can be
 ## found in the COPYING file.
-set(QIBUILD_VERSION "3.14.1" CACHE STRING "qibuild version" FORCE)
+set(QIBUILD_VERSION "3.15" CACHE STRING "qibuild version" FORCE)
